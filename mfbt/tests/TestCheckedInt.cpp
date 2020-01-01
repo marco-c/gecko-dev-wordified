16d502a0d61dbd42c99633007ceb215298d3ddc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 17:53:56 +0000

    Bug 849667 - Fix/simplify CheckedInt's use-outside-of-MFBT setup - r=Waldo
    UltraBlame original commit: 7e347694edf22feab6a55d7e04b7a7a7174a2697

--- mfbt/tests/TestCheckedInt.cpp
+++ mfbt/tests/TestCheckedInt.cpp
@@ -114,10 +114,11 @@ climits
 >
 #
 ifndef
-MOZ_CHECKEDINT_ENABLE_MOZ_ASSERTS
+MOZ_CHECKEDINT_USE_MFBT
 #
 error
-MOZ_CHECKEDINT_ENABLE_MOZ_ASSERTS
+"
+MOZ_CHECKEDINT_USE_MFBT
 should
 be
 defined
@@ -125,6 +126,7 @@ by
 CheckedInt
 .
 h
+"
 #
 endif
 using
