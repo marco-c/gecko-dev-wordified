8725a46fea8c2a405891089c9d990c3620045d18
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 21:16:09 +0000

    Bug 893117: Remove nsIDOMHTMLTableColElement r=peterv
    UltraBlame original commit: 8ea5fd72aa590b215b4dd47a95e620026331e009

--- content/html/content/src/HTMLTableColElement.h
+++ content/html/content/src/HTMLTableColElement.h
@@ -115,13 +115,6 @@ nsGenericHTMLElement
 .
 h
 "
-#
-include
-"
-nsIDOMHTMLTableColElement
-.
-h
-"
 namespace
 mozilla
 {
@@ -135,7 +128,7 @@ MOZ_FINAL
 public
 nsGenericHTMLElement
 public
-nsIDOMHTMLTableColElement
+nsIDOMHTMLElement
 {
 public
 :
@@ -176,10 +169,6 @@ NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
 /
 nsIDOMHTMLElement
 NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
-/
-/
-nsIDOMHTMLTableColElement
-NS_DECL_NSIDOMHTMLTABLECOLELEMENT
 uint32_t
 Span
 (
