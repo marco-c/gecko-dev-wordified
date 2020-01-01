44b9b9546af5907c749a232d180437c9d302ba0c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 11:54:15 +0000

    Bug 580408 - Import jemalloc 3.0.0. r=khuey,r=gerv
    UltraBlame original commit: c27de97f078dc7c2276af6484b718b54730180d8

---
+++ memory/jemalloc/src/src/atomic.c
@@ -0,0 +1,14 @@
+#
+define
+JEMALLOC_ATOMIC_C_
+#
+include
+"
+jemalloc
+/
+internal
+/
+jemalloc_internal
+.
+h
+"
---
+++ memory/jemalloc/src/src/mb.c
@@ -0,0 +1,14 @@
+#
+define
+JEMALLOC_MB_C_
+#
+include
+"
+jemalloc
+/
+internal
+/
+jemalloc_internal
+.
+h
+"
