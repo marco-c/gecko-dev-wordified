28917a49a24471cda6a0e9ba1f9e2020c9d030e2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 17:00:06 +0000

    Bug 836735 - Add interface to QI implementation for ProcessPriorityManager. r=bent
    UltraBlame original commit: a033cebb0e4acb68fd86f18d5a7449920ede252c

--- dom/ipc/ProcessPriorityManager.cpp
+++ dom/ipc/ProcessPriorityManager.cpp
@@ -1266,11 +1266,12 @@ mMemoryMinimizerRunnable
 ;
 }
 ;
-NS_IMPL_ISUPPORTS2
+NS_IMPL_ISUPPORTS3
 (
 ProcessPriorityManager
 nsIObserver
 nsIDOMEventListener
+nsITimerCallback
 )
 ProcessPriorityManager
 :
