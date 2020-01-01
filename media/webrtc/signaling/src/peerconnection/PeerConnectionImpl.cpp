fc3815bd253b000cbdc6a58c32165ebd460cfa41
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 19:24:15 +0000

    Bug 784519 - Part 2: Fix success callback event names r=jesup
    UltraBlame original commit: 6d5ff743bb6bfe3db8b0de28af080122256f6ac6

--- media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
+++ media/webrtc/signaling/src/peerconnection/PeerConnectionImpl.cpp
@@ -908,7 +908,7 @@ if
 mCallState
 =
 =
-CREATEOFFER
+CREATEOFFERSUCCESS
 )
 |
 |
@@ -916,7 +916,7 @@ CREATEOFFER
 mCallState
 =
 =
-CREATEANSWER
+CREATEANSWERSUCCESS
 )
 )
 {
@@ -1119,7 +1119,7 @@ mCallState
 )
 {
 case
-CREATEOFFER
+CREATEOFFERSUCCESS
 :
 mObserver
 -
@@ -1136,7 +1136,7 @@ c_str
 break
 ;
 case
-CREATEANSWER
+CREATEANSWERSUCCESS
 :
 mObserver
 -
@@ -1189,7 +1189,7 @@ c_str
 break
 ;
 case
-SETLOCALDESC
+SETLOCALDESCSUCCESS
 :
 /
 /
@@ -1274,7 +1274,7 @@ OnSetLocalDescriptionSuccess
 break
 ;
 case
-SETREMOTEDESC
+SETREMOTEDESCSUCCESS
 :
 /
 /
@@ -7244,7 +7244,7 @@ event
 )
 {
 case
-SETLOCALDESC
+SETLOCALDESCSUCCESS
 :
 case
 UPDATELOCALDESC
@@ -7261,7 +7261,7 @@ getSDP
 break
 ;
 case
-SETREMOTEDESC
+SETREMOTEDESCSUCCESS
 :
 case
 ADDICECANDIDATE
