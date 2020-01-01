fbb1c540549b05bfb1649682015c5ddf3fce4566
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 02:42:27 +0000

    Bug 513493 - Adjust native theme colors to match Snow Leopard. r=josh
    UltraBlame original commit: 9bbef3004ce835238e7f0e52ab6db623289f56f2

--- widget/src/cocoa/nsToolkit.h
+++ widget/src/cocoa/nsToolkit.h
@@ -487,6 +487,10 @@ MAC_OS_X_VERSION_10_4_HEX
 define
 MAC_OS_X_VERSION_10_5_HEX
 0x00001050
+#
+define
+MAC_OS_X_VERSION_10_6_HEX
+0x00001060
 class
 nsToolkit
 :
@@ -542,6 +546,12 @@ version
 static
 PRBool
 OnLeopardOrLater
+(
+)
+;
+static
+PRBool
+OnSnowLeopardOrLater
 (
 )
 ;
