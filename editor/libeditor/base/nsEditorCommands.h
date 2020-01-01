1d98c46f31961b7bf94bd6191f7100d2e284ac2d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 03:10:00 +0000

    Support receiving data from Mac OS X services into text and HTML editors. b=525389 r=josh r/sr=smaug
    UltraBlame original commit: bee6729280d8b9ac25456cbe64ea7c92104e193b

--- editor/libeditor/base/nsEditorCommands.h
+++ editor/libeditor/base/nsEditorCommands.h
@@ -673,6 +673,10 @@ nsPasteCommand
 )
 NS_DECL_EDITOR_COMMAND
 (
+nsPasteTransferableCommand
+)
+NS_DECL_EDITOR_COMMAND
+(
 nsSwitchTextDirectionCommand
 )
 NS_DECL_EDITOR_COMMAND
--- editor/libeditor/base/nsEditorController.cpp
+++ editor/libeditor/base/nsEditorController.cpp
@@ -746,6 +746,14 @@ cmd_paste
 ;
 NS_REGISTER_ONE_COMMAND
 (
+nsPasteTransferableCommand
+"
+cmd_pasteTransferable
+"
+)
+;
+NS_REGISTER_ONE_COMMAND
+(
 nsSwitchTextDirectionCommand
 "
 cmd_switchTextDirection
