0b5951e7da570aa21b38c729dd8e6b8a3fbafa67
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 05:56:04 +0000

    Bug 605019: Evict entries in the bfcache if they are holding open a database which is being upgraded. r=bent/smaug a=blocker
    UltraBlame original commit: d25bfdc458cf2edd9dd590afd3dc54633095bc59

--- docshell/shistory/src/nsSHEntry.h
+++ docshell/shistory/src/nsSHEntry.h
@@ -570,6 +570,8 @@ public
 nsISHContainer
 public
 nsIMutationObserver
+public
+nsISHEntryInternal
 {
 public
 :
@@ -588,6 +590,7 @@ other
 NS_DECL_ISUPPORTS
 NS_DECL_NSIHISTORYENTRY
 NS_DECL_NSISHENTRY
+NS_DECL_NSISHENTRYINTERNAL
 NS_DECL_NSISHCONTAINER
 NS_DECL_NSIMUTATIONOBSERVER
 void
@@ -627,11 +630,6 @@ private
 :
 ~
 nsSHEntry
-(
-)
-;
-void
-DocumentMutated
 (
 )
 ;
