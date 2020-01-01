fb00453a9768dd19d01d3cbdadc77892b154e666
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 02:10:28 +0000

    Backing out changeset f640af4933fc and changeset ca15d173817d (fix for bug 285167) to investigate TXul regression
    UltraBlame original commit: 9aca8f755e95d69d3b4cb6978f4850c7a5d56d30

--- accessible/tests/mochitest/role.js
+++ accessible/tests/mochitest/role.js
@@ -195,13 +195,6 @@ nsIAccessibleRole
 ROLE_IMAGE_MAP
 ;
 const
-ROLE_INDICATOR
-=
-nsIAccessibleRole
-.
-ROLE_INDICATOR
-;
-const
 ROLE_INTERNAL_FRAME
 =
 nsIAccessibleRole
@@ -284,13 +277,6 @@ ROLE_ROW
 nsIAccessibleRole
 .
 ROLE_ROW
-;
-const
-ROLE_SCROLLBAR
-=
-nsIAccessibleRole
-.
-ROLE_SCROLLBAR
 ;
 const
 ROLE_SECTION
