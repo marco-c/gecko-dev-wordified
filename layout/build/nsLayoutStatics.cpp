e643b7dcd3f8af92db38c64248585ee8e7734070
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:24:52 +0000

    Backed out changeset 18403769ec19
    UltraBlame original commit: 8099980a73d596072695d3856cf784ba08562064

--- layout/build/nsLayoutStatics.cpp
+++ layout/build/nsLayoutStatics.cpp
@@ -855,7 +855,7 @@ h
 endif
 #
 ifdef
-MOZ_SYDNEYAUDIO
+MOZ_OGG
 #
 include
 "
@@ -1493,7 +1493,7 @@ InitMediaTypes
 endif
 #
 ifdef
-MOZ_SYDNEYAUDIO
+MOZ_OGG
 nsAudioStream
 :
 :
@@ -1892,7 +1892,7 @@ ShutdownMediaTypes
 endif
 #
 ifdef
-MOZ_SYDNEYAUDIO
+MOZ_OGG
 nsAudioStream
 :
 :
