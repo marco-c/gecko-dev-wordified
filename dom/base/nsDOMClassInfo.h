49482ef77e720600c85ef0e33c43a50f297e428f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 17:21:31 +0000

    Back out 236ba87c0a9f (bug 440046) and d3b7606d94a6:895a7efc978d (bug 683262) for b2g mochitest failures
    UltraBlame original commit: d72db91639711e5cfbd4a3b949e5c1029223933b

--- dom/base/nsDOMClassInfo.h
+++ dom/base/nsDOMClassInfo.h
@@ -209,17 +209,6 @@ nsIHTMLDocument
 class
 nsNPAPIPluginInstance
 ;
-class
-nsIDOMCrypto
-;
-#
-ifndef
-MOZ_DISABLE_CRYPTOLEGACY
-class
-nsIDOMCRMFObject
-;
-#
-endif
 struct
 nsDOMClassInfoData
 ;
