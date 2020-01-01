295a7ebd977e9a2f020820a02163966a7552e155
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 03:22:31 +0000

    Bug 546765 -  njcpudetect.h needs love for OSX-on-ARM (r=nnethercote)
    UltraBlame original commit: 0fdff97d515010a872cd9f0fd58ef9a5fe46a6da

--- js/src/nanojit/njcpudetect.h
+++ js/src/nanojit/njcpudetect.h
@@ -701,6 +701,13 @@ defined
 (
 __ARM_ARCH_7R__
 )
+|
+|
+\
+defined
+(
+_ARM_ARCH_7
+)
 #
 define
 NJ_COMPILER_ARM_ARCH
@@ -745,6 +752,13 @@ __ARM_ARCH_6ZK__
 defined
 (
 __ARM_ARCH_6M__
+)
+|
+|
+\
+defined
+(
+_ARM_ARCH_6
 )
 #
 define
