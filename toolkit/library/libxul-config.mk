38aacc18898ae02c94518f06e3a30ce6c1d80328
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:20:30 +0000

    Bug 306907 - Build libsqlite as a standard name separate shared library.  Patch by Michael Wu <michael.wumozilla.com>, Robert Relyea <rrelyearedhat.com>, Ryan VanderMeulen <ryanvmgmail.com>, and Shawn Wilsher <meshawnwilsher.com>.  r=benjamin
    UltraBlame original commit: b6f6e946061b7ead5964c39a5ef8a5ce7dbe8303

--- toolkit/library/libxul-config.mk
+++ toolkit/library/libxul-config.mk
@@ -194,6 +194,15 @@ s
 )
 :
 #
+Shawn
+Wilsher
+<
+me
+shawnwilsher
+.
+com
+>
+#
 #
 Alternatively
 the
@@ -840,6 +849,10 @@ COMPONENT_LIBS
 +
 =
 storagecomps
+EXTRA_DSO_LIBS
++
+=
+sqlite3
 endif
 ifdef
 MOZ_PLACES
