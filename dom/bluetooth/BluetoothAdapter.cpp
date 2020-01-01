5ed70380f76f617fd13a610b584a977710ea9f2a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 12:53:47 +0000

    Bug 779702: Fixing Device and UUID array assignments in BluetoothAdapter
    UltraBlame original commit: be7c3fa1a34d9dd373f3749e1347b625ceac2b92

--- dom/bluetooth/BluetoothAdapter.cpp
+++ dom/bluetooth/BluetoothAdapter.cpp
@@ -937,7 +937,7 @@ sc
 GetNativeGlobal
 (
 )
-mUuids
+mDeviceAddresses
 &
 mJsDeviceAddresses
 )
@@ -956,7 +956,7 @@ NS_WARNING
 Cannot
 set
 JS
-Devices
+Device
 Addresses
 object
 !
@@ -1888,7 +1888,7 @@ else
 NS_WARNING
 (
 "
-UUIDs
+Devices
 not
 yet
 set
