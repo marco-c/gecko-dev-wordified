abc282ce2603aef755bd86e93835fcb84cd01daa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 21:12:18 +0000

    Bug 886903 - Send correct UserAgent data for content process. r=jdm
    UltraBlame original commit: 0ded4854067b24c0c2019781eb41bbc6981f0ba6

--- dom/ipc/ContentChild.h
+++ dom/ipc/ContentChild.h
@@ -318,6 +318,12 @@ version
 nsCString
 buildID
 ;
+nsCString
+name
+;
+nsCString
+UAName
+;
 }
 ;
 bool
@@ -992,6 +998,14 @@ const
 nsCString
 &
 buildID
+const
+nsCString
+&
+name
+const
+nsCString
+&
+UAName
 )
 ;
 virtual
