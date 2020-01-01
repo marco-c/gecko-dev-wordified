f7dc95846f9426537d6b91c577e67bc54fcccced
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 19:27:14 +0000

    Bug 862986 - Part 2a: Migrate PROGRAM from Makefile.in to moz.build. r=gps
    UltraBlame original commit: 557f1d26fb712722030373ff369d4dba8477260e

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -69,11 +69,13 @@ ConfigFileSubstitution
     
 DirectoryTraversal
     
-VariablePassthru
-    
 Exports
     
+Program
+    
 ReaderSummary
+    
+VariablePassthru
     
 XpcshellManifests
 )
@@ -632,6 +634,39 @@ Exports
 (
 sandbox
 exports
+)
+        
+program
+=
+sandbox
+.
+get
+(
+'
+PROGRAM
+'
+)
+        
+if
+program
+:
+            
+yield
+Program
+(
+sandbox
+program
+sandbox
+[
+'
+CONFIG
+'
+]
+[
+'
+BIN_SUFFIX
+'
+]
 )
         
 for
