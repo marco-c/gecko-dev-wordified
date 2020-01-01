4d0a0ffabd485af8ca3e2340d0f406d58ca4d4c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 02:26:38 +0000

    Bug 499262 - Remove Metoworks Code for OS 9. r+sr=bsmedberg
    UltraBlame original commit: e5f0916b32d3ecacbb14486b4e1885cc24b6ceb5

--- xpcom/tests/SizeTest02.cpp
+++ xpcom/tests/SizeTest02.cpp
@@ -24,15 +24,6 @@ nsString
 .
 h
 "
-#
-ifdef
-__MWERKS__
-#
-pragma
-exceptions
-off
-#
-endif
 NS_DEF_PTR
 (
 nsIDOMNode
--- xpcom/tests/SizeTest05.cpp
+++ xpcom/tests/SizeTest05.cpp
@@ -17,15 +17,6 @@ nsCOMPtr
 .
 h
 "
-#
-ifdef
-__MWERKS__
-#
-pragma
-exceptions
-off
-#
-endif
 NS_DEF_PTR
 (
 nsIDOMNode
