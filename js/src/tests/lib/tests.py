09722eb9369d53b26cfa162acccf656b5c31cbbc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 12:54:26 +0000

    Bug 777589 - Fix regress-50447-1.js, don't work around it in the harness; r=sfink
    UltraBlame original commit: 01b9cf7935beeae81abd73ed61e6261af5477575

--- js/src/tests/lib/tests.py
+++ js/src/tests/lib/tests.py
@@ -662,24 +662,6 @@ d
 '
 ]
         
-#
-There
-is
-a
-test
-that
-requires
-the
-path
-to
-start
-with
-'
-.
-/
-'
-.
-        
 cmd
 +
 =
@@ -688,11 +670,6 @@ cmd
 -
 f
 '
-'
-.
-/
-'
-+
 self
 .
 path
