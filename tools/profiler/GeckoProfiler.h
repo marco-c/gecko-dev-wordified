9e205f23eb5ca8efebb1a3c0db1a39cd43373748
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 20:21:49 +0000

    Bug 873914 - Allow selecting profiled thread. r=snorp
    UltraBlame original commit: 090ebbb90a7fe87fe4b0b8da1ad2942784c585b5

--- tools/profiler/GeckoProfiler.h
+++ tools/profiler/GeckoProfiler.h
@@ -1054,6 +1054,13 @@ char
 aFeatures
 uint32_t
 aFeatureCount
+const
+char
+*
+*
+aThreadNameFilters
+uint32_t
+aFilterCount
 )
 {
 }
--- tools/profiler/GeckoProfilerFunc.h
+++ tools/profiler/GeckoProfilerFunc.h
@@ -225,6 +225,13 @@ char
 aFeatures
 uint32_t
 aFeatureCount
+const
+char
+*
+*
+aThreadNameFilters
+uint32_t
+aFilterCount
 )
 ;
 void
