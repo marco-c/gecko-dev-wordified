a311f70960f639403a3a3db8c8ee62b49810bcbe
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 02:01:14 +0000

    maintain globalShape in VMFragment only (486049, r=graydon).
    UltraBlame original commit: 246bb72c6c113a50c1ecfa012225fe17ffe4f479

--- js/src/jstracer.h
+++ js/src/jstracer.h
@@ -1746,9 +1746,6 @@ typeMap
 unsigned
 nStackTypes
 ;
-uint32
-globalShape
-;
 SlotList
 *
 globalSlots
@@ -1847,8 +1844,6 @@ nanojit
 Fragment
 *
 _fragment
-uint32
-_globalShape
 SlotList
 *
 _globalSlots
@@ -1877,10 +1872,6 @@ maxCallDepth
 nStackTypes
 (
 0
-)
-globalShape
-(
-_globalShape
 )
 globalSlots
 (
@@ -3916,10 +3907,7 @@ nanojit
 Fragmento
 *
 fragmento
-nanojit
-:
-:
-Fragment
+VMFragment
 *
 peer_root
 )
