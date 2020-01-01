52959591dcbfe65053b658bb5ddd79886e70fbb3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 01:52:48 +0000

    Bug 465158 - Minefield Nightly fails to initiate dial-up login when using internet connection sharing; r=cbiesinger
    UltraBlame original commit: fe7134fc65ec38443fb7b7d74bb4f7fc1a01dc6e

--- netwerk/system/win32/nsNotifyAddrListener.h
+++ netwerk/system/win32/nsNotifyAddrListener.h
@@ -451,6 +451,13 @@ h
 >
 #
 include
+<
+winsock2
+.
+h
+>
+#
+include
 "
 nsINetworkLinkService
 .
@@ -607,6 +614,13 @@ DWORD
 CheckAdaptersAddresses
 (
 void
+)
+;
+BOOL
+CheckAddressIsGateway
+(
+LPSOCKADDR
+aAddress
 )
 ;
 void
