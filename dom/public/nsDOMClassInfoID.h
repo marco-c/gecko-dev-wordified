fe95552ed13da6012a9226dd53203cca6ff86689
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:29:42 +0000

    Fixing bug 400366. Prefix 'Prototype' and 'Constructor' with 'DOM' to avoid conflicting with code on the web. r+sr=petervpropagandism.org
    UltraBlame original commit: c83eaeac59430b5a6cfd302011e8c7d1bf5a22c8

--- dom/public/nsDOMClassInfoID.h
+++ dom/public/nsDOMClassInfoID.h
@@ -483,8 +483,8 @@ eDOMClassInfo_MimeTypeArray_id
 eDOMClassInfo_BarProp_id
 eDOMClassInfo_History_id
 eDOMClassInfo_Screen_id
-eDOMClassInfo_Prototype_id
-eDOMClassInfo_Constructor_id
+eDOMClassInfo_DOMPrototype_id
+eDOMClassInfo_DOMConstructor_id
 /
 /
 Core
