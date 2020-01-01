830432272d5df86b9bd78bbbea00b8fa430135e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 00:58:58 +0000

    Bug 444492 - "Cairo _get_system_quality() function stubbed out in WinCE" (_get_system_quality() Returns DEFAULT_QUALITY on WinCE) [r=vlad]
    UltraBlame original commit: 59a60321d0d6b3ea90afceb592c1badfe48ce42c

--- gfx/cairo/cairo/src/cairo-win32-font.c
+++ gfx/cairo/cairo/src/cairo-win32-font.c
@@ -1339,6 +1339,9 @@ _get_system_quality
 void
 )
 {
+#
+ifndef
+WINCE
 BOOL
 font_smoothing
 ;
@@ -1426,6 +1429,13 @@ return
 DEFAULT_QUALITY
 ;
 }
+#
+else
+return
+DEFAULT_QUALITY
+;
+#
+endif
 }
 /
 *
