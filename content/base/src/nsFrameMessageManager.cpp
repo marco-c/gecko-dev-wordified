c2fa472ac7b74f424e9d0e4f1973add5b5417022
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 08:10:07 +0000

    Backed out changeset 3d3f25a67e24
    UltraBlame original commit: b63623583daf64c29701e3f09fd270752ed07a53

--- content/base/src/nsFrameMessageManager.cpp
+++ content/base/src/nsFrameMessageManager.cpp
@@ -2601,7 +2601,7 @@ JSVAL_VOID
 ;
 if
 (
-JS_ObjectIsCallable
+JS_ObjectIsFunction
 (
 ctx
 object
@@ -2755,7 +2755,7 @@ funval
 ;
 NS_ENSURE_STATE
 (
-JS_ObjectIsCallable
+JS_ObjectIsFunction
 (
 ctx
 funobject
