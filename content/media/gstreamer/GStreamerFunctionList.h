f3f05a3b0fe75835192b1326249f71b6aa8fe287
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 20:24:04 +0000

    Backed out changeset 9173dcf026ee (bug 882567) because of mochitest-1 timeouts on Linux
    Landed on a CLOSED TREE
    
    UltraBlame original commit: 822b350df442906777ba953ba8ef25738ffb05b8

--- content/media/gstreamer/GStreamerFunctionList.h
+++ content/media/gstreamer/GStreamerFunctionList.h
@@ -189,11 +189,6 @@ gst_buffer_new_and_alloc
 GST_FUNC
 (
 LIBGSTREAMER
-gst_bus_set_sync_handler
-)
-GST_FUNC
-(
-LIBGSTREAMER
 gst_bus_timed_pop_filtered
 )
 GST_FUNC
@@ -330,11 +325,6 @@ GST_FUNC
 (
 LIBGSTREAMER
 gst_message_parse_error
-)
-GST_FUNC
-(
-LIBGSTREAMER
-gst_message_type_get_name
 )
 GST_FUNC
 (
