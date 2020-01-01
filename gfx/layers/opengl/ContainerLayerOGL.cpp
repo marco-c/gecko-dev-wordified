f6cb9a61b9a0192228e39d27dd267420a2c19e4c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 17:46:40 +0000

    Bug 848023 - Use the same adjusted max texture size everywhere. r=bjacob
    UltraBlame original commit: bc115ba15bb61ddef8f79c2c00c7d7fe58592c61

--- gfx/layers/opengl/ContainerLayerOGL.cpp
+++ gfx/layers/opengl/ContainerLayerOGL.cpp
@@ -1334,7 +1334,7 @@ discussion
 .
 GLint
 maxTexSize
-;
+=
 aContainer
 -
 >
@@ -1343,11 +1343,8 @@ gl
 )
 -
 >
-fGetIntegerv
+GetMaxTextureSize
 (
-LOCAL_GL_MAX_TEXTURE_SIZE
-&
-maxTexSize
 )
 ;
 framebufferRect
