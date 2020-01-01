e92d1a9ca97958b99509b4696871d4d85a00683e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-01 21:42:41 +0000

    Bug 893117: Remove nsIDOMHTMLFontElement r=bz
    UltraBlame original commit: 1ea6c2e6a468b92314aca449165d0c384af00b00

--- content/html/content/src/HTMLFontElement.h
+++ content/html/content/src/HTMLFontElement.h
@@ -115,13 +115,6 @@ nsGenericHTMLElement
 .
 h
 "
-#
-include
-"
-nsIDOMHTMLFontElement
-.
-h
-"
 namespace
 mozilla
 {
@@ -135,7 +128,7 @@ MOZ_FINAL
 public
 nsGenericHTMLElement
 public
-nsIDOMHTMLFontElement
+nsIDOMHTMLElement
 {
 public
 :
@@ -176,10 +169,6 @@ NS_FORWARD_NSIDOMELEMENT_TO_GENERIC
 /
 nsIDOMHTMLElement
 NS_FORWARD_NSIDOMHTMLELEMENT_TO_GENERIC
-/
-/
-nsIDOMHTMLFontElement
-NS_DECL_NSIDOMHTMLFONTELEMENT
 void
 GetColor
 (
