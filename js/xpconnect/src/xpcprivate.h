2c6e456aaf931d4f3790e4eec096785aff204d30
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 11:33:09 +0000

    Bug 937157, part 3 - Remove XPC_ASSERT_CLASSINFO_CLAIMS. r=bholley
    UltraBlame original commit: 6d16fdc91ac7a044b693a9d3d472b5a4603de294

--- js/xpconnect/src/xpcprivate.h
+++ js/xpconnect/src/xpcprivate.h
@@ -1572,17 +1572,6 @@ XPC_REPORT_SHADOWED_WRAPPED_NATIVE_MEMBERS
 define
 XPC_CHECK_CLASSINFO_CLAIMS
 #
-if
-defined
-(
-DEBUG_jst
-)
-#
-define
-XPC_ASSERT_CLASSINFO_CLAIMS
-#
-endif
-#
 endif
 /
 *
