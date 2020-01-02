0cfaff2296b96b754802c18895de755c4e762bcd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 16:57:23 +0000

    Bug 985954 - Remove deprecated promise.js usage in the testing framework. r=ted
    UltraBlame original commit: 8db4e655f7b925459c3283ac3432757b2cd7f857

--- testing/mochitest/browser-test.js
+++ testing/mochitest/browser-test.js
@@ -925,15 +925,9 @@ gre
 /
 modules
 /
-commonjs
-/
-sdk
-/
-core
-/
-promise
+Promise
 .
-js
+jsm
 "
 null
 )
--- testing/mochitest/tests/browser/browser_getTestFile.js
+++ testing/mochitest/tests/browser/browser_getTestFile.js
@@ -23,15 +23,9 @@ gre
 /
 modules
 /
-commonjs
-/
-sdk
-/
-core
-/
-promise
+Promise
 .
-js
+jsm
 "
 )
 ;
