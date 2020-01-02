525d22b173089248b4dbc6f5507bfe7a5a115ce0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 15:18:17 +0000

    Backed out changeset 2b2f69d46d30 (bug 975182) for SM(arm) failures. CLOSED TREE
    UltraBlame original commit: 048533052033a4939f64616af3857d71947a41d8

--- js/src/jit/AsmJSSignalHandlers.cpp
+++ js/src/jit/AsmJSSignalHandlers.cpp
@@ -8837,7 +8837,7 @@ module
 functionBytes
 (
 )
-PAGE_READWRITE
+PAGE_NOACCESS
 &
 oldProtect
 )
@@ -8866,9 +8866,7 @@ module
 functionBytes
 (
 )
-PROT_READ
-|
-PROT_WRITE
+PROT_NONE
 )
 )
 MOZ_CRASH
