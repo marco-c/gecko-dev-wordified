ee61bcd87df25178ccb91711e5b57a6ec356e677
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 12:02:09 +0000

    Bug 937434: Because Android API-AudioSystem::setStreamVolumeIndex() is different when Android Version more than 16, we have to provide A2DP device information to let the volume of the A2DP devices can be changed r=mchen
    UltraBlame original commit: a5585091e1e2e50a8d6a96f24d1ffba2a4351ad9

--- dom/system/gonk/AudioManager.h
+++ dom/system/gonk/AudioManager.h
@@ -330,10 +330,7 @@ mCurrentStreamVolumeTbl
 AUDIO_STREAM_CNT
 ]
 ;
-android
-:
-:
-status_t
+nsresult
 SetStreamVolumeIndex
 (
 int32_t
@@ -342,10 +339,7 @@ int32_t
 aIndex
 )
 ;
-android
-:
-:
-status_t
+nsresult
 GetStreamVolumeIndex
 (
 int32_t
