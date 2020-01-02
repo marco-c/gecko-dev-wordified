f8c264223d1f92aed08770b983bbf30763e8d034
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 10:32:57 +0000

    Bug 913985: Part 6 - Remove redundant AndroidBridge::Bridge() checks. r=kats; DONTBUILD
    UltraBlame original commit: de21920d2b8ebd5bc369081140b5cf9b65786d95

--- uriloader/exthandler/android/nsExternalSharingAppService.cpp
+++ uriloader/exthandler/android/nsExternalSharingAppService.cpp
@@ -175,15 +175,6 @@ EmptyString
 (
 )
 ;
-if
-(
-AndroidBridge
-:
-:
-Bridge
-(
-)
-)
 return
 GeckoAppShell
 :
@@ -200,9 +191,6 @@ title
 ?
 NS_OK
 :
-NS_ERROR_FAILURE
-;
-return
 NS_ERROR_FAILURE
 ;
 }
