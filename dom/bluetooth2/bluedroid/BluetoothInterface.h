7dd827dcca9f9fc07fab691be0c333810dc3bc02
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 21:48:54 +0000

    Bug 1036977: Convert Bluedroid Socket's Listen method to asynchronous design (under bluetooth2/), r=btian
    UltraBlame original commit: 13fce7913cc4a5e2ade4712cbf8ebd7465651b0c

--- dom/bluetooth2/bluedroid/BluetoothInterface.h
+++ dom/bluetooth2/bluedroid/BluetoothInterface.h
@@ -266,7 +266,7 @@ is
 handled
 by
 BluetoothInterface
-bt_status_t
+void
 Listen
 (
 btsock_type_t
@@ -282,10 +282,10 @@ aServiceUuid
 int
 aChannel
 int
-&
-aSockFd
-int
 aFlags
+BluetoothSocketResultHandler
+*
+aRes
 )
 ;
 bt_status_t
