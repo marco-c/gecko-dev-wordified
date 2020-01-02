fd8708dccc5138122492fde018f2886de718fb10
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 16:42:35 +0000

    Backed out changeset 18f95fb1d866 (bug 948901) for breaking OSX m-oth
    UltraBlame original commit: 75a932c24023f4d60620125c6b80fba06ce43c20

--- netwerk/streamconv/converters/nsIndexedToHTML.h
+++ netwerk/streamconv/converters/nsIndexedToHTML.h
@@ -213,13 +213,13 @@ FormatSizeString
 (
 int64_t
 inSize
-nsCString
+nsString
 &
 outSizeString
 )
 ;
 nsresult
-SendToListener
+FormatInputStream
 (
 nsIRequest
 *
@@ -228,7 +228,7 @@ nsISupports
 *
 aContext
 const
-nsACString
+nsAString
 &
 aBuffer
 )
@@ -249,7 +249,7 @@ request
 nsISupports
 *
 aContext
-nsCString
+nsString
 &
 aBuffer
 )
@@ -313,6 +313,9 @@ lines
 .
 bool
 mExpectAbsLoc
+;
+nsString
+mEscapedEllipsis
 ;
 }
 ;
