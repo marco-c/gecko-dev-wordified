17e3d53cedf9aa823309d229b16dcc78417c7fd3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 13:45:49 +0000

    Bug 959570 - Don't draw rounded corners and a highlight line in fullscreen mode. r=smichaud
    UltraBlame original commit: 6bf3e0350c4a3c760c81b852460cebd8bac7faac

--- widget/cocoa/nsChildView.h
+++ widget/cocoa/nsChildView.h
@@ -1452,6 +1452,16 @@ typedef
 NSUInteger
 NSEventPhase
 ;
+enum
+{
+NSFullScreenWindowMask
+=
+1
+<
+<
+14
+}
+;
 interface
 NSWindow
 (
@@ -4492,6 +4502,9 @@ mDevPixelCornerRadius
 ;
 bool
 mIsCoveringTitlebar
+;
+bool
+mIsFullscreen
 ;
 nsIntRect
 mTitlebarRect
