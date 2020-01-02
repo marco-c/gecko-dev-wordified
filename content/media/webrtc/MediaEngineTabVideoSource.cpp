51c3e57b17b6d35bd16fbe2a113cfbb584548154
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 17:24:02 +0000

    Bug 694814: Patch 4 - Add audio playout delay config var r=padenot
    UltraBlame original commit: 263d6539e0a906efae53987ab4b72f4ddab4e526

--- content/media/webrtc/MediaEngineTabVideoSource.cpp
+++ content/media/webrtc/MediaEngineTabVideoSource.cpp
@@ -1694,6 +1694,7 @@ bool
 uint32_t
 bool
 uint32_t
+int32_t
 )
 {
 return
--- dom/media/MediaManager.h
+++ dom/media/MediaManager.h
@@ -730,6 +730,8 @@ bool
 aNoiseOn
 uint32_t
 aNoise
+int32_t
+aPlayoutDelay
 )
 {
 if
@@ -780,6 +782,7 @@ aAgcOn
 aAGC
 aNoiseOn
 aNoise
+aPlayoutDelay
 )
 NS_DISPATCH_NORMAL
 )
