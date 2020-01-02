dbdedb78773318bac8497581eab48ab47bdcac7a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 16:24:00 +0000

    Bug 984819 - Remove deprecated promise.js usage in Firefox for Android tests. r=bnicholson
    UltraBlame original commit: 953eea0ac46c13560bc740722dccce110d16fe67

--- mobile/android/base/tests/testMozPay.js
+++ mobile/android/base/tests/testMozPay.js
@@ -157,15 +157,9 @@ gre
 /
 modules
 /
-commonjs
-/
-sdk
-/
-core
-/
-promise
+Promise
 .
-js
+jsm
 "
 )
 ;
--- mobile/android/base/tests/testSharedPreferences.js
+++ mobile/android/base/tests/testSharedPreferences.js
@@ -136,15 +136,9 @@ gre
 /
 modules
 /
-commonjs
-/
-sdk
-/
-core
-/
-promise
+Promise
 .
-js
+jsm
 "
 )
 ;
