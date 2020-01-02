9887bc108177607f32ba94430031321db3c4c9ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 10:29:50 +0000

    Bug 884708 - Port GTK2 to GTK3 - build failures r=karlt
    UltraBlame original commit: df11d218b99009df117bf7f24102926e92460aa3

--- toolkit/components/jsdownloads/src/DownloadPlatform.cpp
+++ toolkit/components/jsdownloads/src/DownloadPlatform.cpp
@@ -159,7 +159,7 @@ h
 endif
 #
 ifdef
-MOZ_WIDGET_GTK2
+MOZ_WIDGET_GTK
 #
 include
 <
@@ -222,7 +222,7 @@ gDownloadPlatformService
 if
 defined
 (
-MOZ_WIDGET_GTK2
+MOZ_WIDGET_GTK
 )
 g_type_init
 (
@@ -348,7 +348,7 @@ MOZ_WIDGET_ANDROID
 |
 defined
 (
-MOZ_WIDGET_GTK2
+MOZ_WIDGET_GTK
 )
 nsAutoString
 path
@@ -380,7 +380,7 @@ XP_WIN
 |
 defined
 (
-MOZ_WIDGET_GTK2
+MOZ_WIDGET_GTK
 )
 /
 /
@@ -449,7 +449,7 @@ get
 elif
 defined
 (
-MOZ_WIDGET_GTK2
+MOZ_WIDGET_GTK
 )
 GtkRecentManager
 *
