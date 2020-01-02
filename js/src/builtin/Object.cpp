547fdeb765b98e9c45e92eb4df28dda84e359d52
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 23:00:44 +0000

    Bug 937855 - Implement Object.assign.  Patch is a tag-team effort with till.  Also add tests: most courtesy of André Bargull, some from me, some suggested by jwalden.  r=jwalden
    UltraBlame original commit: 53769e48d35bcdb4b69c97f64815d20fe3fcdda6

--- js/src/builtin/Object.cpp
+++ js/src/builtin/Object.cpp
@@ -7918,6 +7918,17 @@ object_static_selfhosted_methods
 ]
 =
 {
+JS_SELF_HOSTED_FN
+(
+"
+assign
+"
+"
+ObjectStaticAssign
+"
+2
+0
+)
 JS_FS_END
 }
 ;
--- js/src/builtin/Utilities.js
+++ js/src/builtin/Utilities.js
@@ -520,6 +520,13 @@ Object
 getPrototypeOf
 ;
 var
+std_Object_getOwnPropertyDescriptor
+=
+Object
+.
+getOwnPropertyDescriptor
+;
+var
 std_RegExp_test
 =
 RegExp
