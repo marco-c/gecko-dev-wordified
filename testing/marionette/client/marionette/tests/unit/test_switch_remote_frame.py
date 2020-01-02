f29cc34600947cd0105488bebb1bd730dcfd494d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-02 17:56:52 +0000

    Backed out changeset 6bb03867a3ad (bug 929175)
    UltraBlame original commit: 83b46f317e111e08a2f930ba54b0dcf737592b7f

--- testing/marionette/client/marionette/tests/unit/test_switch_remote_frame.py
+++ testing/marionette/client/marionette/tests/unit/test_switch_remote_frame.py
@@ -398,29 +398,15 @@ html
 )
 )
         
-remote_iframe
-=
-self
-.
-marionette
-.
-find_element
-(
-"
-id
-"
-"
-remote_iframe
-"
-)
-        
 self
 .
 marionette
 .
 switch_to_frame
 (
+"
 remote_iframe
+"
 )
         
 main_process
@@ -643,20 +629,9 @@ marionette
 .
 switch_to_frame
 (
-self
-.
-marionette
-.
-find_element
-(
-"
-id
-"
-                                                                     
 "
 remote_iframe
 "
-)
 )
         
 main_process
@@ -737,20 +712,9 @@ marionette
 .
 switch_to_frame
 (
-self
-.
-marionette
-.
-find_element
-(
-"
-id
-"
-                                                                     
 "
 remote_iframe
 "
-)
 )
         
 main_process
