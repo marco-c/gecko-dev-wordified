b569ae23bb2e5a9ef7086bccf9349f8062080a67
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 09:29:37 +0000

    Bug 1075670 - Use MozUpdateWindowPos event to notify content process of window move. r=smaug
    TabParents now register for the MozUpdateWindowPos event on the chrome TopWindowRoot. When the window is moved, the OS widget calls WindowMoved on its listener (the nsWebShellWindow), which sends a MozUpdateWindowPos event.
    
    UltraBlame original commit: e6d6f0c1113315c9eb00527c0f1a87cdd21a35d0

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -244,6 +244,13 @@ TypeDecls
 .
 h
 "
+#
+include
+"
+nsIDOMEventListener
+.
+h
+"
 class
 nsFrameLoader
 ;
@@ -322,6 +329,8 @@ TabParent
 :
 public
 PBrowserParent
+public
+nsIDOMEventListener
 public
 nsITabParent
 public
@@ -495,6 +504,11 @@ mBrowserDOMWindow
 aBrowserDOMWindow
 ;
 }
+/
+/
+nsIDOMEventListener
+interfaces
+NS_DECL_NSIDOMEVENTLISTENER
 already_AddRefed
 <
 nsILoadContext
@@ -2495,6 +2509,9 @@ mDimensions
 ;
 ScreenOrientation
 mOrientation
+;
+nsIntPoint
+mChromeDisp
 ;
 float
 mDPI
