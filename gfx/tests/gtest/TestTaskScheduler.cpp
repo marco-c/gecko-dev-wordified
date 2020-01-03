ee1df48d04f3e3739e67e4b4bd5679a539d57a7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:02:57 +0000

    Bug 1083101 - Win32 implementation of the TaskScheduler. r=jrmuizel
    UltraBlame original commit: 051166e3d6e4fd7413dee59206f0d64d93a569f3

--- gfx/tests/gtest/TestTaskScheduler.cpp
+++ gfx/tests/gtest/TestTaskScheduler.cpp
@@ -47,9 +47,6 @@ zero
 *
 /
 #
-ifndef
-WIN32
-#
 include
 "
 gtest
@@ -79,6 +76,9 @@ TaskScheduler
 h
 "
 #
+ifndef
+WIN32
+#
 include
 <
 pthread
@@ -92,6 +92,8 @@ sched
 .
 h
 >
+#
+endif
 #
 include
 <
@@ -149,6 +151,9 @@ MaybeYieldThread
 (
 )
 {
+#
+ifndef
+WIN32
 if
 (
 rand
@@ -166,6 +171,8 @@ sched_yield
 )
 ;
 }
+#
+endif
 }
 /
 /
@@ -1533,5 +1540,3 @@ ShutDown
 }
 }
 }
-#
-endif
