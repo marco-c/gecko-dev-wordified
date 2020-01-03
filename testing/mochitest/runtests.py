2eaa0365431d68fb4dc9c65f3e6abbb3d056a8fa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 04:42:16 +0000

    Bug 1057512 - hotfix - disable m-e10s(dt) run-by-dir as it fails. r=dminor
    UltraBlame original commit: 34594cb8b2e4a356a0a71b6685b198c70b4c4f0e

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -14664,10 +14664,33 @@ making
 this
 default
     
+#
+We
+need
+to
+ignore
+dt
+*
+jobs
+on
+e10s
+runs
+    
 if
 options
 .
 browserChrome
+and
+not
+(
+options
+.
+subsuite
+and
+options
+.
+e10s
+)
 :
       
 options
