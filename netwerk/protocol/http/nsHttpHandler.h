1dd62a51f55520782c9c6d3da3a86f7a1ec18473
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 08:21:09 +0000

    bug 1102923 - debug code to get a useful stack during test_spdy.js fail r=hurley
    UltraBlame original commit: 9af8e51bde96bdbab715670952854029c9660526

--- netwerk/protocol/http/nsHttpHandler.h
+++ netwerk/protocol/http/nsHttpHandler.h
@@ -842,6 +842,15 @@ return
 mEnableAltSvcOE
 ;
 }
+bool
+Debug1102923
+(
+)
+{
+return
+mDebug1102923
+;
+}
 uint32_t
 ConnectTimeout
 (
@@ -2600,6 +2609,11 @@ mEnableAltSvc
 ;
 uint32_t
 mEnableAltSvcOE
+:
+1
+;
+uint32_t
+mDebug1102923
 :
 1
 ;
