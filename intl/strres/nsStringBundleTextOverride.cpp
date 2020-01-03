14a598483898120ce3ab6fd7ef8e8b574c38599b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:46:51 +0000

    Bug 1205153 - Use channel->Open2() in intl/strres/nsStringBundleTextOverride.cpp (r=sicking)
    UltraBlame original commit: 7ac8d5a59602fb39f491209226b04eb852ee1e8d

--- intl/strres/nsStringBundleTextOverride.cpp
+++ intl/strres/nsStringBundleTextOverride.cpp
@@ -853,7 +853,7 @@ GetSystemPrincipal
 nsILoadInfo
 :
 :
-SEC_NORMAL
+SEC_ALLOW_CROSS_ORIGIN_DATA_IS_NULL
 nsIContentPolicy
 :
 :
@@ -877,7 +877,7 @@ rv
 channel
 -
 >
-Open
+Open2
 (
 getter_AddRefs
 (
