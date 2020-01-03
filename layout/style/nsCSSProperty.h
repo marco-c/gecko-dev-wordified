23e8de76f9dbbf614ec4677d63bfc8b61fa80449
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 07:21:25 +0000

    Bug 1120284 - Part 5: Support logical axis properties. r=dbaron
    UltraBlame original commit: 5e1ea6b6bed8b95bfff00d36a6e9911fc976f7aa

--- layout/style/nsCSSProperty.h
+++ layout/style/nsCSSProperty.h
@@ -497,6 +497,17 @@ eCSSPropertyLogicalGroup_UNKNOWN
 1
 #
 define
+CSS_PROP_LOGICAL_GROUP_AXIS
+(
+name_
+)
+\
+eCSSPropertyLogicalGroup_
+#
+#
+name_
+#
+define
 CSS_PROP_LOGICAL_GROUP_BOX
 (
 name_
@@ -530,6 +541,9 @@ CSS_PROP_LOGICAL_GROUP_SHORTHAND
 #
 undef
 CSS_PROP_LOGICAL_GROUP_BOX
+#
+undef
+CSS_PROP_LOGICAL_GROUP_AXIS
 eCSSPropertyLogicalGroup_COUNT
 }
 ;
