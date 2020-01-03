5f635da644a59ae94fa1fd0835d9c1b7c0f0a852
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 21:27:33 +0000

    Bug 1183232 - Allow selecting tests and suites with just a tag from mach try. r=ahal,gps
    UltraBlame original commit: cab252737a1240f3b6c4e248e8c9a2a7884ff36c

--- testing/tools/autotry/autotry.py
+++ testing/tools/autotry/autotry.py
@@ -209,30 +209,22 @@ _use_git
 True
     
 def
-manifests_by_flavor
+resolve_manifests
 (
 self
 paths
-)
-:
-        
-manifests_by_flavor
 =
-defaultdict
-(
-set
+None
+tags
+=
+None
 )
-        
-if
-not
-paths
 :
-            
-return
-dict
-(
-manifests_by_flavor
-)
+        
+assert
+tags
+or
+paths
         
 tests
 =
@@ -244,6 +236,10 @@ resolver
 .
 resolve_tests
 (
+tags
+=
+tags
+                                                 
 paths
 =
 paths
@@ -256,6 +252,13 @@ mach_context
 .
 cwd
 )
+)
+        
+manifests_by_flavor
+=
+defaultdict
+(
+set
 )
         
 for
