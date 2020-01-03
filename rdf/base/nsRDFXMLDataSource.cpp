c3c4202179878ff7bf67a97658b8b6a5c7faa00a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:31:50 +0000

    Bug 1197918 - Use channel->Open2() in rdf/base/nsRDFXMLDataSource.cpp (r=sicking)
    UltraBlame original commit: bdb5b24b1f4e39412022f62354e6de3911cb34eb

--- rdf/base/nsRDFXMLDataSource.cpp
+++ rdf/base/nsRDFXMLDataSource.cpp
@@ -2492,7 +2492,7 @@ GetSystemPrincipal
 nsILoadInfo
 :
 :
-SEC_NORMAL
+SEC_ALLOW_CROSS_ORIGIN_DATA_IS_NULL
 nsIContentPolicy
 :
 :
@@ -2520,7 +2520,7 @@ rv
 channel
 -
 >
-Open
+Open2
 (
 getter_AddRefs
 (
