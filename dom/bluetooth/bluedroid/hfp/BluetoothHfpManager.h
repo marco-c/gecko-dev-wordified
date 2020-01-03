22fdfb1823b75050eb72a3418e4ae7f169a1ee2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 00:11:01 +0000

    Bug 1061126: Fix constants in Bluedroid HFP manager, r=shuang
    This patch replaces the remaining Bluedroid constants from Bluedroid's
    |BluetoothHfpManager| with Gecko constants.
    
    UltraBlame original commit: ad902f1fd574941061127da71d3ee2fafc0df79f

--- dom/bluetooth/bluedroid/hfp/BluetoothHfpManager.h
+++ dom/bluetooth/bluedroid/hfp/BluetoothHfpManager.h
@@ -832,13 +832,13 @@ BluetoothHandsfreeAtResponse
 aResponseCode
 )
 ;
-int
+BluetoothHandsfreeConnectionState
 mConnectionState
 ;
-int
+BluetoothHandsfreeConnectionState
 mPrevConnectionState
 ;
-int
+BluetoothHandsfreeAudioState
 mAudioState
 ;
 /
