cbb3d17eb647ed173b69f7d4610d97f8f4b000a7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 21:19:40 +0000

    Bug 1186883 - Remove references to setup.sh for aurora android repacks; r=jlund
    UltraBlame original commit: 1ada2f33f5a889507d7d86b45bf0651e818bf858

--- testing/mozharness/configs/single_locale/mozilla-aurora_android-api-9.py
+++ testing/mozharness/configs/single_locale/mozilla-aurora_android-api-9.py
@@ -307,25 +307,6 @@ s
 "
 +
 MOZILLA_DIR
-        
-"
-bootstrap_cmd
-"
-:
-[
-"
-bash
-"
-"
--
-xe
-"
-"
-setup
-.
-sh
-"
-]
     
 }
     
--- testing/mozharness/configs/single_locale/mozilla-aurora_android.py
+++ testing/mozharness/configs/single_locale/mozilla-aurora_android.py
@@ -303,25 +303,6 @@ s
 "
 +
 MOZILLA_DIR
-        
-"
-bootstrap_cmd
-"
-:
-[
-"
-bash
-"
-"
--
-xe
-"
-"
-setup
-.
-sh
-"
-]
     
 }
     
