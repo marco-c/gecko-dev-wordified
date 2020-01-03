486823c4e26fecdab30b2b14f35898133651c3e7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 20:42:21 +0000

    Bug 1126228 - Directory (--directory) argument for |mach warnings-list| and |mach warnings-summary|. r=gps
    UltraBlame original commit: 8e3baacbe31ba2ec3f1b9cf65053ec16a154a8be

--- python/mozbuild/mozbuild/compilation/warnings.py
+++ python/mozbuild/mozbuild/compilation/warnings.py
@@ -83,6 +83,12 @@ mozbuild
 util
 import
 hash_file
+import
+mozpack
+.
+path
+as
+mozpath
 #
 Regular
 expression
@@ -1131,12 +1137,13 @@ warnings
 yield
 w
     
-property
-    
 def
 type_counts
 (
 self
+dirpath
+=
+None
 )
 :
         
@@ -1185,18 +1192,46 @@ warnings
 ]
 :
                 
-count
-=
-types
+if
+dirpath
+and
+not
+mozpath
 .
-get
+normsep
 (
+warning
+[
+'
+filename
+'
+]
+)
+.
+startswith
+(
+dirpath
+)
+:
+                    
+continue
+                
+flag
+=
 warning
 [
 '
 flag
 '
 ]
+                
+count
+=
+types
+.
+get
+(
+flag
 0
 )
                 
@@ -1207,12 +1242,7 @@ count
                 
 types
 [
-warning
-[
-'
 flag
-'
-]
 ]
 =
 count
