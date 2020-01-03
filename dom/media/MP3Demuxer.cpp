c30be5ef4a10c13bdc2782cdec28e0010c43b5ed
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:44:33 +0000

    Bug 1204757: P1. Update MediaDataDemuxer::Init() behavior. r=cpearce
    A MediaDataDemuxer is now not to resolve the init promise until it has all the metadata.
    Except MediaSource, all demuxers would be doing blocking read to scan for the metadata, and having partial metadata would be an error.
    For MediaSource, we pass the NotifyDataArrived message which will cause the MediaSourceDemuxer to re-attempt to search for the metadata.
    
    When used within MediaSource's TrackBuffersManager, a demuxer will never be created until we have received a complete init segment (this task is performed by the various ContainerParsers)
    
    UltraBlame original commit: a3a0f4f5b416f442f3f3fe654e92aa43513484e1

--- dom/media/MP3Demuxer.cpp
+++ dom/media/MP3Demuxer.cpp
@@ -370,7 +370,7 @@ CreateAndReject
 DemuxerFailureReason
 :
 :
-WAITING_FOR_DATA
+DEMUXER_ERROR
 __func__
 )
 ;
--- dom/media/fmp4/MP4Demuxer.cpp
+++ dom/media/fmp4/MP4Demuxer.cpp
@@ -636,7 +636,7 @@ CreateAndReject
 DemuxerFailureReason
 :
 :
-WAITING_FOR_DATA
+DEMUXER_ERROR
 __func__
 )
 ;
