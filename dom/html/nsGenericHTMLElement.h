2eb73cbeeb39bf0ea104a825e2b3fe9a05e2e56c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 16:31:06 +0000

    Bug 1162765 - Part 1: Convert nsGenericHTMLFormElement::mForm to a strong reference; r=mccr8
    UltraBlame original commit: c41fa4e8dfb24821a25f85a33914bb5d6afeca04

--- dom/html/nsGenericHTMLElement.h
+++ dom/html/nsGenericHTMLElement.h
@@ -7419,6 +7419,11 @@ NodeInfo
 aNodeInfo
 )
 ;
+NS_DECL_CYCLE_COLLECTION_CLASS_INHERITED
+(
+nsGenericHTMLFormElement
+nsGenericHTMLElement
+)
 NS_DECL_ISUPPORTS_INHERITED
 nsINode
 *
@@ -8245,6 +8250,8 @@ this
 control
 *
 /
+nsRefPtr
+<
 mozilla
 :
 :
@@ -8252,7 +8259,7 @@ dom
 :
 :
 HTMLFormElement
-*
+>
 mForm
 ;
 /
