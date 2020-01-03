cfeafd5c642df93f4b70adfaf4b8ea75e306881e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 09:25:42 +0000

    Backed out changeset bc2fdecaaec7 (bug 1129769) for hazard failures on a CLOSED TREE
    UltraBlame original commit: 63c2c0fe808e4b6d3a850267e411d024b2cc8055

--- tools/profiler/ProfileEntry.cpp
+++ tools/profiler/ProfileEntry.cpp
@@ -1404,11 +1404,6 @@ keyedBy
 keyedBy
 )
 ;
-if
-(
-name
-)
-{
 mWriter
 .
 NameValue
@@ -1419,7 +1414,6 @@ name
 name
 )
 ;
-}
 if
 (
 location
@@ -1435,15 +1429,6 @@ location
 location
 )
 ;
-}
-if
-(
-lineno
-!
-=
-UINT32_MAX
-)
-{
 mWriter
 .
 NameValue
