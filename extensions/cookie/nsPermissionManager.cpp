5aefca4269249dcdeb2d289e174dc974800d1cb6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 23:46:50 +0000

    Bug 1205151 - Use channel->Open2() in extensions/cookie/nsPermissionManager.cpp (r=sicking)
    UltraBlame original commit: 840452fa48bda27169b26a24ac484f99dc7bf34f

--- extensions/cookie/nsPermissionManager.cpp
+++ extensions/cookie/nsPermissionManager.cpp
@@ -16360,7 +16360,7 @@ GetSystemPrincipal
 nsILoadInfo
 :
 :
-SEC_NORMAL
+SEC_ALLOW_CROSS_ORIGIN_DATA_IS_NULL
 nsIContentPolicy
 :
 :
@@ -16384,7 +16384,7 @@ rv
 channel
 -
 >
-Open
+Open2
 (
 getter_AddRefs
 (
