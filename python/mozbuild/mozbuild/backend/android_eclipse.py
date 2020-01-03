182bd16dffbea72b01890d33dc50371ef1a8735f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 00:18:44 +0000

    Backed out changeset 65e611823b0f (bug 1062566) for checktest failures.
    UltraBlame original commit: 1d4e40dab49d4b9051e397bf8f2fc51a5c11a7f4

--- python/mozbuild/mozbuild/backend/android_eclipse.py
+++ python/mozbuild/mozbuild/backend/android_eclipse.py
@@ -2277,24 +2277,6 @@ ANDROID_TARGET_SDK
 '
 ]
         
-defines
-[
-'
-MOZ_ANDROID_MIN_SDK_VERSION
-'
-]
-=
-self
-.
-environment
-.
-defines
-[
-'
-MOZ_ANDROID_MIN_SDK_VERSION
-'
-]
-        
 copier
 =
 FileCopier
