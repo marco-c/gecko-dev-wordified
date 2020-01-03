94aa399aa8e6cdb0147dd7b1736d71e8ac5d408f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 08:56:23 +0000

    Bug 981869 - Blacklist crashy flump3dec gstreamer plugin - r=kinetik
    UltraBlame original commit: ab6236affc7299bfe0fb3e986af200b7a1276eea

--- dom/media/gstreamer/GStreamerFunctionList.h
+++ dom/media/gstreamer/GStreamerFunctionList.h
@@ -369,6 +369,11 @@ gst_plugin_feature_get_rank
 GST_FUNC
 (
 LIBGSTREAMER
+gst_plugin_feature_get_type
+)
+GST_FUNC
+(
+LIBGSTREAMER
 gst_registry_feature_filter
 )
 GST_FUNC
@@ -491,6 +496,11 @@ GST_FUNC
 (
 LIBGSTREAMER
 gst_pad_set_bufferalloc_function
+)
+GST_FUNC
+(
+LIBGSTREAMER
+gst_plugin_feature_get_name
 )
 GST_FUNC
 (
