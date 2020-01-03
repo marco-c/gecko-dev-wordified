f008963a7704a7c3c071cf21cc926581acc78250
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 14:46:28 +0000

    Bug 1102048 (Part 28, DecoderPool) - Make image/src files comply with the Mozilla Coding Style Guide. r=seth
    UltraBlame original commit: 8f97cb29ce9c61277ba6c76c7178ff812585bd10

--- image/src/DecodePool.h
+++ image/src/DecodePool.h
@@ -111,10 +111,10 @@ images
 /
 #
 ifndef
-MOZILLA_IMAGELIB_DECODEPOOL_H_
+mozilla_image_src_DecodePool_h
 #
 define
-MOZILLA_IMAGELIB_DECODEPOOL_H_
+mozilla_image_src_DecodePool_h
 #
 include
 "
@@ -678,4 +678,4 @@ mozilla
 endif
 /
 /
-MOZILLA_IMAGELIB_DECODEPOOL_H_
+mozilla_image_src_DecodePool_h
