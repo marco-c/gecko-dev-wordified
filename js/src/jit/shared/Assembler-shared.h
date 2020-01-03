3dd92fc88cd6ec5920e096289d88db22ad2e9988
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 04:08:44 +0000

    Bug 1091916 - simplify the interrupt/jitStackLimit situation (r=bhackett)
    UltraBlame original commit: 583284c299fb80f5cdf4cd27d125cb87ca1b8e48

--- js/src/jit/shared/Assembler-shared.h
+++ js/src/jit/shared/Assembler-shared.h
@@ -4212,7 +4212,7 @@ AsmJSExit
 :
 Builtin_ATan2D
 AsmJSImm_Runtime
-AsmJSImm_RuntimeInterrupt
+AsmJSImm_RuntimeInterruptUint32
 AsmJSImm_StackLimit
 AsmJSImm_ReportOverRecursed
 AsmJSImm_OnDetached
