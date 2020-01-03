19c6d28e0a0eccae6f7bda6dbcebf51fe6c942de
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 18:21:11 +0000

    Bug 1171330: P5. Split AppendData task to be closer to spec. r=cajbir.
    UltraBlame original commit: 5d30edb9048a289f28c7ef6dd13d4a53fb0170d8

--- dom/media/mediasource/SourceBuffer.h
+++ dom/media/mediasource/SourceBuffer.h
@@ -680,7 +680,7 @@ SourceBuffer
 ;
 friend
 class
-AppendDataRunnable
+BufferAppendRunnable
 ;
 friend
 class
@@ -796,13 +796,8 @@ aRv
 )
 ;
 void
-AppendData
+BufferAppend
 (
-MediaLargeByteBuffer
-*
-aData
-TimeUnit
-aTimestampOffset
 uint32_t
 aAppendID
 )
