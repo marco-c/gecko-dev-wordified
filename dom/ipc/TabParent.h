6ade7352a355b6e512695d85ffe2f86f5a6f2888
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-03 09:07:58 +0000

    Merge inbound to m-c. a=merge
    UltraBlame original commit: b6c56fab513ddd109307a8d9720c53947bd1756e

--- dom/ipc/TabParent.h
+++ dom/ipc/TabParent.h
@@ -244,13 +244,6 @@ TypeDecls
 .
 h
 "
-#
-include
-"
-nsIDOMEventListener
-.
-h
-"
 class
 nsFrameLoader
 ;
@@ -329,8 +322,6 @@ TabParent
 :
 public
 PBrowserParent
-public
-nsIDOMEventListener
 public
 nsITabParent
 public
@@ -504,11 +495,6 @@ mBrowserDOMWindow
 aBrowserDOMWindow
 ;
 }
-/
-/
-nsIDOMEventListener
-interfaces
-NS_DECL_NSIDOMEVENTLISTENER
 already_AddRefed
 <
 nsILoadContext
@@ -1479,6 +1465,10 @@ const
 nsIntSize
 &
 size
+const
+nsIntPoint
+&
+chromeDisp
 )
 ;
 void
