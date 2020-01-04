30d7dfdec9f2c05fb3ddc52275082189a13940f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 04:58:57 +0000

    Bug 1231315 - Build CONFIGURE_DEFINE_FILES at build time instead of during configure. r=gps
    UltraBlame original commit: 75e62a17dbba803107221515cbe3fab7f540fcdf

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -152,8 +152,6 @@ ExternalStaticLibrary
     
 ExternalSharedLibrary
     
-HeaderFileSubstitution
-    
 HostDefines
     
 HostLibrary
@@ -3830,15 +3828,60 @@ CONFIGURE_DEFINE_FILES
 ]
 :
             
-yield
-self
+script
+=
+mozpath
 .
-_create_substitution
+join
 (
-HeaderFileSubstitution
+mozpath
+.
+dirname
+(
+mozpath
+.
+dirname
+(
+__file__
+)
+)
+                                  
+'
+action
+'
+'
+process_define_files
+.
+py
+'
+)
+            
+yield
+GeneratedFile
+(
 context
-                
+script
+'
+process_define_file
+'
 path
+                                
+[
+mozpath
+.
+join
+(
+context
+.
+srcdir
+path
++
+'
+.
+in
+'
+)
+]
 )
         
 for
