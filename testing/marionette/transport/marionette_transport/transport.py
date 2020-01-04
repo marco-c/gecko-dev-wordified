732ccadd9c21214321ee8670f87e482bab6260fd
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:38:33 +0000

    Bug 1240601 - Set timeout on socket in transport.wait_for_port; r=automatedtester
    If the client is able to connect but the server never sends any data,
    the socket should time out in order for the function not to hang forever.
    
    That said, polling for ports like this is inherently racy and we should
    instead specify and bind what port we wish Marionette to start on in
    the future.
    
    UltraBlame original commit: 2ee5142ae16414ca6f58728d89fa1541931ac5e1

--- testing/marionette/transport/marionette_transport/transport.py
+++ testing/marionette/transport/marionette_transport/transport.py
@@ -1947,7 +1947,7 @@ host
 /
 port
 to
-be
+become
 available
 .
 "
@@ -2014,6 +2014,15 @@ SOCK_STREAM
             
 sock
 .
+settimeout
+(
+0
+.
+5
+)
+            
+sock
+.
 connect
 (
 (
@@ -2038,9 +2047,9 @@ close
 )
             
 if
-'
+"
 :
-'
+"
 in
 data
 :
@@ -2061,6 +2070,9 @@ finally
             
 if
 sock
+is
+not
+None
 :
                 
 sock
