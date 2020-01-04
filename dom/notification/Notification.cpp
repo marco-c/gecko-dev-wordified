eaa1280f1e635d58af6c39fb1c72b8fcaf17577e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:25:04 +0000

    Bug 1264815 - Rename 'serviceWorkerRegistrationID' to 'serviceWorkerRegistrationScope'. r=kcambridge
    UltraBlame original commit: 88fd1577bab5f8fee9faf91d065e0fc9ee4a9dde

--- dom/notification/Notification.cpp
+++ dom/notification/Notification.cpp
@@ -559,7 +559,7 @@ mBehavior
 ;
 const
 nsString
-mServiceWorkerRegistrationID
+mServiceWorkerRegistrationScope
 ;
 }
 ;
@@ -632,7 +632,7 @@ aBehavior
 const
 nsAString
 &
-aServiceWorkerRegistrationID
+aServiceWorkerRegistrationScope
 )
 final
 {
@@ -681,7 +681,7 @@ mScope
 .
 Equals
 (
-aServiceWorkerRegistrationID
+aServiceWorkerRegistrationScope
 )
 )
 {
@@ -731,7 +731,7 @@ aBehavior
 )
 nsString
 (
-aServiceWorkerRegistrationID
+aServiceWorkerRegistrationScope
 )
 }
 ;
@@ -947,7 +947,7 @@ mStrings
 i
 ]
 .
-mServiceWorkerRegistrationID
+mServiceWorkerRegistrationScope
 result
 )
 ;
@@ -5009,7 +5009,7 @@ aData
 const
 nsAString
 &
-aServiceWorkerRegistrationID
+aServiceWorkerRegistrationScope
 ErrorResult
 &
 aRv
@@ -5117,7 +5117,7 @@ notification
 >
 SetScope
 (
-aServiceWorkerRegistrationID
+aServiceWorkerRegistrationScope
 )
 ;
 return
@@ -10604,7 +10604,7 @@ mStrings
 i
 ]
 .
-mServiceWorkerRegistrationID
+mServiceWorkerRegistrationScope
 result
 )
 ;
