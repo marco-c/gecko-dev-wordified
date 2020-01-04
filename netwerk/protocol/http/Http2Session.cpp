6f30ea9c73ad121920d07b00e22baa5d54aa4551
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 01:21:20 +0000

    Backed out changeset 3cd86929d883 (bug 1208114) for test_video_playback.py crashes
    UltraBlame original commit: 0b1fbd9ec5498a47ef31dca8ba952d0e048ff7b8

--- netwerk/protocol/http/Http2Session.cpp
+++ netwerk/protocol/http/Http2Session.cpp
@@ -21308,12 +21308,6 @@ mDownstreamState
 =
 =
 NOT_USING_NETWORK
-|
-|
-mDownstreamState
-=
-=
-BUFFERING_FRAME_HEADER
 )
 {
 return
@@ -21835,11 +21829,6 @@ return
 NS_OK
 ;
 }
-MOZ_ASSERT
-(
-false
-)
-;
 return
 NS_ERROR_UNEXPECTED
 ;
