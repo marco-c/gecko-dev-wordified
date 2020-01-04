b5604cac615954f6f695a845a4ab8f29adc3edd3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 06:54:36 +0000

    Bug 1241549 - Annotate intentional switch fallthrough in hal/linux/UPowerClient.cpp. r=gsvelto
    hal/linux/UPowerClient.cpp:420:5: warning: unannotated fall-through between switch labels [-Wimplicit-fallthrough]
    
    UltraBlame original commit: 70cf738618530f220563072c91a2ba3901cd4a02

--- hal/linux/UPowerClient.cpp
+++ hal/linux/UPowerClient.cpp
@@ -136,6 +136,15 @@ include
 <
 mozilla
 /
+Attributes
+.
+h
+>
+#
+include
+<
+mozilla
+/
 dom
 /
 battery
@@ -2208,6 +2217,8 @@ eState_FullyCharged
 isFull
 =
 true
+;
+MOZ_FALLTHROUGH
 ;
 case
 eState_Charging
