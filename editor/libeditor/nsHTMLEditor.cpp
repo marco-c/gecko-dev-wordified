4bd0dffba18323799ebb9b8ececea8579520232d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 12:48:34 +0000

    Bug 1190172 part 10 - Clean up nsHTMLEditor::ReturnInListItem; r=ehsan
    UltraBlame original commit: 6dabbd41ffa254694a008a45517c7de38a9a3e11

--- editor/libeditor/nsHTMLEditor.cpp
+++ editor/libeditor/nsHTMLEditor.cpp
@@ -29127,7 +29127,7 @@ aPreviousBlock
 nsIDOMNode
 *
 aNewBlock
-nsIDOMNode
+Element
 *
 *
 aOutBrNode
@@ -29513,13 +29513,10 @@ deepestStyle
 *
 aOutBrNode
 =
-GetAsDOMNode
-(
 CreateBR
 (
 deepestStyle
 0
-)
 )
 ;
 NS_ENSURE_STATE
