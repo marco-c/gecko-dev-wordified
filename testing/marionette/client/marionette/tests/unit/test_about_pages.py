1e4385a714c52e838914496203fa9205af5cd7e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:24:51 +0000

    Bug 1184507 - Disable TestAboutPages:test_type_to_non_remote_tab for being racy. r=ato
    This test sends keys to the urlbar causing a page navigation, then waits on
    the current url to confirm the navigation is reflected. Because the navigation
    changes remoteness, the url check and loading the content listener in the
    new process race. When the url check wins, it causes a hang by sending a
    message before the frame script to receive it loads.
    
    This is a very specific scenario that only impacts tests that need to cause
    navigation to in-process pages with key events. If these sort of tests
    become a priority, this will need to be revisited.
    
    UltraBlame original commit: 29e88f597e0ee5437c049cb9dc50f14f6168f8f6

--- testing/marionette/client/marionette/tests/unit/test_about_pages.py
+++ testing/marionette/client/marionette/tests/unit/test_about_pages.py
@@ -58,6 +58,12 @@ marionette
 import
 MarionetteTestCase
 from
+marionette
+.
+marionette_test
+import
+skip_if_e10s
+from
 marionette_driver
 .
 keys
@@ -499,6 +505,8 @@ switch_to_window
 (
 start_win
 )
+    
+skip_if_e10s
     
 def
 test_type_to_non_remote_tab
