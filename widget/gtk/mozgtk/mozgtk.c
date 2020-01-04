6d4ee6ca0a10f0b22ca20c4de82c2b31f059e804
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:05:53 +0000

    Bug 1250704 - use same widget heirarchy as GTK for tooltip text color. r=stransky
    MozReview-Commit-ID: 41sDYu3IqYq
    
    UltraBlame original commit: daea8ab4c159c3a9c8b04c4ed068cef14260aa44

--- widget/gtk/mozgtk/mozgtk.c
+++ widget/gtk/mozgtk/mozgtk.c
@@ -2088,6 +2088,10 @@ gdk_x11_display_get_type
 )
 STUB
 (
+gtk_box_new
+)
+STUB
+(
 gtk_cairo_should_draw_window
 )
 STUB
@@ -2305,6 +2309,10 @@ gtk_widget_get_state_flags
 STUB
 (
 gtk_widget_get_style_context
+)
+STUB
+(
+gtk_widget_path_append_for_widget
 )
 STUB
 (
