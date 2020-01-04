f781c96a748c92ccd6296dd374bb4e46c58800f4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 17:06:09 +0000

    Backed out changeset 1ec6e0357c42 (bug 906116)
    UltraBlame original commit: 3a2cec4fd398e6cdedb1f15bafa9c6e202d4449f

--- layout/base/nsChangeHint.h
+++ layout/base/nsChangeHint.h
@@ -2309,6 +2309,16 @@ nsChangeHint_AllReflowHints
 )
 #
 define
+NS_STYLE_HINT_FRAMECHANGE
+\
+nsChangeHint
+(
+NS_STYLE_HINT_REFLOW
+|
+nsChangeHint_ReconstructFrame
+)
+#
+define
 nsChangeHint_Hints_CanIgnoreIfNotVisible
 \
 nsChangeHint
