5e942e567c23ece57b8cb206ab84b0d8ff49862d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:21:39 +0000

    Merge mozilla-central to fx-team
    UltraBlame original commit: abac231f245e808658e2df368719d561ddc4d45e

--- modules/libpref/init/all.js
+++ modules/libpref/init/all.js
@@ -5164,6 +5164,11 @@ me
 spreed
 .
 com
+air
+.
+mozilla
+.
+org
 "
 )
 ;
@@ -5340,6 +5345,11 @@ me
 spreed
 .
 com
+air
+.
+mozilla
+.
+org
 "
 )
 ;
@@ -14080,6 +14090,7 @@ secure
 "
 gzip
 deflate
+brotli
 "
 )
 ;
