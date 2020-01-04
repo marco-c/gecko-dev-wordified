9c9b7c1bb6e895a52f6c43fd9ea589f48d464f3a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 09:49:59 +0000

    Backed out changeset b6ca6b5832d8 (bug 933257)
    UltraBlame original commit: f87fe6fda7179cfe741510950ef55412dbc11259

--- js/src/asmjs/WasmTypes.cpp
+++ js/src/asmjs/WasmTypes.cpp
@@ -189,13 +189,6 @@ h
 #
 include
 "
-fdlibm
-.
-h
-"
-#
-include
-"
 jslibmath
 .
 h
@@ -1421,9 +1414,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 sin
 Args_Double_Double
 )
@@ -1445,9 +1435,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 cos
 Args_Double_Double
 )
@@ -1467,9 +1454,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 tan
 Args_Double_Double
 )
@@ -1489,9 +1473,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 asin
 Args_Double_Double
 )
@@ -1511,9 +1492,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 acos
 Args_Double_Double
 )
@@ -1533,9 +1511,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 atan
 Args_Double_Double
 )
@@ -1555,9 +1530,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 ceil
 Args_Double_Double
 )
@@ -1577,9 +1549,6 @@ float
 )
 >
 (
-fdlibm
-:
-:
 ceilf
 Args_Float32_Float32
 )
@@ -1599,9 +1568,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 floor
 Args_Double_Double
 )
@@ -1621,9 +1587,6 @@ float
 )
 >
 (
-fdlibm
-:
-:
 floorf
 Args_Float32_Float32
 )
@@ -1643,9 +1606,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 exp
 Args_Double_Double
 )
@@ -1665,9 +1625,6 @@ double
 )
 >
 (
-fdlibm
-:
-:
 log
 Args_Double_Double
 )
