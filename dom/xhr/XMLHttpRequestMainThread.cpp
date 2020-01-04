ff3d63c2986aa6eabab14ac0d1b288e243c836c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:34:25 +0000

    Backed out changeset b64d6f5b63db (bug 918751) for OS X permaorange in XMLHttpRequest/send-non-same-origin.sub.htm
    UltraBlame original commit: 26e22af660e543ebb69930f082188b69ec756185

--- dom/xhr/XMLHttpRequestMainThread.cpp
+++ dom/xhr/XMLHttpRequestMainThread.cpp
@@ -14992,7 +14992,7 @@ mFlagSynchronous
 )
 {
 return
-NS_ERROR_DOM_NETWORK_ERR
+rv
 ;
 }
 }
@@ -16469,7 +16469,7 @@ mFlagSynchronous
 )
 {
 return
-NS_ERROR_DOM_NETWORK_ERR
+NS_ERROR_FAILURE
 ;
 }
 else
