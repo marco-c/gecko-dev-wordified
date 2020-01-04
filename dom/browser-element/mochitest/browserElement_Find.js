eacea7d6bf3dfc22f77624f4c5be15fd46d1ed97
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 16:01:47 +0000

    Bug 384458 - part 5: add unit test coverage for the findbar modal highlight feature. r=jaws
    UltraBlame original commit: 1c86ba5d7a5b9cb16ef3ad77ff740688a923108a

--- dom/browser-element/mochitest/browserElement_Find.js
+++ dom/browser-element/mochitest/browserElement_Find.js
@@ -289,7 +289,7 @@ foo
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 1
@@ -357,7 +357,7 @@ foo
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 2
@@ -425,7 +425,7 @@ foo
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 1
@@ -498,7 +498,7 @@ xxx
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 0
@@ -571,7 +571,7 @@ bar
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 1
@@ -639,7 +639,7 @@ bar
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 2
@@ -707,7 +707,7 @@ bar
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 3
@@ -775,7 +775,7 @@ bar
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 4
@@ -843,7 +843,7 @@ bar
 '
 searchLimit
 :
-100
+1000
 activeMatchOrdinal
 :
 1
