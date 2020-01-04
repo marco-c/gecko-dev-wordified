3d78b86fd950bc3ad73c7aec27f311485a7dc3bf
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 03:42:53 +0000

    Bug 1176792 part 1 - [css-grid] Implement the 'grid-column-gap', 'grid-row-gap', and 'grid-gap' properties in the style system.  r=dholbert,dbaron
    UltraBlame original commit: 3c50e66165d5f4160872e13e0f5d99281cb4fef3

--- layout/style/nsComputedDOMStylePropertyList.h
+++ layout/style/nsComputedDOMStylePropertyList.h
@@ -1044,6 +1044,11 @@ GridColumnEnd
 )
 COMPUTED_STYLE_PROP
 (
+grid_column_gap
+GridColumnGap
+)
+COMPUTED_STYLE_PROP
+(
 grid_column_start
 GridColumnStart
 )
@@ -1051,6 +1056,11 @@ COMPUTED_STYLE_PROP
 (
 grid_row_end
 GridRowEnd
+)
+COMPUTED_STYLE_PROP
+(
+grid_row_gap
+GridRowGap
 )
 COMPUTED_STYLE_PROP
 (
