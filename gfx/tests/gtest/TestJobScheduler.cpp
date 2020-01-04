93cc5ade57a78abb7f649b190199985cb05ae52a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 00:29:13 +0000

    Bug 1083101 - Win32 implementation of the JobScheduler. r=jrmuizel
    UltraBlame original commit: 6a9e2e39539c9a95dda3a062a1892a6ea8df462f

--- gfx/tests/gtest/TestJobScheduler.cpp
+++ gfx/tests/gtest/TestJobScheduler.cpp
@@ -47,9 +47,6 @@ zero
 *
 /
 #
-ifndef
-WIN32
-#
 include
 "
 gtest
@@ -79,6 +76,9 @@ JobScheduler
 h
 "
 #
+ifndef
+WIN32
+#
 include
 <
 pthread
@@ -92,6 +92,8 @@ sched
 .
 h
 >
+#
+endif
 #
 include
 <
@@ -149,6 +151,9 @@ MaybeYieldThread
 (
 )
 {
+#
+ifndef
+WIN32
 if
 (
 rand
@@ -166,6 +171,8 @@ sched_yield
 )
 ;
 }
+#
+endif
 }
 /
 /
@@ -1528,5 +1535,3 @@ ShutDown
 }
 }
 }
-#
-endif
