e8f7fe7f117bac48b1d7da4be710dabe4c54d954
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 05:52:42 +0000

    Bug 1211366: Follow-up: Fix ESLint errors. r=trivial
    UltraBlame original commit: e657460f49969b218ccaca26b158b46e7baf9524

--- browser/components/extensions/test/browser/browser_ext_contextMenus.js
+++ browser/components/extensions/test/browser/browser_ext_contextMenus.js
@@ -248,9 +248,6 @@ title
 =
 context
 ;
-var
-id
-=
 browser
 .
 contextMenus
@@ -340,7 +337,7 @@ info
 ;
 }
 }
-var
+let
 parent
 =
 browser
@@ -379,6 +376,9 @@ genericOnClick
 }
 )
 ;
+let
+child2
+=
 browser
 .
 contextMenus
@@ -400,7 +400,7 @@ genericOnClick
 }
 )
 ;
-var
+let
 parentToDel
 =
 browser
