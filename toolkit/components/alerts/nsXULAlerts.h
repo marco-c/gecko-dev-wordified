9bb915b61c619038df70900fda2fc690fd41b5e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 08:12:47 +0000

    Bug 1206560 - Show the site favicon in XUL notifications. r=jaws
    MozReview-Commit-ID: Fe3R91icOuc
    
    UltraBlame original commit: 4357fe5a9d22822ffbc5d15246c7df34a484816f

--- toolkit/components/alerts/nsXULAlerts.h
+++ toolkit/components/alerts/nsXULAlerts.h
@@ -134,6 +134,8 @@ public
 nsIAlertsService
 public
 nsIAlertsDoNotDisturb
+public
+nsIAlertsIconURI
 {
 friend
 class
@@ -141,6 +143,7 @@ nsXULAlertObserver
 ;
 public
 :
+NS_DECL_NSIALERTSICONURI
 NS_DECL_NSIALERTSDONOTDISTURB
 NS_DECL_NSIALERTSSERVICE
 NS_DECL_ISUPPORTS
