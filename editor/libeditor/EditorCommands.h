c4e451a326a385980beb0fa71990c97a01ec39d2
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 20:12:39 +0000

    Bug 748308 - Support insertParagraph and insertLineBreak per spec/Blink/WebKit; r=masayuki
    Previously we supported insertParagraph as a synonym for formatBlock
    "p", which is both useless and incompatible with all other browsers.
    Edge's behavior doesn't look useful and looks redundant with insertHTML
    (although it matches the name nicely, probably because they invented
    the command).  Blink/WebKit treat it the same as hitting Enter, which is
    useful for cross-browser testing until we get synthesizeKey() in wpt.
    
    At the same time, I added insertLineBreak, which Blink/WebKit support
    with the same functionality as pressing Shift-Enter.
    
    The new event.html failures are spurious -- we used to pass by mistake
    because we didn't support insertlinebreak at all.  insertlinebreak.html
    has only new passes, no new failures, although it's not clear on the
    diff.
    
    MozReview-Commit-ID: 16oPcxXwGcj
    
    UltraBlame original commit: afaae749f78d2ef896c0a8fcc496a5bffe77f812

--- editor/libeditor/EditorCommands.h
+++ editor/libeditor/EditorCommands.h
@@ -405,6 +405,14 @@ InsertPlaintextCommand
 )
 NS_DECL_EDITOR_COMMAND
 (
+InsertParagraphCommand
+)
+NS_DECL_EDITOR_COMMAND
+(
+InsertLineBreakCommand
+)
+NS_DECL_EDITOR_COMMAND
+(
 PasteQuotationCommand
 )
 #
