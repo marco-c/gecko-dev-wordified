c54a1c19bd60aa212edc4ee40898e9eb45bc51bb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-04 18:54:03 +0000

    Bug 1284914, add support in html for the ondragexit attribute, r=smaug
    UltraBlame original commit: 7e96b394d5f04bebd40c6385da9b869851019967

--- dom/events/EventNameList.h
+++ dom/events/EventNameList.h
@@ -1080,6 +1080,13 @@ eDragEventClass
 )
 EVENT
 (
+dragexit
+eDragExit
+EventNameType_HTMLXUL
+eDragEventClass
+)
+EVENT
+(
 dragleave
 eDragLeave
 EventNameType_HTMLXUL
@@ -2248,13 +2255,6 @@ commandupdate
 eXULCommandUpdate
 EventNameType_XUL
 eBasicEventClass
-)
-NON_IDL_EVENT
-(
-dragexit
-eDragExit
-EventNameType_XUL
-eDragEventClass
 )
 NON_IDL_EVENT
 (
