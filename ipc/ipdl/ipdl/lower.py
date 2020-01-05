f2940b4b1c2bbfba09a54d87fb6201a7b0b0c8c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 02:12:40 +0000

    Bug 1319271 - Return early in Send__delete__ method when Send fails. r=billm
    MozReview-Commit-ID: GOVSpzT3JlX
    
    UltraBlame original commit: a9856363aac76201b73e4885673889a4983e5f21

--- ipc/ipdl/ipdl/lower.py
+++ ipc/ipdl/ipdl/lower.py
@@ -33337,6 +33337,25 @@ msgvar
 actorvar
 )
         
+failif
+=
+StmtIf
+(
+ExprNot
+(
+sendok
+)
+)
+        
+failif
+.
+addifstmt
+(
+StmtReturn
+.
+FALSE
+)
+        
 method
 .
 addstmts
@@ -33374,6 +33393,7 @@ sendstmts
             
 +
 [
+failif
 Whitespace
 .
 NL
@@ -33536,6 +33556,25 @@ md
 msgvar
 replyvar
 actorvar
+)
+        
+failif
+=
+StmtIf
+(
+ExprNot
+(
+sendok
+)
+)
+        
+failif
+.
+addifstmt
+(
+StmtReturn
+.
+FALSE
 )
         
 method
@@ -33705,6 +33744,14 @@ method
 addstmts
 (
             
+[
+failif
+Whitespace
+.
+NL
+]
+            
++
 self
 .
 dtorEpilogue
