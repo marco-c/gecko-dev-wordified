3fe0aa96d64c52d209a47f57851697ec48188f4b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 17:31:15 +0000

    Backed out 2 changesets (bug 1350660) for causing merge conflicts a=backout
    Backed out changeset 13d070200dfe (bug 1350660)
    Backed out changeset f033452487fc (bug 1350660)
    
    UltraBlame original commit: 5bb665191fd25521dcd35d452d75a0e255bfa65f

--- gfx/ipc/GPUChild.cpp
+++ gfx/ipc/GPUChild.cpp
@@ -540,13 +540,13 @@ this
 ;
 #
 ifdef
-MOZ_GECKO_PROFILER
+MOZ_ENABLE_PROFILER_SPS
 mProfilerController
 =
-MakeUnique
-<
 CrossProcessProfilerController
->
+:
+:
+ForProtocol
 (
 this
 )
@@ -1559,7 +1559,7 @@ nullptr
 }
 #
 ifdef
-MOZ_GECKO_PROFILER
+MOZ_ENABLE_PROFILER_SPS
 mProfilerController
 =
 nullptr
