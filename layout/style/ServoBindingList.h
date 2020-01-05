9a63eb5a587d76f2f60bcb35fd17ee740c8b8354
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 19:29:46 +0000

    Bug 1332633 - Add Servo_AnimationValues_ComputeDistance. r=emilio
    This FFI will be used by nsDOMWindowUtils::ComputeAnimationDistance,
    which will be implemented in another bug.
    
    MozReview-Commit-ID: G3aYICDnCLA
    
    UltraBlame original commit: f902d870c642e0f673cfa3a04d03f41d1311be44

--- layout/style/ServoBindingList.h
+++ layout/style/ServoBindingList.h
@@ -797,6 +797,15 @@ to
 )
 SERVO_BINDING_FUNC
 (
+Servo_AnimationValues_ComputeDistance
+double
+RawServoAnimationValueBorrowed
+from
+RawServoAnimationValueBorrowed
+to
+)
+SERVO_BINDING_FUNC
+(
 Servo_AnimationValue_Serialize
 void
 RawServoAnimationValueBorrowed
