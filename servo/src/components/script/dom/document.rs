55ac38c7670088183c633a0729c27ea22c3e6cd1
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 06:55:19 +0000

    servo: Merge #2199 - Implement Element.prefix (Fixes #1737) (from sankha93:prefix-impl); r=Ms2ger
    See #1737.
    
    r? ms2ger
    
    Source-Repo: https://github.com/servo/servo
    Source-Revision: c760577aee3ca31e58b4dd2b7f919d14fcaa2d2f
    
    UltraBlame original commit: 8b2f75304a39f3b3395e1018f60898a0e4d01bf8

--- servo/src/components/script/dom/document.rs
+++ servo/src/components/script/dom/document.rs
@@ -2315,6 +2315,10 @@ match
 &
 ns
 prefix_from_qname
+.
+clone
+(
+)
 local_name_from_qname
 .
 as_slice
@@ -2583,6 +2587,7 @@ new
 (
 local_name_from_qname
 ns
+prefix_from_qname
 abstract_self
 )
 )
--- servo/src/components/script/dom/htmlelement.rs
+++ servo/src/components/script/dom/htmlelement.rs
@@ -317,6 +317,7 @@ namespace
 :
 :
 HTML
+None
 document
 )
 }
