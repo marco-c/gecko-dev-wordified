486fb9bddf6ce3adaa02f820e99861166ee6a4a0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 00:58:01 +0000

    Bug 1315521 - remove CC for audio channel agent. r=jwwang
    Because audio channel agent uses weak ref to HTMLMediaElement, so we don't need to add it into the CC list.
    
    MozReview-Commit-ID: 4cFfDUlbMFB
    
    UltraBlame original commit: 87b6987646642d3d998df229e423b466271ccd4d

--- dom/html/HTMLMediaElement.cpp
+++ dom/html/HTMLMediaElement.cpp
@@ -4495,10 +4495,6 @@ mSourceLoadCandidate
 )
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE
 (
-mAudioChannelAgent
-)
-NS_IMPL_CYCLE_COLLECTION_TRAVERSE
-(
 mError
 )
 for
@@ -4623,10 +4619,6 @@ mLoadBlockedDoc
 NS_IMPL_CYCLE_COLLECTION_UNLINK
 (
 mSourceLoadCandidate
-)
-NS_IMPL_CYCLE_COLLECTION_UNLINK
-(
-mAudioChannelAgent
 )
 NS_IMPL_CYCLE_COLLECTION_UNLINK
 (
