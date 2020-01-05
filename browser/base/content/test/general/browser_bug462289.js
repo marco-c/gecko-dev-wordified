a1ef5e4f1b17c2975c9d415d698e260b83d67bb4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 23:50:47 +0000

    Bug 1363829 P14 Make browser_bug462289.js test use setTimeout(f, 0) like it did before bug 1102017. r=dao
    UltraBlame original commit: 9b5fb2901eac7ccd48c7c0bb58ea229f817e963b

--- browser/base/content/test/general/browser_bug462289.js
+++ browser/base/content/test/general/browser_bug462289.js
@@ -101,9 +101,10 @@ tab1
 }
 )
 ;
-executeSoon
+setTimeout
 (
 step2
+0
 )
 ;
 }
@@ -155,9 +156,10 @@ tab1
 }
 )
 ;
-executeSoon
+setTimeout
 (
 step3
+0
 )
 ;
 }
@@ -283,9 +285,10 @@ tab1
 }
 )
 ;
-executeSoon
+setTimeout
 (
 step4
+0
 )
 ;
 }
@@ -475,9 +478,10 @@ mousedown
 }
 )
 ;
-executeSoon
+setTimeout
 (
 step6
+0
 )
 ;
 }
