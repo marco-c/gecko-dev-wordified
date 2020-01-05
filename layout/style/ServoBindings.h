fcf831a7ad30068eca8d849c841cd4672e0ad670
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 21:58:22 +0000

    Bug 1328319 part 4 - Add binding for setting pair value of nsCSSValue. r=manishearth
    MozReview-Commit-ID: 2bAH90Mh4GP
    
    UltraBlame original commit: e1804542ca963914ccd30ecc3e11598959422106

--- layout/style/ServoBindings.h
+++ layout/style/ServoBindings.h
@@ -3414,6 +3414,17 @@ unit
 )
 ;
 void
+Gecko_CSSValue_SetPair
+(
+nsCSSValueBorrowedMut
+css_value
+nsCSSValueBorrowed
+xvalue
+nsCSSValueBorrowed
+yvalue
+)
+;
+void
 Gecko_CSSValue_Drop
 (
 nsCSSValueBorrowedMut
