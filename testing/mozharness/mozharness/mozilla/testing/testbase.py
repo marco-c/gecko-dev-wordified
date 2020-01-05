4526aacb201d06d629e913f79fca1d6d79a6e296
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-05 20:08:35 +0000

    Bug 1359988 - Remove references to gaia in mozharness; r=mshal
    These were the last occurrences of "gaia" in the mozharness directory.
    
    MozReview-Commit-ID: 8ACEw1rMoUS
    
    UltraBlame original commit: 45200ddaa281007c2ffac15184f6499da3656bc2

--- testing/mozharness/mozharness/mozilla/testing/testbase.py
+++ testing/mozharness/mozharness/mozilla/testing/testbase.py
@@ -2580,27 +2580,6 @@ expected_length
 3
 ]
             
-if
-buildbot_prop_branch
-.
-startswith
-(
-'
-gaia
--
-try
-'
-)
-:
-                
-expected_length
-=
-range
-(
-1
-1000
-)
-            
 actual_length
 =
 len
