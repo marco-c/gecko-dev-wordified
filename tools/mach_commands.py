e329d04a37d2975623cd6d8ef364b82dbd71117b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 20:04:41 +0000

    Bug 1411004 - ./mach clang-format: Better handling of relative paths r=mystor=mystor
    For example, this is addressing the issue:
    ./mach clang-format -p ./mfbt/
    
    MozReview-Commit-ID: Le8mPTOEfA7
    
    UltraBlame original commit: 31f259ee387b61199ae3ea2f92d68fea45d88db2

--- tools/mach_commands.py
+++ tools/mach_commands.py
@@ -3111,11 +3111,34 @@ strip
 :
                 
 continue
+             
+#
+The
+regexp
+is
+to
+make
+sure
+we
+are
+managing
+relative
+paths
             
 ignored_dir
 .
 append
 (
+"
+^
+[
+\
+.
+/
+]
+*
+"
++
 line
 .
 rstrip
