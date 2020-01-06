c312d66923f99a73d4668ea0059dd4c6ca3c87c7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:36:17 +0000

    Backed out 2 changesets (bug 1382377) under suspicion of causing asan devtools failures a=backout CLOSED TREE
    Backed out changeset c23f8bd95172 (bug 1382377)
    Backed out changeset e9156a9ede8e (bug 1382377)
    
    MozReview-Commit-ID: GnLs0Kzd5em
    
    UltraBlame original commit: 9182a041d8891cc61efb3e2cbe35397b3f981364

--- dom/workers/WorkerPrefs.h
+++ dom/workers/WorkerPrefs.h
@@ -587,16 +587,6 @@ resistFingerprinting
 ResistFingerprintingEnabled
 RESISTFINGERPRINTING_ENABLED
 )
-WORKER_SIMPLE_PREF
-(
-"
-devtools
-.
-enabled
-"
-DevToolsEnabled
-DEVTOOLS_ENABLED
-)
 WORKER_PREF
 (
 "
