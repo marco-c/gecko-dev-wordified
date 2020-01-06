32b535c42a108a04916c9af578a44d65ca983661
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:27:44 +0000

    Backed out changeset 018e683b25cd (bug 1370007) for breaking tier 2 nightly builds on windows
    UltraBlame original commit: 173533a310060d0a52d6f6c3b3bee998ab9199d2

--- gfx/layers/d3d11/genshaders.py
+++ gfx/layers/d3d11/genshaders.py
@@ -71,8 +71,6 @@ import
 tempfile
 import
 yaml
-import
-buildconfig
 def
 shell_main
 (
@@ -463,22 +461,13 @@ output_fp
 )
 :
   
-fxc_location
-=
-buildconfig
-.
-substs
-[
-'
-FXC
-'
-]
-  
 argv
 =
 [
     
-fxc_location
+'
+fxc
+'
     
 '
 -
@@ -532,36 +521,6 @@ Vi
 '
   
 ]
-  
-if
-'
-Linux
-'
-in
-buildconfig
-.
-substs
-[
-'
-HOST_OS_ARCH
-'
-]
-:
-    
-argv
-.
-insert
-(
-0
-buildconfig
-.
-substs
-[
-'
-WINE
-'
-]
-)
   
 if
 shader_model
@@ -688,12 +647,6 @@ proc_stdout
 )
     
 assert
-'
-fxc2
-'
-in
-fxc_location
-or
 len
 (
 deps
