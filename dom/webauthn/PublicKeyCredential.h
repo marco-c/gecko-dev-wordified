a5bd50b00a21be5bfb8b85c19e88eacd82d15086
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 21:47:09 +0000

    Bug 1406468 - Web Authentication - Implement isUserVerifyingPlatformAuthenticatorAvailable() method r=jcj,smaug
    Summary:
    We currently implement no platform authenticators, so this would always
    resolve to false. For those cases, the spec recommends a resolve timeout
    on the order of 10 minutes to avoid fingerprinting.
    
    A simple solution is thus to never resolve the promise, otherwise we'd
    have to track every single call to this method along with a promise
    and timer to resolve it after exactly X minutes.
    
    A Relying Party has to deal with a non-response in a timely fashion, so
    we can keep this as-is (and not resolve) even when we support platform
    authenticators but they're not available, or a user rejects a website's
    request to use them.
    
    Reviewers: jcj, smaug
    
    Reviewed By: jcj, smaug
    
    Bug #: 1406468
    
    Differential Revision: https://phabricator.services.mozilla.com/D217
    
    UltraBlame original commit: 1ddfda4ef74f39e2558220e901ed51a665a66733

--- dom/webauthn/PublicKeyCredential.h
+++ dom/webauthn/PublicKeyCredential.h
@@ -290,7 +290,7 @@ already_AddRefed
 <
 Promise
 >
-IsPlatformAuthenticatorAvailable
+IsUserVerifyingPlatformAuthenticatorAvailable
 (
 GlobalObject
 &
--- dom/webidl/WebAuthentication.webidl
+++ dom/webidl/WebAuthentication.webidl
@@ -201,7 +201,7 @@ Promise
 <
 boolean
 >
-isPlatformAuthenticatorAvailable
+isUserVerifyingPlatformAuthenticatorAvailable
 (
 )
 ;
