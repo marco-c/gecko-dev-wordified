df8073a0650e45d4761fc4701dfca55d7cac5ba7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 03:18:00 +0000

    Bug 1378499 - add missing include files. r=mstange
    MozReview-Commit-ID: LQ6E0wAbhU0
    
    UltraBlame original commit: c12c7198c4d3cefca7bd3ae798a2c52efaeccd55

--- widget/cocoa/nsCocoaWindow.mm
+++ widget/cocoa/nsCocoaWindow.mm
@@ -130,6 +130,13 @@ h
 #
 include
 "
+ScreenHelperCocoa
+.
+h
+"
+#
+include
+"
 TextInputHandler
 .
 h
--- widget/cocoa/nsPrintDialogX.mm
+++ widget/cocoa/nsPrintDialogX.mm
@@ -105,6 +105,15 @@ h
 #
 include
 "
+mozilla
+/
+Preferences
+.
+h
+"
+#
+include
+"
 nsPrintDialogX
 .
 h
