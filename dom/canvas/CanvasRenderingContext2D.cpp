08da078b5d3071657eadddf7bf0352a9136412fc
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:33:03 +0000

    Bug 1401379 - Part 2: Add more cycle collector edges for nsDocShell, r=smaug
    MozReview-Commit-ID: 3j9jfLv7MO2
    
    UltraBlame original commit: 9b5b03b2114d377ead28cffbb991b792457cb509

--- dom/canvas/CanvasRenderingContext2D.cpp
+++ dom/canvas/CanvasRenderingContext2D.cpp
@@ -5734,6 +5734,10 @@ NS_IMPL_CYCLE_COLLECTION_UNLINK
 (
 mCanvasElement
 )
+NS_IMPL_CYCLE_COLLECTION_UNLINK
+(
+mDocShell
+)
 for
 (
 uint32_t
@@ -5944,6 +5948,10 @@ CanvasRenderingContext2D
 NS_IMPL_CYCLE_COLLECTION_TRAVERSE
 (
 mCanvasElement
+)
+NS_IMPL_CYCLE_COLLECTION_TRAVERSE
+(
+mDocShell
 )
 for
 (
