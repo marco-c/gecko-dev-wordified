79296005e809fde81fd3f22ec13f8af953d52240
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 05:23:38 +0000

    Bug 1365876. Blur text shadows on the CPU. r=lsalzman
    UltraBlame original commit: a75faf9cd6a7bf61bb3fc5b9202932de01a61e0b

--- layout/base/nsLayoutUtils.cpp
+++ layout/base/nsLayoutUtils.cpp
@@ -25305,10 +25305,8 @@ return
 false
 ;
 }
+static
 void
-nsLayoutUtils
-:
-:
 AddBoxesForFrame
 (
 nsIFrame
@@ -39489,6 +39487,10 @@ AppUnitsPerDevPixel
 aDestCtx
 aDirtyRect
 nullptr
+nsContextBoxBlur
+:
+:
+DISABLE_HARDWARE_ACCELERATION_BLUR
 )
 ;
 if
