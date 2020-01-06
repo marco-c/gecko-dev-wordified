be96553b268b987aa90acc5f5e40a24b5522f0a6
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 10:40:36 +0000

    Bug 1382377 - Make Console API be a no-op until DevTools are opened at least once. r=baku,jdescottes
    MozReview-Commit-ID: 4xuzRQKH3Vg
    
    UltraBlame original commit: 39432409ceddebce3161c1713fe51b4b4184ee41

--- dom/workers/WorkerPrefs.h
+++ dom/workers/WorkerPrefs.h
@@ -587,6 +587,16 @@ resistFingerprinting
 ResistFingerprintingEnabled
 RESISTFINGERPRINTING_ENABLED
 )
+WORKER_SIMPLE_PREF
+(
+"
+devtools
+.
+enabled
+"
+DevToolsEnabled
+DEVTOOLS_ENABLED
+)
 WORKER_PREF
 (
 "
