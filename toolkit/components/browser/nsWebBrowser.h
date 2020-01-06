5dd73a1a9af7a735e9f55d93f49974ce00a91cb5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 16:33:13 +0000

    Backed out changeset 8a26cdedeb58 (bug 1401379)
    UltraBlame original commit: 200ae65803f738eec82653428f3a7dd54f045fc3

--- toolkit/components/browser/nsWebBrowser.h
+++ toolkit/components/browser/nsWebBrowser.h
@@ -149,13 +149,6 @@ nsCOMPtr
 .
 h
 "
-#
-include
-"
-nsCycleCollectionParticipant
-.
-h
-"
 /
 /
 Interfaces
@@ -492,12 +485,7 @@ nsWebBrowser
 (
 )
 ;
-NS_DECL_CYCLE_COLLECTING_ISUPPORTS
-NS_DECL_CYCLE_COLLECTION_CLASS_AMBIGUOUS
-(
-nsWebBrowser
-nsIWebBrowser
-)
+NS_DECL_ISUPPORTS
 NS_DECL_NSIBASEWINDOW
 NS_DECL_NSIDOCSHELLTREEITEM
 NS_DECL_NSIINTERFACEREQUESTOR
