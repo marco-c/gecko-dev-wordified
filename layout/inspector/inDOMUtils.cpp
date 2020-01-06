5c2115e54be15cd447c04fb828adfc313b4d5fb8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-06 01:35:23 +0000

    Bug 1370034 - Add CSS properties -moz-window-transform and -moz-window-transform-origin, style system parts (excluding stylo). r=dbaron
    MozReview-Commit-ID: BlLMswF0bKO
    
    UltraBlame original commit: 1e5fe4feb6598fc62106078eb0ca9828f5bb8e9e

--- layout/inspector/inDOMUtils.cpp
+++ layout/inspector/inDOMUtils.cpp
@@ -4588,6 +4588,9 @@ eCSSProperty__moz_outline_radius_bottomleft
 case
 eCSSProperty__moz_outline_radius_bottomright
 :
+case
+eCSSProperty__moz_window_transform_origin
+:
 supported
 =
 VARIANT_LP
--- layout/style/generate-stylestructlist.py
+++ layout/style/generate-stylestructlist.py
@@ -387,6 +387,8 @@ UIReset
 nullptr
 "
 NORMAL_DEP
++
+LENGTH_DEP
 )
     
 (
