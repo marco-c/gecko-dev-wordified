e99f14d4f85738eea2996195504965cc816d8ae4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 00:30:11 +0000

    Bug 1417709 - Add system colors called "-moz-mac-vibrant-titlebar-light/dark" for use with -moz-font-smoothing-background-color. r=emilio
    MozReview-Commit-ID: 9FbRlPvrLMX
    
    UltraBlame original commit: 000e6eab5b25576d4180f9bbf9e08881f8d7b457

--- widget/LookAndFeel.h
+++ widget/LookAndFeel.h
@@ -597,6 +597,8 @@ appearance
 names
 eColorID__moz_mac_vibrancy_light
 eColorID__moz_mac_vibrancy_dark
+eColorID__moz_mac_vibrant_titlebar_light
+eColorID__moz_mac_vibrant_titlebar_dark
 eColorID__moz_mac_menupopup
 eColorID__moz_mac_menuitem
 eColorID__moz_mac_active_menuitem
