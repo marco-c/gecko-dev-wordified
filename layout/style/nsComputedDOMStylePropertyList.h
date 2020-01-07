911598c6820949e8e32961f4fcbaf9b5d5e63073
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 05:54:31 +0000

    Bug 1207734 - Part 5. Implement the getComputedStyle API for individual transform. r=birtles
    MozReview-Commit-ID: 3w1kA6VCXsg
    (grafted from ee81993c07a30fe5b8788d2ce6707967eb0a832a)
    
    UltraBlame original commit: e011097312763a5108f478463819e74e3c181f45

--- layout/style/nsComputedDOMStylePropertyList.h
+++ layout/style/nsComputedDOMStylePropertyList.h
@@ -1506,6 +1506,11 @@ Right
 )
 COMPUTED_STYLE_PROP
 (
+rotate
+Rotate
+)
+COMPUTED_STYLE_PROP
+(
 ruby_align
 RubyAlign
 )
@@ -1513,6 +1518,11 @@ COMPUTED_STYLE_PROP
 (
 ruby_position
 RubyPosition
+)
+COMPUTED_STYLE_PROP
+(
+scale
+Scale
 )
 COMPUTED_STYLE_PROP
 (
@@ -1720,6 +1730,11 @@ COMPUTED_STYLE_PROP
 (
 transition_timing_function
 TransitionTimingFunction
+)
+COMPUTED_STYLE_PROP
+(
+translate
+Translate
 )
 COMPUTED_STYLE_PROP
 (
