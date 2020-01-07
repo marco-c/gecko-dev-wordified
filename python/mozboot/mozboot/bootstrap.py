4d4d5a5edbb84b7c1de8b3b75febaec346b5360c
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 08:42:50 +0000

    Bug 1440428 - Fix: Don't install Proguard JAR in |mach bootstrap|. r=jchen
    MozReview-Commit-ID: EOvKSE5z28P
    
    UltraBlame original commit: ee70413814591848629fb6bdc53e4514f59ba6b4

--- python/mozboot/mozboot/bootstrap.py
+++ python/mozboot/mozboot/bootstrap.py
@@ -2647,24 +2647,6 @@ ensure_stylo_packages
 state_dir
 checkout_root
 )
-            
-if
-'
-mobile_android
-'
-in
-application
-:
-                
-self
-.
-instance
-.
-ensure_proguard_packages
-(
-state_dir
-checkout_root
-)
         
 print
 (
