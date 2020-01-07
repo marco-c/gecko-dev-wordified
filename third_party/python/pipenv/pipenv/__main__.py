968e3f4d33eceaabdc1396c566d69ae7edf95f64
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 20:59:19 +0000

    Bug 1437593 - Vendor pipenv 11.9.0; r=ted
    This was vendored by downloading the tar.gz for the release from https://github.com/pypa/pipenv/releases and extracting into third_party/python. The release number was then stripped from the resulting directory.
    
    MozReview-Commit-ID: 563xZxZ88lm
    
    UltraBlame original commit: c7fa833eff1d7818ef5ca4a5b853d03c326ec122

---
+++ third_party/python/pipenv/pipenv/__main__.py
@@ -0,0 +1,17 @@
+from
+.
+cli
+import
+cli
+if
+__name__
+=
+=
+'
+__main__
+'
+:
+    
+cli
+(
+)
---
+++ third_party/python/pipenv/pipenv/vendor/Levenshtein/__init__.py
@@ -0,0 +1,15 @@
+from
+Levenshtein
+import
+_levenshtein
+from
+Levenshtein
+.
+_levenshtein
+import
+*
+__doc__
+=
+_levenshtein
+.
+__doc__
