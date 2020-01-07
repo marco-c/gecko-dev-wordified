87101307c5634da90346932f8e10d6ba90ab2e2e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 17:01:42 +0000

    Backed out changeset 614181fad27a (bug 1449064) for assertion failures on workspace/build/src/modules/libpref/Preferences.cpp
    UltraBlame original commit: a8cb19565e34130e30c82b75ec132ebb44c013c9

--- parser/html/nsHtml5TreeOpExecutor.h
+++ parser/html/nsHtml5TreeOpExecutor.h
@@ -268,6 +268,10 @@ public
 NS_DECL_ISUPPORTS_INHERITED
 private
 :
+static
+bool
+sExternalViewSource
+;
 #
 ifdef
 DEBUG_NS_HTML5_TREE_OP_EXECUTOR_FLUSH
@@ -1073,6 +1077,12 @@ const
 nsAString
 &
 aURL
+)
+;
+static
+void
+InitializeStatics
+(
 )
 ;
 private
