db279ad2e6862ebba8e8876dea5acf6224a0244f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-07 21:25:56 +0000

    Bug 1456743 - P3. Add SourceBuffer.changeType experimental method. r=bryce,bz
    Non functional code.
    
    SourceBuffer.changeType allows to reconfigure a sourceBuffer so that a new codec type or new container format can be fed later.
    
    The new code is placed behind the media.mediasource.experimental.enabled pref.
    
    MozReview-Commit-ID: 5wj6J4uzLbA
    
    UltraBlame original commit: d89660b4978f082c7676af47e3c4da2789e20153

--- dom/media/mediasource/SourceBuffer.h
+++ dom/media/mediasource/SourceBuffer.h
@@ -511,6 +511,18 @@ ErrorResult
 aRv
 )
 ;
+void
+ChangeType
+(
+const
+nsAString
+&
+aType
+ErrorResult
+&
+aRv
+)
+;
 IMPL_EVENT_HANDLER
 (
 updatestart
@@ -1008,7 +1020,6 @@ RangeRemovalPromise
 >
 mPendingRemoval
 ;
-const
 MediaContainerType
 mType
 ;
