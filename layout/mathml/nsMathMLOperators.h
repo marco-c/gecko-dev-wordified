abab690694cb29b0abecc5cb227a9d37b15e7522
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:49:12 +0000

    Bug 745089 - Replace nscoord-specific positive-infinity/is-nan primitives with FloatingPoint.h uses.  r=dbaron
    UltraBlame original commit: 11c5f8019bca30e59f0a99df346b796d6d6f1aa6

--- layout/mathml/nsMathMLOperators.h
+++ layout/mathml/nsMathMLOperators.h
@@ -360,8 +360,16 @@ NS_MATHML_OPERATOR_RSPACE_ATTR
 #
 define
 NS_MATHML_OPERATOR_SIZE_INFINITY
-NS_IEEEPositiveInfinity
 (
+mozilla
+:
+:
+PositiveInfinity
+<
+float
+>
+(
+)
 )
 class
 nsMathMLOperators
