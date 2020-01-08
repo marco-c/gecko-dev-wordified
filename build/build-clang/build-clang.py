63f5696e9352b6aec056b91fe702b8daeceaf53e
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 20:38:52 +0000

    Backout changeset 99b28f8874bb (bug 1494216) to give time to toolchains to build without blocking other landings.
    UltraBlame original commit: 8444d933e44c34caeaa7de8f758bb7059c21222d

--- build/build-clang/build-clang.py
+++ build/build-clang/build-clang.py
@@ -5516,29 +5516,9 @@ extra_asmflags
 [
 ]
         
-#
-Avoid
-libLLVM
-internal
-function
-calls
-going
-through
-the
-PLT
-.
-        
 extra_ldflags
 =
 [
-'
--
-Wl
--
-Bsymbolic
--
-functions
-'
 ]
         
 if
