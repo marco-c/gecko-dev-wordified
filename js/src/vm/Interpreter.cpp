9e1bf5e06fc976c6f2162cff9a6b2bf38f6cbaff
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 22:33:32 +0000

    Bug 1479961 - Synthesize call edges from Interpret and EnterJit to all JSNatives, r=jonco
    UltraBlame original commit: c22b643cdf5168be413948a2437584426bfd8c06

--- js/src/vm/Interpreter.cpp
+++ js/src/vm/Interpreter.cpp
@@ -2602,6 +2602,7 @@ mutableThisv
 }
 static
 MOZ_NEVER_INLINE
+JS_HAZ_JSNATIVE_CALLER
 bool
 Interpret
 (
@@ -13127,6 +13128,7 @@ rref
 }
 static
 MOZ_NEVER_INLINE
+JS_HAZ_JSNATIVE_CALLER
 bool
 Interpret
 (
