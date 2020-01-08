74627419af6f0a4fa82c293a08a23d9921a89429
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 19:46:26 +0000

    Bug 1492689 [wpt PR 13091] - Try to unlock the global lock in StructuredLogger after reloading logging, a=testonly
    Automatic update from web-platform-testsMerge pull request #13091 from web-platform-tests/sync_c7b706deed40a88d583f690fadad434a06cb2561
    
    Merge pull request #13091 from sync_c7b706deed40a88d583f690fadad434a06cb2561
    --
    
    wpt-commits: e1c38af5d56cd7ad5cb93038900482bf90b0c3a0
    wpt-pr: 13091
    
    UltraBlame original commit: 5af864b7a2ecabdd7a74fb508ab293cec0fc2608

--- testing/web-platform/tests/tools/serve/serve.py
+++ testing/web-platform/tests/tools/serve/serve.py
@@ -59,6 +59,12 @@ localpaths
 import
 repo_root
 from
+six
+.
+moves
+import
+reload_module
+from
 manifest
 .
 sourcefile
@@ -66,6 +72,12 @@ import
 read_script_metadata
 js_meta_re
 parse_variants
+from
+mozlog
+.
+structuredlog
+import
+StructuredLogger
 from
 wptserve
 import
@@ -5058,17 +5070,26 @@ logging
 module
 unlocked
     
+reload_module
+(
+logging
+)
+    
 try
 :
         
-logging
+StructuredLogger
 .
-_releaseLock
+_lock
+.
+release
 (
 )
     
 except
-RuntimeError
+threading
+.
+ThreadError
 :
         
 pass
@@ -5143,17 +5164,26 @@ logging
 module
 unlocked
     
+reload_module
+(
+logging
+)
+    
 try
 :
         
-logging
+StructuredLogger
 .
-_releaseLock
+_lock
+.
+release
 (
 )
     
 except
-RuntimeError
+threading
+.
+ThreadError
 :
         
 pass
