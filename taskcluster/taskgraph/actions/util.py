72ce9a8078342f6eac1152e4088deb94f046366b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 04:07:40 +0000

    Backed out 1 changesets (bug 1472777) for linting failures create_interactive.py CLOSED TREE
    UltraBlame original commit: 021b2f3683cfa311166050cb84aff780fca60d73

--- taskcluster/taskgraph/actions/util.py
+++ taskcluster/taskgraph/actions/util.py
@@ -1145,15 +1145,6 @@ leave
 out
 decision_task_id
 .
-    
-Returns
-an
-updated
-label_to_taskid
-containing
-the
-new
-tasks
 "
 "
 "
@@ -1345,6 +1336,3 @@ label_to_taskid
 params
 decision_task_id
 )
-    
-return
-label_to_taskid
