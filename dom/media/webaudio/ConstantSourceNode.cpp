9fe1930c6c814be34b9b54e62adbae3b004ff90f
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 06:26:02 +0000

    Bug 1413284 - A RangeError exception is thrown if start/stop is called with a out of range param r=padenot
    MozReview-Commit-ID: G65bFgMu10D
    
    UltraBlame original commit: 4a6d4d1b11fde4dd848f6c28137a557cea20faad

--- dom/media/webaudio/ConstantSourceNode.cpp
+++ dom/media/webaudio/ConstantSourceNode.cpp
@@ -1018,7 +1018,7 @@ aRv
 .
 Throw
 (
-NS_ERROR_DOM_NOT_SUPPORTED_ERR
+NS_ERROR_RANGE_ERR
 )
 ;
 return
@@ -1101,7 +1101,7 @@ aRv
 .
 Throw
 (
-NS_ERROR_DOM_NOT_SUPPORTED_ERR
+NS_ERROR_RANGE_ERR
 )
 ;
 return
