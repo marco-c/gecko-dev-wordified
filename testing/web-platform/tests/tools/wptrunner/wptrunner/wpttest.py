3d3e33818423476da09dadb65970168d2df82afa
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 05:34:35 +0000

    Backed out 2 changesets (bug 1474569, bug 1474581) for wpt failures in test harness output on a CLOSED TREE
    Backed out changeset b39da1b094e9 (bug 1474581)
    Backed out changeset d8c824521e7b (bug 1474569)
    
    UltraBlame original commit: 25915c35842591babf7cfe5fc8ff0d5c3adbb327

--- testing/web-platform/tests/tools/wptrunner/wptrunner/wpttest.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/wpttest.py
@@ -1653,9 +1653,6 @@ False
 jsshell
 =
 False
-scripts
-=
-None
 )
 :
         
@@ -1685,15 +1682,6 @@ self
 jsshell
 =
 jsshell
-        
-self
-.
-scripts
-=
-scripts
-or
-[
-]
     
 classmethod
     
@@ -1780,31 +1768,6 @@ jsshell
 else
 False
         
-scripts
-=
-[
-v
-for
-(
-k
-v
-)
-in
-manifest_item
-.
-source_file
-.
-script_metadata
-if
-k
-=
-=
-b
-"
-script
-"
-]
-        
 return
 cls
 (
@@ -1845,10 +1808,6 @@ testdriver
 jsshell
 =
 jsshell
-                   
-scripts
-=
-scripts
 )
     
 property
