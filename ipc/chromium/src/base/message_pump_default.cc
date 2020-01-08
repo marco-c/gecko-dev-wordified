0e254c51847aecd3b5fd8a77bb425c240ffa4776
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 01:08:12 +0000

    Bug 1462784 - Annotate idle stacks in the chromium event loop. r=froydnj
    MozReview-Commit-ID: Ac8H02GKnmV
    
    UltraBlame original commit: 8d812a39d56f05c248f02845438a3cacdb3f111e

--- ipc/chromium/src/base/message_pump_default.cc
+++ ipc/chromium/src/base/message_pump_default.cc
@@ -183,6 +183,17 @@ Delegate
 delegate
 )
 {
+AUTO_PROFILER_LABEL
+(
+"
+MessagePumpDefault
+:
+:
+Run
+"
+OTHER
+)
+;
 DCHECK
 (
 keep_running_
@@ -360,7 +371,7 @@ Run
 :
 Wait
 "
-OTHER
+IDLE
 )
 ;
 {
@@ -413,7 +424,7 @@ Run
 :
 Wait
 "
-OTHER
+IDLE
 )
 ;
 {
