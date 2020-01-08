d6ad5c1efa49ae1a0531f2a34a13d3ed8eef5de5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 05:34:35 +0000

    Bug 1474581 - Add support for external scripts in wpt .any.js tests in jstests harness; r=jgraham
    UltraBlame original commit: b39da1b094e9f89a42ce93f305d4cf1c09c8b4a5

--- testing/web-platform/tests/tools/wptrunner/wptrunner/wpttest.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/wpttest.py
@@ -1653,6 +1653,9 @@ False
 jsshell
 =
 False
+scripts
+=
+None
 )
 :
         
@@ -1682,6 +1685,15 @@ self
 jsshell
 =
 jsshell
+        
+self
+.
+scripts
+=
+scripts
+or
+[
+]
     
 classmethod
     
@@ -1768,6 +1780,31 @@ jsshell
 else
 False
         
+scripts
+=
+[
+v
+for
+(
+k
+v
+)
+in
+manifest_item
+.
+source_file
+.
+script_metadata
+if
+k
+=
+=
+b
+"
+script
+"
+]
+        
 return
 cls
 (
@@ -1808,6 +1845,10 @@ testdriver
 jsshell
 =
 jsshell
+                   
+scripts
+=
+scripts
 )
     
 property
