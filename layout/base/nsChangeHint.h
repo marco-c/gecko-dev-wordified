910484de879dbb6a34b2f5918aede92463bb2032
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-08 03:06:03 +0000

    Backed out 9 changesets (bug 1430884) for frequently failing dom/animation/test/mozilla/test_restyles.html on android-em-4-3-armv7-api16 debug
    Backed out changeset 262dbc8daac1 (bug 1430884)
    Backed out changeset c3f9f3f66a98 (bug 1430884)
    Backed out changeset 39e1c4e3c8c9 (bug 1430884)
    Backed out changeset 4505e3f87b2e (bug 1430884)
    Backed out changeset e7e8977e0e92 (bug 1430884)
    Backed out changeset 6dbae2b8957d (bug 1430884)
    Backed out changeset 1e047fbcae2a (bug 1430884)
    Backed out changeset 52be07f10eaa (bug 1430884)
    Backed out changeset cec68a5b01da (bug 1430884)
    
    UltraBlame original commit: 3de09bc10e6ddc05348debebd754d296d648363a

--- layout/base/nsChangeHint.h
+++ layout/base/nsChangeHint.h
@@ -3055,12 +3055,6 @@ nsChangeHint_UpdateOpacityLayer
 nsChangeHint_AddOrRemoveTransform
 |
 \
-nsChangeHint_UpdateContainingBlock
-|
-\
-nsChangeHint_UpdateOverflow
-|
-\
 nsChangeHint_UpdatePostTransformOverflow
 |
 \
