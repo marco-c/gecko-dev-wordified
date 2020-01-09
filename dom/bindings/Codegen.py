7aeb786f1cce6fa9ea1c32598f0bcfe83d28efa9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:33:02 +0000

    Bug 1528286.  Remove bogus comment.  r=froydnj
    There are no relocations needed for string constants.
    
    Differential Revision: https://phabricator.services.mozilla.com/D19956
    
    UltraBlame original commit: 23f41d068c945fffba31d27c126d1603a91ac212

--- dom/bindings/Codegen.py
+++ dom/bindings/Codegen.py
@@ -14170,27 +14170,6 @@ isConstructing
 )
 {
               
-/
-/
-XXXbz
-wish
-I
-could
-get
-the
-name
-from
-the
-callee
-instead
-of
-              
-/
-/
-Adding
-more
-relocations
-              
 return
 ThrowConstructorWithoutNew
 (
