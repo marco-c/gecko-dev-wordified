579fa2820d8902ffc8d9c982aaf3b8d39fdc1ef7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 08:29:29 +0000

    Backed out 2 changesets (bug 1516337) for build bustage. CLOSED TREE
    Backed out changeset 3c4b8e03e722 (bug 1516337)
    Backed out changeset 4fc377013db5 (bug 1516337)
    
    UltraBlame original commit: def9811f03111c1fe26bb578255cc30e56a87414

--- third_party/rust/thread_local/src/lib.rs
+++ third_party/rust/thread_local/src/lib.rs
@@ -807,6 +807,10 @@ warn
 missing_docs
 )
 ]
+extern
+crate
+unreachable
+;
 #
 [
 macro_use
@@ -817,9 +821,6 @@ lazy_static
 ;
 mod
 thread_id
-;
-mod
-unreachable
 ;
 use
 std
@@ -1432,7 +1433,7 @@ id
 .
 wrapping_mul
 (
-0x9E37_79B9_7F4A_7C15
+0x9E3779B97F4A7C15
 )
 >
 >
