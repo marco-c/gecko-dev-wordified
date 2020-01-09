6356d29e2c7b8b37f39e3949e6eac2339ce3b186
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 00:40:54 +0000

    Bug 1497898 - Add a custom implementation of os.walk, r=ato
    Compared to the normal os.walk this has a couple of differences:
    
    * It returns lists of (name, stat) for filenames and directories,
      allowing callers to reuse the stat data without going back to the
      system to re-request it.
    
    * Directories are always returned as paths relative to the root, and
      the root itself is returned as the empty string.
    
    * It is non-recursive.
    
    There are also a few features missing that aren't required for our use
    cases.
    
    Depends on D8226
    
    Differential Revision: https://phabricator.services.mozilla.com/D8227
    
    UltraBlame original commit: 8d205db77cb6f668af766b9c83824c2520efc1fa

--- testing/web-platform/tests/tools/lint/tests/test_lint.py
+++ testing/web-platform/tests/tools/lint/tests/test_lint.py
@@ -3458,7 +3458,11 @@ patch
 (
             
 '
-os
+tools
+.
+lint
+.
+lint
 .
 walk
 '
@@ -3468,53 +3472,60 @@ return_value
 [
 (
 '
-.
 '
                            
 [
+(
 '
 dir_a
 '
+None
+)
+(
 '
 dir_b
 '
+None
+)
 ]
                            
 [
+(
 '
 file_a
 '
+None
+)
+(
 '
 file_b
 '
+None
+)
 ]
 )
                           
 (
-os
-.
-path
-.
-join
-(
-'
-.
-'
 '
 dir_a
 '
-)
                            
 [
 ]
                            
 [
+(
 '
 file_c
 '
+None
+)
+(
 '
 file_d
 '
+None
+)
 ]
 )
 ]
