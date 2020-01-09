3cddf91a83a93dfe2b88f08ad30b47fe6caf508b
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 01:01:22 +0000

    Merge mozilla-central to mozilla-inbound. a=merge
    UltraBlame original commit: 4115e3cf7ad0054a3846926eb7b85afb019ff385

--- dom/media/webrtc/MediaEngineWebRTCAudio.cpp
+++ dom/media/webrtc/MediaEngineWebRTCAudio.cpp
@@ -1249,6 +1249,10 @@ ApplySettings
 outputPrefs
 )
 ;
+mCurrentPrefs
+=
+outputPrefs
+;
 return
 NS_OK
 ;
@@ -2539,6 +2543,10 @@ NS_OK
 )
 )
 ;
+mCurrentPrefs
+=
+outputPrefs
+;
 return
 rv
 ;
@@ -3377,6 +3385,11 @@ NS_OK
 ;
 }
 )
+)
+;
+ApplySettings
+(
+mCurrentPrefs
 )
 ;
 MOZ_ASSERT
