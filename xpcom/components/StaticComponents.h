acc01fe67eb90b35ff38c94cfc979e7e5c0b469a
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:21:17 +0000

    Bug 1524688: Part 1b - Support static JavaScript components in browser_all_files_referenced. r=mccr8
    UltraBlame original commit: 042a975d3971e60f1edbaa8338332c0daa596b90

--- xpcom/components/StaticComponents.h
+++ xpcom/components/StaticComponents.h
@@ -179,6 +179,9 @@ class
 nsIFactory
 ;
 class
+nsIUTF8StringEnumerator
+;
+class
 nsISupports
 ;
 namespace
@@ -1294,6 +1297,15 @@ bool
 aInvalid
 =
 true
+)
+;
+static
+already_AddRefed
+<
+nsIUTF8StringEnumerator
+>
+GetComponentJSMs
+(
 )
 ;
 /
