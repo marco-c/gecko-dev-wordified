274a2a3b1c8bc27c2bce5554f54ef6fc4a75d9af
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 03:32:34 +0000

    Bug 1501878 - beetmove complete mars. r=tomprince,Callek
    Differential Revision: https://phabricator.services.mozilla.com/D11732
    
    UltraBlame original commit: cf994c5954587af5c7956e99bacc944673f6dc47

--- taskcluster/taskgraph/transforms/beetmover_repackage.py
+++ taskcluster/taskgraph/transforms/beetmover_repackage.py
@@ -1211,6 +1211,28 @@ installer
 msi
 '
 ]
+UPSTREAM_ARTIFACT_SIGNED_MAR_PATHS
+=
+[
+    
+'
+target
+.
+complete
+.
+mar
+'
+    
+'
+target
+.
+bz2
+.
+complete
+.
+mar
+'
+]
 #
 Voluptuous
 uses
@@ -1769,6 +1791,14 @@ signing
 msi
 "
         
+mar_signing_name
+=
+"
+mar
+-
+signing
+"
+        
 if
 job
 .
@@ -1815,6 +1845,16 @@ signing
 -
 l10n
 "
+            
+mar_signing_name
+=
+"
+mar
+-
+signing
+-
+l10n
+"
         
 dependencies
 =
@@ -1845,6 +1885,17 @@ signing
 upstream_deps
 [
 signing_name
+]
+            
+"
+mar
+-
+signing
+"
+:
+upstream_deps
+[
+mar_signing_name
 ]
         
 }
@@ -2103,6 +2154,10 @@ UPSTREAM_ARTIFACT_SIGNED_REPACKAGE_PATHS
 msi_signing_mapping
 =
 UPSTREAM_ARTIFACT_SIGNED_MSI_PATHS
+    
+mar_signing_mapping
+=
+UPSTREAM_ARTIFACT_SIGNED_MAR_PATHS
     
 artifact_prefix
 =
@@ -2418,6 +2473,18 @@ msi
 repackage
 '
 msi_signing_mapping
+)
+        
+(
+'
+mar
+-
+signing
+'
+'
+signing
+'
+mar_signing_mapping
 )
     
 ]
