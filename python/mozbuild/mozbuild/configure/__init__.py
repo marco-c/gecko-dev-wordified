9e1ed096a32b581bb7aec7ad80da20e616593462
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 05:17:21 +0000

    Bug 1512918 - Emit a warning instead of a hard error when an unknown option is used with configure --help. r=froydnj
    Differential Revision: https://phabricator.services.mozilla.com/D14114
    
    UltraBlame original commit: a2e01e197a1a1afc40fdc31a480b551f9d1af5c8

--- python/mozbuild/mozbuild/configure/__init__.py
+++ python/mozbuild/mozbuild/configure/__init__.py
@@ -2295,6 +2295,12 @@ queue_debug
                 
 yield
         
+self
+.
+_logger
+=
+logger
+        
 #
 Some
 callers
@@ -3120,9 +3126,8 @@ split
 0
 ]
             
-raise
-InvalidOptionError
-(
+msg
+=
 '
 Unknown
 option
@@ -3132,6 +3137,29 @@ s
 '
 %
 without_value
+            
+if
+self
+.
+_help
+:
+                
+self
+.
+_logger
+.
+warning
+(
+msg
+)
+            
+else
+:
+                
+raise
+InvalidOptionError
+(
+msg
 )
         
 #
