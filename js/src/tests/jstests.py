8cccf47d6bf755f869786bfa3287585f62e3b5f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-09 13:45:40 +0000

    Bug 1528770 - Support release_or_beta key in wpt-in-jstests; r=jgraham
    UltraBlame original commit: 4a7c9fc1c794d8bbfda5f08b4e19ed24b2a7aa50

--- js/src/tests/jstests.py
+++ js/src/tests/jstests.py
@@ -3693,6 +3693,26 @@ extras
 run_info_extras
 )
     
+run_info
+[
+"
+release_or_beta
+"
+]
+=
+xul_tester
+.
+test
+(
+"
+getBuildConfiguration
+(
+)
+.
+release_or_beta
+"
+)
+    
 path_filter
 =
 testloader
