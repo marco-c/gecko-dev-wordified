bffeecbc5bfac496e7d2205af122b22d334051d0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 22:33:28 +0000

    Bug 1577759 - add strings and urls for a keyboard check type for non-focusable and non-semantic clickable elements. r=gl,fluent-reviewers,flod
    Depends on D44232
    
    Differential Revision: https://phabricator.services.mozilla.com/D44233
    
    UltraBlame original commit: de24dca77618399972b7589ed854b66ccc2a7778

--- devtools/client/accessibility/constants.js
+++ devtools/client/accessibility/constants.js
@@ -81,6 +81,7 @@ FOCUSABLE_NO_SEMANTICS
 FOCUSABLE_POSITIVE_TABINDEX
 INTERACTIVE_NO_ACTION
 INTERACTIVE_NOT_FOCUSABLE
+MOUSE_INTERACTIVE_ONLY
 NO_FOCUS_VISIBLE
 }
 [
@@ -960,6 +961,13 @@ INTERACTIVE_NOT_FOCUSABLE
 :
 "
 Interactive_elements_must_be_focusable
+"
+[
+MOUSE_INTERACTIVE_ONLY
+]
+:
+"
+Clickable_elements_must_be_focusable_and_should_have_interactive_semantics
 "
 [
 NO_FOCUS_VISIBLE
