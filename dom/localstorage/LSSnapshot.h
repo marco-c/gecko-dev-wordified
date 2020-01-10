1c4e56e9530a4112e19c002a7c51fa7878307fe3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 07:16:27 +0000

    Bug 1546723 - Part 2: Make it more clear that checkpointing also notifies observers; r=asuth
    This patch renames the Checkpoint IPC message to CheckpointAndNotify. Other structures used by checkpointing are renamed too. Datastore methods SetItem/RemoveItem/Clear no longer call NotifyObservers, it's now up to RecvCheckpointAndNotify to call it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31197
    
    UltraBlame original commit: 52d60f7805dc4818b43ce73af6bfb09eba65a599

--- dom/localstorage/LSSnapshot.h
+++ dom/localstorage/LSSnapshot.h
@@ -146,7 +146,7 @@ class
 LSSnapshotInitInfo
 ;
 class
-LSWriteInfo
+LSWriteAndNotifyInfo
 ;
 class
 LSSnapshot
@@ -546,9 +546,9 @@ mValues
 ;
 nsTArray
 <
-LSWriteInfo
+LSWriteAndNotifyInfo
 >
-mWriteInfos
+mWriteAndNotifyInfos
 ;
 uint32_t
 mInitLength
