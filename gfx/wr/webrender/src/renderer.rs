05f4799eb6822062dc93c921561dd8dae1ca41a8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 07:10:13 +0000

    Bug 1551735 - Add a mode to the AsyncScreenshotGrabber to enable composition recording r=kvark
    The AsyncScreenshotGrabber now can operate in two modes:
    
    * `ProfilerScreenshots`, which does asynchronous scaling of the captured frames
      for inclusion in profiles by the Gecko Profiler; and
    * `CompositionRecorder`, which does not do any scaling and is used for visual
      metrics computations.
    
    The latter mode is exposed by on the `Renderer` via the `record_frame`,
    `map_recorded_frame`, and `release_composition_recorder_structures` methods.
    
    A different handle type (`RecordedFrameHandle`) is returned and consumed by
    these functions, but they translate between `RecordedFrameHandle` and
    `AsyncScreenshotHandle` when communicating with the underlying
    `AsyncScreenshotGrabber`.
    
    I considered making the `AsyncScreenshotGrabber` generic over its handle type,
    but the extra cost of monomorphization just to change the handle type did not
    seem worth it.
    
    Differential Revision: https://phabricator.services.mozilla.com/D32232
    
    UltraBlame original commit: 2e6ca6d6c527229f7a8e7d96e1616c8aeda2d0d2

--- gfx/wr/webrender/src/renderer.rs
+++ gfx/wr/webrender/src/renderer.rs
@@ -9708,6 +9708,17 @@ pub
 in
 crate
 )
+async_frame_recorder
+:
+Option
+<
+AsyncScreenshotGrabber
+>
+pub
+(
+in
+crate
+)
 async_screenshots
 :
 Option
@@ -12448,6 +12459,9 @@ Vec
 new
 (
 )
+async_frame_recorder
+:
+None
 async_screenshots
 :
 None
