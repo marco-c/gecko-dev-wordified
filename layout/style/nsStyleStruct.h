961e8ce0a8650aaf0ca8f24d24a7cd5ad0584d02
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 13:36:16 +0000

    Bug 1353164 Part 2 - Change dominant-baseline from reset to inherit r=heycam
    UltraBlame original commit: 5c8a0f41b86df61e7a1ef26b64fb355b5a11ab7d

--- layout/style/nsStyleStruct.h
+++ layout/style/nsStyleStruct.h
@@ -8189,7 +8189,7 @@ mozilla
 :
 :
 StyleLengthOrAuto
-mTextDecorationThickness
+mTextDecorationWidth
 ;
 }
 ;
@@ -15200,6 +15200,13 @@ mStrokeLinejoin
 NS_STYLE_STROKE_LINEJOIN_
 *
 uint8_t
+mDominantBaseline
+;
+/
+/
+NS_STYLE_DOMINANT_BASELINE_
+*
+uint8_t
 mTextAnchor
 ;
 /
@@ -15853,13 +15860,6 @@ mStopOpacity
 float
 mFloodOpacity
 ;
-uint8_t
-mDominantBaseline
-;
-/
-/
-NS_STYLE_DOMINANT_BASELINE_
-*
 uint8_t
 mVectorEffect
 ;
