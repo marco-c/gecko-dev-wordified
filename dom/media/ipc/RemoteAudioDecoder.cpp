550dc26fccebfdbbd438875048f0a677bbe71cd0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 15:42:10 +0000

    Bug 1356046 - Remove expired media telemetry; r=jya,alwu,bryce
    This removes all telemetry which expired in Firefox 69 or earlier, with the
    exceptions of the following, which we plan to renew:
    * AUDIO_TRACK_SILENCE_PROPORTION
    * MEDIA_AUTOPLAY_WOULD_BE_ALLOWED_COUNT
    * MEDIA_AUTOPLAY_WOULD_NOT_BE_ALLOWED_COUNT
    * MEDIACACHESTREAM_LENGTH_KB
    * MEDIA_MKV_CANPLAY_REQUESTED
    * MEDIA_PAGE_COUNT
    * MEDIA_PAGE_HAD_MEDIA_COUNT
    * VIDEO_DROPPED_FRAMES_PROPORTION
    * VIDEO_PLAY_TIME
    * VIDEO_HIDDEN_PLAY_TIME
    * VIDEO_HIDDEN_PLAY_TIME_PERCENTAGE
    * VIDEO_INFERRED_DECODE_SUSPEND_PERCENTAGE
    * VIDEO_INTER_KEYFRAME_AVERAGE_MS
    * VIDEO_INTER_KEYFRAME_MAX_MS
    * VIDEO_SUSPEND_RECOVERY_TIME_MS
    * VIDEO_VP9_BENCHMARK_FPS
    * WEB_AUDIO_BECOMES_AUDIBLE_TIME
    * WEBVTT_TRACK_KINDS
    
    Differential Revision: https://phabricator.services.mozilla.com/D37313
    
    UltraBlame original commit: b8661a3b5dc23c855fbebf7af1ac0c2c47ddd20d

--- dom/media/ipc/RemoteAudioDecoder.cpp
+++ dom/media/ipc/RemoteAudioDecoder.cpp
@@ -489,12 +489,6 @@ false
 nsCString
 errorDescription
 ;
-nsCString
-blacklistedD3D11Driver
-;
-nsCString
-blacklistedD3D9Driver
-;
 if
 (
 manager
@@ -510,10 +504,6 @@ Nothing
 )
 &
 success
-&
-blacklistedD3D11Driver
-&
-blacklistedD3D9Driver
 &
 errorDescription
 )
