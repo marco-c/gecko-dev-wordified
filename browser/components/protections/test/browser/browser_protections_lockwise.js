f800d14462fd3e91d99b5f5b18c1b34b121d16e3
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 13:10:57 +0000

    Bug 1566218 - Fix Intermittent[TV] test for lockwise card. r=ewright
    Differential Revision: https://phabricator.services.mozilla.com/D38258
    
    UltraBlame original commit: 1d0e9a449081ba119e0680c28e692d92d2a70a8b

--- browser/components/protections/test/browser/browser_protections_lockwise.js
+++ browser/components/protections/test/browser/browser_protections_lockwise.js
@@ -298,6 +298,13 @@ gBrowser
 }
 )
 ;
+const
+{
+getLoginData
+}
+=
+AboutProtectionsHandler
+;
 info
 (
 "
@@ -1307,6 +1314,12 @@ removeLogin
 (
 TEST_LOGIN2
 )
+;
+AboutProtectionsHandler
+.
+getLoginData
+=
+getLoginData
 ;
 await
 BrowserTestUtils
