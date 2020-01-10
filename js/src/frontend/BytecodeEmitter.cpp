6eb34d5ce1790b894ee84ea8c90f1a443d44c015
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 00:59:49 +0000

    Bug 1535994 - Part 5: Move tryNodeList to BytecodeSection class. r=jorendorff
    Differential Revision: https://phabricator.services.mozilla.com/D25735
    
    UltraBlame original commit: 7c763b9a2118ade0f7b0e372da562e415679c610

--- js/src/frontend/BytecodeEmitter.cpp
+++ js/src/frontend/BytecodeEmitter.cpp
@@ -798,6 +798,10 @@ notes_
 (
 cx
 )
+tryNoteList_
+(
+cx
+)
 {
 }
 BytecodeEmitter
@@ -893,10 +897,6 @@ numberList
 cx
 )
 scopeList
-(
-cx
-)
-tryNoteList
 (
 cx
 )
@@ -56937,7 +56937,13 @@ inPrologue
 )
 ;
 return
+bytecodeSection
+(
+)
+.
 tryNoteList
+(
+)
 .
 append
 (
