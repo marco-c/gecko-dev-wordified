25ea32233895f7bc5b9f2e2505eea579dd9f5a98
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-10 07:35:37 +0000

    Bug 1492121 - Rename env-vars MOZ_PROFILER_* to MOZ_BASE_PROFILER_* - r=njn
    Notice the extra 'BASE' in the env-var names.
    This is to control BaseProfiler separately from the Gecko Profiler.
    
    Differential Revision: https://phabricator.services.mozilla.com/D31928
    
    UltraBlame original commit: ac8da816859da7f17c74774af9856e1d91310804

--- mozglue/baseprofiler/public/BaseProfiler.h
+++ mozglue/baseprofiler/public/BaseProfiler.h
@@ -2093,14 +2093,14 @@ the
 profiler
 .
 If
-MOZ_PROFILER_STARTUP
+MOZ_BASE_PROFILER_STARTUP
 is
 set
 the
 profiler
+/
+/
 will
-/
-/
 also
 be
 started
@@ -7931,7 +7931,7 @@ mDocShellHistoryId
 /
 Get
 the
-MOZ_PROFILER_STARTUP
+MOZ_BASE_PROFILER_STARTUP
 *
 environment
 variables
