631e24728b8470022ab08dc53c440072dc8cc5b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 00:41:24 +0000

    Bug 1581511 - fix move constructor of ScopeExit; r=froydnj
    add `MOZ_GUARD_OBJECT_NOTIFIER_{PARAM,INIT}` to move constructor.
    
    Differential Revision: https://phabricator.services.mozilla.com/D46005
    
    UltraBlame original commit: a9b3127b31bd7a906e6ad99feb52f461a5fc98dd

--- mfbt/ScopeExit.h
+++ mfbt/ScopeExit.h
@@ -666,6 +666,7 @@ ScopeExit
 &
 &
 rhs
+MOZ_GUARD_OBJECT_NOTIFIER_PARAM
 )
 :
 mExitFunction
@@ -687,6 +688,8 @@ rhs
 mExecuteOnDestruction
 )
 {
+MOZ_GUARD_OBJECT_NOTIFIER_INIT
+;
 rhs
 .
 release
