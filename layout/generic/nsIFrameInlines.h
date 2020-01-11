3e9e87d2596bd68d8031947732fd0cf4a5a283a4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 03:59:56 +0000

    Bug 1102175 Part 1 - Propagate used writing-mode from body element to its ancestors. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D45481
    
    UltraBlame original commit: c6daecd1e4f5b14d2a6a353aa9d21f3d14724229

--- layout/generic/nsIFrameInlines.h
+++ layout/generic/nsIFrameInlines.h
@@ -1146,13 +1146,13 @@ void
 nsIFrame
 :
 :
-PropagateRootElementWritingMode
+PropagateWritingModeToSelfAndAncestors
 (
 mozilla
 :
 :
 WritingMode
-aRootElemWM
+aWM
 )
 {
 MOZ_ASSERT
@@ -1186,7 +1186,7 @@ f
 >
 mWritingMode
 =
-aRootElemWM
+aWM
 ;
 }
 }
