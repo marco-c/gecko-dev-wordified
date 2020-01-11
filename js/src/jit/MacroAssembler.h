6234968b83c447b1aceddb3691550c134c99ff73
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 14:21:47 +0000

    Bug 1605648 - Factor out MacroAssembler::loadJitCodeMaybeNoArgCheck. r=jandem
    When IonMonkey uses the no-arg-check entry point, we may still see lazy
    function clones. In this case the no-arg-check entry point is unavailable,
    but we can still fallback to the standard jitCodeRaw. This patch factors this
    fallback behaviour out of visitCallKnown.
    
    The new version guards the warmUpData tag bits instead of loading the
    function flags. This makes it compatible with removing the INTERPRETED_LAZY
    flag. In the future, the fallback case will no longer be necesarry once
    delazification reuses the same script and all function clones delazify at
    same time as their canonical one does.
    
    Depends on D58096
    
    Differential Revision: https://phabricator.services.mozilla.com/D58097
    
    UltraBlame original commit: 3c68364a0d9cf5c1cc4fd77161250423d59c70ec

--- js/src/jit/MacroAssembler.h
+++ js/src/jit/MacroAssembler.h
@@ -22776,7 +22776,7 @@ void
 loadJitCodeRaw
 (
 Register
-callee
+func
 Register
 dest
 )
@@ -22785,7 +22785,16 @@ void
 loadJitCodeNoArgCheck
 (
 Register
-callee
+func
+Register
+dest
+)
+;
+void
+loadJitCodeMaybeNoArgCheck
+(
+Register
+func
 Register
 dest
 )
