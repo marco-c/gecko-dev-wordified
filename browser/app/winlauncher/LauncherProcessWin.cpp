27741e3ba81e5a3f3837a01430238e4bc44b3599
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 01:19:00 +0000

    Backed out 8 changesets (bug 1542830) for causing spidermonkey bustages CLOSED TREE
    Backed out changeset b9f7fc8d0172 (bug 1542830)
    Backed out changeset b85f58fd5bbd (bug 1542830)
    Backed out changeset 0a9169ab2623 (bug 1542830)
    Backed out changeset 39a7c05d54ef (bug 1542830)
    Backed out changeset 7a3cbd2f59f0 (bug 1542830)
    Backed out changeset 268530552281 (bug 1542830)
    Backed out changeset df658ffb8599 (bug 1542830)
    Backed out changeset e7bef6486a38 (bug 1542830)
    
    UltraBlame original commit: 7c98a62b35284047bde4647d2424fcccf06d8ae1

--- browser/app/winlauncher/LauncherProcessWin.cpp
+++ browser/app/winlauncher/LauncherProcessWin.cpp
@@ -253,7 +253,7 @@ h
 #
 include
 "
-DllBlocklistInit
+DllBlocklistWin
 .
 h
 "
--- python/mozbuild/mozbuild/frontend/data.py
+++ python/mozbuild/mozbuild/frontend/data.py
@@ -7513,17 +7513,6 @@ h
 inc
 '
             
-context
-.
-config
-.
-substs
-[
-'
-LIB_SUFFIX
-'
-]
-            
 '
 .
 py
