a3815521cc5a5f865d5d9a1e5c206312d5f2dc91
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 11:37:35 +0000

    Bug 1522830: Part 3 - Change launcher blocklist generation to include child processes; r=bytesized
    Now that the launcher blocklist will support child processes, we need to add
    them to the launcher blocklist. The revised criteria the `Launcher` blocklist
    matches the criteria already in use by the `Legacy` blocklist.
    
    Differential Revision: https://phabricator.services.mozilla.com/D53675
    
    UltraBlame original commit: bade900883211c379effe09c20597b6f3acfc4d8

--- mozglue/dllservices/gen_dll_blocklist_defs.py
+++ mozglue/dllservices/gen_dll_blocklist_defs.py
@@ -2419,14 +2419,19 @@ BlocklistDescriptor
 '
 Launcher
 '
-[
+ALL_DEFINITION_LISTS
+flagspec
+=
+{
+                        
 '
-ALL_PROCESSES
+CHILD_PROCESSES
 '
-'
-BROWSER_PROCESS
-'
-]
+:
+{
+CHILD_PROCESSES_ONLY
+}
+}
 )
     
 BlocklistDescriptor
