91ec382f75359b0a3a3a4dfb68068ab319403e59
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-01-11 05:11:22 +0000

    Bug 1476339 - Set coverage LDFLAGS as part of configure. r=firefox-build-system-reviewers,chmanchester
    Differential Revision: https://phabricator.services.mozilla.com/D49830
    
    UltraBlame original commit: db0e1ce31ac38d41d30a5f20b58ce2f8fa2ad774

--- python/mozbuild/mozbuild/frontend/context.py
+++ python/mozbuild/mozbuild/frontend/context.py
@@ -3409,6 +3409,29 @@ LDFLAGS
 '
 )
 )
+            
+(
+'
+COVERAGE_LDFLAGS
+'
+context
+.
+config
+.
+substs
+.
+get
+(
+'
+COVERAGE_LDFLAGS
+'
+)
+(
+'
+LDFLAGS
+'
+)
+)
         
 )
         
@@ -4290,7 +4313,7 @@ CXXFLAGS
             
 (
 '
-COVERAGE
+COVERAGE_CFLAGS
 '
 context
 .
@@ -4304,6 +4327,7 @@ get
 COVERAGE_CFLAGS
 '
 )
+             
 (
 '
 CXXFLAGS
