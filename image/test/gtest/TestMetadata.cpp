689298ce06bddb76ad3a8ccbdfdb6d0f9cb040e4
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-12 13:17:42 +0000

    Bug 1620600 - Add flags to allow image decoders to produce sRGB output. r=tnikkel
    Currently we can only use the gfx.color_management.force_srgb pref to
    force all images to sRGB, or just accept device space. It would be nice
    to be able to test device space in our tests, as well as sRGB. This
    patch adds a surface flag which allows us to selectively output sRGB.
    
    This will also be useful for clipboard and re-encoding purposes, since
    they want a neutral output. In an ideal world we would just output the
    color profile and the pixel data in the original color space, but for
    now this is a relatively simple approach that works on all platforms and
    interops well with all applications.
    
    Differential Revision: https://phabricator.services.mozilla.com/D65734
    
    UltraBlame original commit: e6e5816403d958a0bcaf427195ca7b113263d01f

--- image/test/gtest/TestMetadata.cpp
+++ image/test/gtest/TestMetadata.cpp
@@ -761,9 +761,9 @@ DecoderFlags
 :
 :
 FIRST_FRAME_ONLY
-DefaultSurfaceFlags
-(
-)
+aTestCase
+.
+mSurfaceFlags
 )
 ;
 ASSERT_TRUE
@@ -1728,9 +1728,9 @@ get
 RasterSurfaceKey
 (
 imageSize
-DefaultSurfaceFlags
-(
-)
+testCase
+.
+mSurfaceFlags
 PlaybackType
 :
 :
