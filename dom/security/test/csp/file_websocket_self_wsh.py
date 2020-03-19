19595865310d4bc66edc665097fc6e33c945b45d
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-19 14:01:26 +0000

    Backed out changeset a5ce9219be07 (bug 1622682) for causing lint failures.
    CLOSED TREE
    
    UltraBlame original commit: 51edd86a9ec8445619794873bb56d046608c0e87

--- dom/security/test/csp/file_websocket_self_wsh.py
+++ dom/security/test/csp/file_websocket_self_wsh.py
@@ -1,10 +1,14 @@
+from
+mod_pywebsocket
+import
+msgutil
 def
 web_socket_do_extra_handshake
 (
 request
 )
 :
-    
+  
 pass
 def
 web_socket_transfer_data
@@ -12,5 +16,5 @@ web_socket_transfer_data
 request
 )
 :
-    
+  
 pass
