799366583c9e84f312254dcc12dd570ed880dfd7
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-03-23 22:30:46 +0000

    Bug 1622682 : removed unused imports and variables. r=sylvestre
    Differential Revision: https://phabricator.services.mozilla.com/D67011
    
    UltraBlame original commit: 1cd418755b5c60590babf18bc0297110037e106d

--- dom/security/test/mixedcontentblocker/file_main_bug803225_websocket_wsh.py
+++ dom/security/test/mixedcontentblocker/file_main_bug803225_websocket_wsh.py
@@ -1,14 +1,10 @@
-from
-mod_pywebsocket
-import
-msgutil
 def
 web_socket_do_extra_handshake
 (
 request
 )
 :
-  
+    
 pass
 def
 web_socket_transfer_data
@@ -16,8 +12,5 @@ web_socket_transfer_data
 request
 )
 :
-  
-resp
-=
-"
-"
+    
+pass
