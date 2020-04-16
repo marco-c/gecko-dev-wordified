7552af879977d02433e5455f5e5a9515fa4a4dfb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-16 01:08:59 +0000

    Bug 1363541 - Modernize the PermissionManager - part 2 - PermissionDelegateHandler to mozilla::PermissionDelegateHandler, r=timhuang
    Differential Revision: https://phabricator.services.mozilla.com/D69964
    
    UltraBlame original commit: 90b516fa9aa52dda7bad6525c04f3c496910fdd0

--- dom/base/Document.h
+++ dom/base/Document.h
@@ -868,9 +868,6 @@ class
 nsXULPrototypeElement
 ;
 class
-PermissionDelegateHandler
-;
-class
 nsIPermissionDelegateHandler
 ;
 struct
@@ -914,6 +911,9 @@ LangGroupFontPrefs
 ;
 class
 PendingAnimationTracker
+;
+class
+PermissionDelegateHandler
 ;
 class
 PresShell
