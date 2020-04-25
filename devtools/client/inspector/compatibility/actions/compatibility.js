9cb8a39b5e108db836ace3eeeac9d454f7b5dbeb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-04-25 19:04:24 +0000

    Bug 1590981: Make target browsers persistent. r=mtigley
    Depends on D70321
    
    Differential Revision: https://phabricator.services.mozilla.com/D70322
    
    UltraBlame original commit: 7e7c2e26b33602b04d5e7c0d0e08ef2d6699738e

--- devtools/client/inspector/compatibility/actions/compatibility.js
+++ devtools/client/inspector/compatibility/actions/compatibility.js
@@ -250,7 +250,11 @@ getDefaultTargetBrowsers
 const
 targetBrowsers
 =
-defaultTargetBrowsers
+UserSettings
+.
+getTargetBrowsers
+(
+)
 ;
 dispatch
 (
@@ -586,6 +590,13 @@ COMPATIBILITY_UPDATE_TARGET_BROWSERS_START
 ;
 try
 {
+UserSettings
+.
+setTargetBrowsers
+(
+targetBrowsers
+)
+;
 const
 {
 selectedNode
