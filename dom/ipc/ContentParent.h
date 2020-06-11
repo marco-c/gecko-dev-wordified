beec64246e4a50e37b346efaf801ba1298df7d87
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-06-11 23:23:17 +0000

    Bug 1637085: Use AsyncShutdown for ContentParent shutdown. r=nika
    Differential Revision: https://phabricator.services.mozilla.com/D74746
    
    UltraBlame original commit: 925f89a10fc8beb0e7c5ca02c8b873b0ebd138d7

--- dom/ipc/ContentParent.h
+++ dom/ipc/ContentParent.h
@@ -395,6 +395,13 @@ h
 #
 include
 "
+nsIAsyncShutdown
+.
+h
+"
+#
+include
+"
 nsIContentParent
 .
 h
@@ -777,6 +784,8 @@ public
 nsIDOMGeoPositionCallback
 public
 nsIDOMGeoPositionErrorCallback
+public
+nsIAsyncShutdownBlocker
 public
 nsIInterfaceRequestor
 public
@@ -1934,6 +1943,7 @@ NS_DECL_NSICONTENTPARENT
 NS_DECL_NSIOBSERVER
 NS_DECL_NSIDOMGEOPOSITIONCALLBACK
 NS_DECL_NSIDOMGEOPOSITIONERRORCALLBACK
+NS_DECL_NSIASYNCSHUTDOWNBLOCKER
 NS_DECL_NSIINTERFACEREQUESTOR
 /
 *
@@ -4064,6 +4074,16 @@ ContentParent
 >
 >
 sContentParents
+;
+void
+AddShutdownBlockers
+(
+)
+;
+void
+RemoveShutdownBlockers
+(
+)
 ;
 #
 if
