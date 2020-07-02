53f2c8e3330b925fb88f1257a1669e1211a04361
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-07-02 12:49:51 +0000

    Bug 1631327 - Do not fail IPC when predictor service fails to be obtained because of a rare possibiliy of reentrancy in XPCOM component manager, r=valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D80044
    
    UltraBlame original commit: 45b4ced93987c3e92ba6bb24a45f6d294f53a263

--- netwerk/ipc/NeckoParent.cpp
+++ netwerk/ipc/NeckoParent.cpp
@@ -4175,9 +4175,8 @@ rv
 NS_ENSURE_SUCCESS
 (
 rv
-IPC_FAIL_NO_REASON
+IPC_OK
 (
-this
 )
 )
 ;
@@ -4283,9 +4282,8 @@ rv
 NS_ENSURE_SUCCESS
 (
 rv
-IPC_FAIL_NO_REASON
+IPC_OK
 (
-this
 )
 )
 ;
@@ -4357,9 +4355,8 @@ rv
 NS_ENSURE_SUCCESS
 (
 rv
-IPC_FAIL_NO_REASON
+IPC_OK
 (
-this
 )
 )
 ;
