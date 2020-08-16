ee47de6dac4eebad97b022ffbbd8be16a6cc68b8
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-08-16 01:03:06 +0000

    Bug 1657231, part 1 - Only register the URL preloader as a reporter after it has been addrefed. r=kmag
    Normally this happens during initialization because we add the preloader
    as an observer, but that seems fragile.
    
    Differential Revision: https://phabricator.services.mozilla.com/D86029
    
    UltraBlame original commit: 5f4aab0ad04a647c69915762cd4084bc0b64ff37

--- js/xpconnect/loader/URLPreloader.h
+++ js/xpconnect/loader/URLPreloader.h
@@ -364,6 +364,8 @@ MallocSizeOf
 URLPreloader
 (
 )
+=
+default
 ;
 public
 :
@@ -1611,6 +1613,15 @@ ResolveURI
 nsIURI
 *
 uri
+)
+;
+static
+already_AddRefed
+<
+URLPreloader
+>
+Create
+(
 )
 ;
 Result
