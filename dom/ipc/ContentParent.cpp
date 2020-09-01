3e6ac29b7be67af94d2a046ee0a97a501a9270f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-01 12:23:06 +0000

    Bug 1658821 - Move Id from SessionHistoryInfo to LoadingSessionHistoryInfo, r=peterv
    Differential Revision: https://phabricator.services.mozilla.com/D86864
    
    UltraBlame original commit: c13d546cb1329bfd79a81d5d0b759f7efa6e4d6d

--- dom/ipc/ContentParent.cpp
+++ dom/ipc/ContentParent.cpp
@@ -44613,7 +44613,7 @@ aContext
 const
 uint64_t
 &
-aSessionHistoryEntryID
+aLoadID
 const
 nsID
 &
@@ -44639,7 +44639,7 @@ get_canonical
 >
 SessionHistoryCommit
 (
-aSessionHistoryEntryID
+aLoadID
 aChangeID
 )
 ;
