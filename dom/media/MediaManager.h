91face43ad4db2e6e896d7f4cf82ee283e58daf0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 19:11:27 +0000

    Bug 1654430 - Replace 200ms mainthread sleep on inserting/removing USB/bluetooth cam/mic/speaker with WaitFor() promise. r=achronop
    Differential Revision: https://phabricator.services.mozilla.com/D84458
    
    UltraBlame original commit: 788a6c7c52f1335a2c27e45d483ce8aa77ee3a2d

--- dom/media/MediaManager.h
+++ dom/media/MediaManager.h
@@ -302,6 +302,9 @@ mozilla
 class
 TaskQueue
 ;
+class
+MediaTimer
+;
 namespace
 dom
 {
@@ -2035,6 +2038,12 @@ GetUserMediaRequest
 >
 >
 mPendingGUMRequest
+;
+RefPtr
+<
+MediaTimer
+>
+mDeviceChangeTimer
 ;
 bool
 mCamerasMuted
