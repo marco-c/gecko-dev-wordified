9ef4ba63dcb4afd2d9c4a2074d0da501c3042dc0
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-09-28 19:00:46 +0000

    Bug 1663368 - Cherry-pick and retrofit webrtc.org bug 10069 (REVERT this before applying next webrtc.org update). r=dminor
    See https://crbug.com/webrtc/10069 for details and full diffs.
    
    Differential Revision: https://phabricator.services.mozilla.com/D90670
    
    UltraBlame original commit: 2776093cc8c8dc79f6ea6509baa33632bac4bb19

--- media/webrtc/trunk/webrtc/media/engine/simulcast_encoder_adapter.h
+++ media/webrtc/trunk/webrtc/media/engine/simulcast_encoder_adapter.h
@@ -521,6 +521,15 @@ send_stream
 ;
 }
 ;
+enum
+class
+StreamResolution
+{
+OTHER
+HIGHEST
+LOWEST
+}
+;
 /
 /
 Populate
@@ -547,8 +556,8 @@ int
 stream_index
 uint32_t
 start_bitrate_kbps
-bool
-highest_resolution_stream
+StreamResolution
+stream_resolution
 webrtc
 :
 :
