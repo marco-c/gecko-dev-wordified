8ffec3a53fb0e3653b69485cb0a278c0eb93f6cb
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-27 18:52:35 +0000

    Bug 1675675 - Only put mach_bootstrap paths in PYTHONPATH for mach python --no-virtualenv. r=firefox-build-system-reviewers,rstewart
    With PYTHONPATH containing other directories, such as the python ones,
    bad things can happen when the python script that is being run then
    goes on to subprocess.Popen a python process for a different virtualenv,
    or a different version, or whatever.
    
    Differential Revision: https://phabricator.services.mozilla.com/D96155
    
    UltraBlame original commit: f548a20f4b3f1185df43b2476d06faa01eb7a5a3

--- python/mach_commands.py
+++ python/mach_commands.py
@@ -334,6 +334,11 @@ if
 no_virtualenv
 :
             
+from
+mach_bootstrap
+import
+mach_sys_path
+            
 python_path
 =
 sys
@@ -353,9 +358,12 @@ pathsep
 .
 join
 (
-sys
+mach_sys_path
+(
+self
 .
-path
+topsrcdir
+)
 )
         
 else
