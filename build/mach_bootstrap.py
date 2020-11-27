c374b6ef397d01880891739245029451e1b70aee
Marco Castelluccio <mcastelluccio@mozilla.com>
2020-11-27 17:43:37 +0000

    Bug 1655845 - Collect OS version information in `mach` telemetry r=firefox-build-system-reviewers,dmajor
    We add new metrics `distro` and `distro_version`. Their meaning varies based on the actual OS:
    
    1. For Linux, the pair will be the name of the distribution and the distribution's version (e.g. `ubuntu`/`20.04`);
    
    2. for macOS, the pair will be the string `macos` and the macOS version (e.g. `10.15.7`); and
    
    3. for Windows, the pair will be the string (`windows`, `MAJOR.MINOR.BUILD`);
    
    4. and for all other OS'es, the first will be the value of `sys.platform`, and the version string will be empty.
    
    Differential Revision: https://phabricator.services.mozilla.com/D94781
    
    UltraBlame original commit: 6df2f1d27ab1750f915f6ba4703731bb33ff96fb

--- build/mach_bootstrap.py
+++ build/mach_bootstrap.py
@@ -3657,8 +3657,15 @@ mozbuild
 .
 telemetry
 import
+(
+        
 get_cpu_brand
+        
+get_distro_and_version
+        
 get_psutil_stats
+    
+)
     
 mach_metrics
 =
@@ -3707,6 +3714,31 @@ set
 get_cpu_brand
 (
 )
+)
+    
+distro
+version
+=
+get_distro_and_version
+(
+)
+    
+system_metrics
+.
+distro
+.
+set
+(
+distro
+)
+    
+system_metrics
+.
+distro_version
+.
+set
+(
+version
 )
     
 has_psutil
