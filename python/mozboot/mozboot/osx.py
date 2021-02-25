b26a09862a40ed6250efcae52b96c5ae76f06d40
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-25 00:08:15 +0000

    Bug 1687980: Make macOS quickstart docs more concise r=sylvestre,mhoye
    * Puts the docs in order, so that contributors aren't jumping to the
      middle of the page to install system tools, then back to the top to
      clone Firefox.
    * Removes docs on MacPorts since it's being removed in bug 1688263.
    * Removes step to manually install brew packages since that happens
       automatically in bootstrap now.
    * Simplifies mercurial installation docs
    * Removes unnecessary mozconfig-tweaking instructions
    * Removes almost-always-unnecessary DEFINE and troubleshooting
      information.
    
    Differential Revision: https://phabricator.services.mozilla.com/D102973
    
    UltraBlame original commit: e4a58d499e2f0cda56deab62799c15b6b8147344

--- python/mozboot/mozboot/osx.py
+++ python/mozboot/mozboot/osx.py
@@ -1488,26 +1488,33 @@ html
 "
         
 "
-#
-install
--
-via
--
-homebrew
 for
 guidance
 on
 how
 to
-install
-Python
-on
+prepare
 your
+system
+to
+build
+Firefox
+.
+Perhaps
 "
         
 "
-system
-.
+you
+need
+to
+update
+Xcode
+or
+install
+Python
+using
+brew
+?
 "
     
 )
