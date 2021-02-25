aa5eee22474bf2164c37734ace86c4f22dcbcceb
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-02-25 00:09:40 +0000

    Bug 1693021 - [puppeteer] Add --enable-webrender argument to mach command. r=remote-protocol-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D105361
    
    UltraBlame original commit: b4e561567e1e1ddf31c26ed81a5d95d1804833c0

--- remote/mach_commands.py
+++ remote/mach_commands.py
@@ -2761,6 +2761,21 @@ default
 preferences
 .
         
+enable_webrender
+:
+          
+Boolean
+to
+indicate
+whether
+to
+enable
+WebRender
+compositor
+in
+Gecko
+.
+        
 write_results
 :
           
@@ -3025,6 +3040,28 @@ PUPPETEER_PRODUCT
 "
 firefox
 "
+            
+env
+[
+"
+MOZ_WEBRENDER
+"
+]
+=
+"
+%
+d
+"
+%
+params
+.
+get
+(
+"
+enable_webrender
+"
+False
+)
         
 command
 =
@@ -3585,6 +3622,39 @@ add_argument
         
 "
 -
+-
+enable
+-
+webrender
+"
+        
+action
+=
+"
+store_true
+"
+        
+help
+=
+"
+Enable
+the
+WebRender
+compositor
+in
+Gecko
+.
+"
+    
+)
+    
+p
+.
+add_argument
+(
+        
+"
+-
 z
 "
 "
@@ -3992,6 +4062,10 @@ ci
 False
         
 enable_fission
+=
+False
+        
+enable_webrender
 =
 False
         
@@ -4460,6 +4534,12 @@ headless
 "
 :
 headless
+            
+"
+enable_webrender
+"
+:
+enable_webrender
             
 "
 extra_prefs
