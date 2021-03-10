ece742ec938a366a66acd8c1907a7a28158b2abb
Marco Castelluccio <mcastelluccio@mozilla.com>
2021-03-10 17:44:40 +0000

    Bug 1694172 - Decrease test timeout for some talos tests. r=perftest-reviewers
    This patch decreases the test timeout for some Talos tests which are hitting task timeouts. It also increases the task timeout for one of the tests.
    
    Differential Revision: https://phabricator.services.mozilla.com/D106638
    
    UltraBlame original commit: f7fa859944a362467dfb835d3eff18c54fa68b4b

--- testing/talos/talos/test.py
+++ testing/talos/talos/test.py
@@ -1242,6 +1242,10 @@ cycles
 =
 20
     
+timeout
+=
+600
+    
 extensions
 =
 [
@@ -1731,6 +1735,10 @@ sessionRestored
 "
 "
 "
+    
+timeout
+=
+300
     
 preferences
 =
@@ -2467,6 +2475,10 @@ tppagecycles
 =
 20
     
+timeout
+=
+600
+    
 gecko_profile_entries
 =
 1000000
@@ -2585,6 +2597,10 @@ manifest
 tppagecycles
 =
 20
+    
+timeout
+=
+600
     
 gecko_profile_entries
 =
@@ -2782,6 +2798,10 @@ tppagecycles
 =
 20
     
+timeout
+=
+600
+    
 gecko_profile_entries
 =
 1000000
@@ -2975,6 +2995,10 @@ manifest
 tppagecycles
 =
 5
+    
+timeout
+=
+600
     
 gecko_profile_entries
 =
@@ -3395,6 +3419,10 @@ True
 tpmozafterpaint
 =
 False
+    
+timeout
+=
+600
     
 gecko_profile_interval
 =
@@ -3843,6 +3871,10 @@ tpchrome
 =
 False
     
+timeout
+=
+600
+    
 gecko_profile_interval
 =
 10
@@ -4013,6 +4045,10 @@ False
 tpchrome
 =
 False
+    
+timeout
+=
+600
     
 gecko_profile_interval
 =
@@ -4692,6 +4728,10 @@ gecko_profile_entries
 tpscrolltest
 =
 True
+    
+timeout
+=
+2400
     
 "
 "
@@ -5780,6 +5820,10 @@ tpchrome
 =
 False
     
+timeout
+=
+600
+    
 gecko_profile_interval
 =
 10
@@ -5918,6 +5962,10 @@ tpchrome
 =
 False
     
+timeout
+=
+600
+    
 gecko_profile_interval
 =
 1
@@ -6012,6 +6060,10 @@ tpchrome
 =
 False
     
+timeout
+=
+600
+    
 gecko_profile_interval
 =
 1
@@ -6101,6 +6153,10 @@ False
 tpchrome
 =
 False
+    
+timeout
+=
+600
     
 gecko_profile_interval
 =
@@ -6278,6 +6334,10 @@ True
 tpchrome
 =
 False
+    
+timeout
+=
+600
     
 preferences
 =
@@ -6462,6 +6522,10 @@ unit
 "
 fps
 "
+    
+timeout
+=
+600
 register_test
 (
 )
@@ -6909,6 +6973,10 @@ tpchrome
 =
 False
     
+timeout
+=
+600
+    
 gecko_profile_interval
 =
 2
@@ -7063,6 +7131,10 @@ tpchrome
 =
 False
     
+timeout
+=
+600
+    
 gecko_profile_interval
 =
 2
@@ -7208,6 +7280,10 @@ False
 tpchrome
 =
 False
+    
+timeout
+=
+600
     
 gecko_profile_interval
 =
