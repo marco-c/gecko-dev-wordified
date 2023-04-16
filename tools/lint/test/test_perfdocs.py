c29675f8d6259811933c532326fc4f1118904204
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-16 23:16:43 +0000

    Bug 1691558 - Strengthen a log to confirm that the file will be searched r=sparky
    Differential Revision: https://phabricator.services.mozilla.com/D107391
    
    UltraBlame original commit: 866067c51ed6b7fddfbadc0b6b0f401251c8b3d8

--- tools/lint/test/test_perfdocs.py
+++ tools/lint/test/test_perfdocs.py
@@ -1046,6 +1046,42 @@ fetch_perfdocs_tree
 (
 )
     
+expected
+=
+"
+Found
+1
+perfdocs
+directories
+"
+    
+args
+_
+=
+logger
+.
+log
+.
+call_args
+    
+assert
+expected
+in
+args
+[
+0
+]
+    
+assert
+logger
+.
+log
+.
+call_count
+=
+=
+1
+    
 assert
 gatherer
 .
@@ -1974,12 +2010,19 @@ name
 ?
 "
         
+os
+.
+path
+.
+realpath
+(
 perfdocs_sample
 [
 "
 config
 "
 ]
+)
     
 )
     
