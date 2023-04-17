31b10898eb108b16b5f7a684f61488835393f359
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 06:28:17 +0000

    Bug 1710935 - [marionette] Always return proxy capabilities for WebDriver:NewSession. r=marionette-reviewers,webdriver-reviewers,jdescottes
    Differential Revision: https://phabricator.services.mozilla.com/D115129
    
    UltraBlame original commit: 0cc6b9d31a1ddc5789da9ce99125bd05d7f10665

--- testing/marionette/harness/marionette_harness/tests/unit/test_proxy.py
+++ testing/marionette/harness/marionette_harness/tests/unit/test_proxy.py
@@ -194,35 +194,6 @@ tearDown
 )
     
 def
-test_proxy_object_none_by_default
-(
-self
-)
-:
-        
-self
-.
-marionette
-.
-start_session
-(
-)
-        
-self
-.
-assertNotIn
-(
-"
-proxy
-"
-self
-.
-marionette
-.
-session_capabilities
-)
-    
-def
 test_proxy_object_in_returned_capabilities
 (
 self
@@ -397,7 +368,7 @@ proxy
 )
     
 def
-test_proxy_type_manual_without_port
+test_proxy_type_manual
 (
 self
 )
