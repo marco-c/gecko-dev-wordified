ba4fe62ba4b9638ca7d82c2827d7a03d26e5b3ef
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 05:53:07 +0000

    Bug 1701695 - Add NumberFormat::TryCreate; r=tcampbell
    This adds a fallible factory method to create new NumberFormat instances. This
    allows us to report initialization errors at time of initialization, rather than
    when format is called, and remove internal checks in the implementation for
    successful initialization.
    
    The existing fluent code assumes that creating a NumberFormat instance always
    succeeds. This patch updates that code to handle failures.
    
    Differential Revision: https://phabricator.services.mozilla.com/D114593
    
    UltraBlame original commit: e88dd41c0a9000090b88b332be4af27c94fae9e2

--- config/check_vanilla_allocations.py
+++ config/check_vanilla_allocations.py
@@ -1587,6 +1587,30 @@ o
             
 continue
         
+#
+Ignore
+allocations
+from
+the
+m
+-
+c
+intl
+/
+components
+implementations
+.
+        
+if
+"
+intl_components
+"
+in
+filename
+:
+            
+continue
+        
 fn
 =
 m
