ca0fafe623f7960157d95c2c6ab20201dc19b7ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 09:41:22 +0000

    Backed out changeset 15c6a16f5828 (bug 1714688) for docker images bustage. CLOSED TREE
    UltraBlame original commit: d9ce7bf4cebb533721cb204ac41a87a182bef766

--- python/mozbuild/mozbuild/action/tooltool.py
+++ python/mozbuild/mozbuild/action/tooltool.py
@@ -408,12 +408,54 @@ PACKAGE
 HAWK_VER
 =
 1
+PY3
+=
+sys
+.
+version_info
+[
+0
+]
+=
+=
+3
+if
+PY3
+:
+    
+six_binary_type
+=
+bytes
+    
+unicode
+=
+(
+        
+str
+#
+Silence
+pyflakes
+from
+reporting
+undefined
+name
+'
+unicode
+'
+in
+Python
+3
+.
+    
+)
+    
 import
 urllib
 .
 request
 as
 urllib2
+    
 from
 http
 .
@@ -421,6 +463,7 @@ client
 import
 HTTPSConnection
 HTTPConnection
+    
 from
 urllib
 .
@@ -428,12 +471,14 @@ parse
 import
 urlparse
 urljoin
+    
 from
 urllib
 .
 request
 import
 Request
+    
 from
 urllib
 .
@@ -441,6 +486,34 @@ error
 import
 HTTPError
 URLError
+else
+:
+    
+six_binary_type
+=
+str
+    
+import
+urllib2
+    
+from
+httplib
+import
+HTTPSConnection
+HTTPConnection
+    
+from
+urllib2
+import
+Request
+HTTPError
+URLError
+    
+from
+urlparse
+import
+urlparse
+urljoin
 log
 =
 logging
@@ -1223,6 +1296,10 @@ req
 )
 :
     
+if
+PY3
+:
+        
 return
 req
 .
@@ -1230,6 +1307,13 @@ data
 is
 not
 None
+    
+return
+req
+.
+has_data
+(
+)
 def
 get_hexdigest
 (
@@ -1575,7 +1659,7 @@ if
 isinstance
 (
 val
-bytes
+six_binary_type
 )
 :
         
@@ -1707,7 +1791,7 @@ if
 isinstance
 (
 part
-bytes
+six_binary_type
 )
 else
 part
@@ -1940,7 +2024,7 @@ if
 isinstance
 (
 val
-bytes
+six_binary_type
 )
 :
         
@@ -1968,12 +2052,19 @@ normalize_string
 (
     
 mac_type
+    
 timestamp
+    
 nonce
+    
 method
+    
 name
+    
 host
+    
 port
+    
 content_hash
 )
 :
@@ -2161,7 +2252,7 @@ not
 isinstance
 (
 normalized
-bytes
+six_binary_type
 )
 :
         
@@ -2181,7 +2272,7 @@ not
 isinstance
 (
 access_token
-bytes
+six_binary_type
 )
 :
         
@@ -2287,11 +2378,26 @@ req
 )
 :
         
+if
+PY3
+:
+            
 data
 =
 req
 .
 data
+        
+else
+:
+            
+data
+=
+req
+.
+get_data
+(
+)
         
 content_hash
 =
@@ -3569,18 +3675,23 @@ record_list
 required_fields
 =
 [
+            
 "
 filename
 "
+            
 "
 size
 "
+            
 "
 algorithm
 "
+            
 "
 digest
 "
+        
 ]
         
 if
@@ -4586,6 +4697,12 @@ manifest_file
 "
 r
 "
+if
+PY3
+else
+"
+rb
+"
 )
 as
 f
@@ -4704,6 +4821,7 @@ log
 .
 error
 (
+            
 "
 failed
 to
@@ -4719,14 +4837,19 @@ s
 %
 s
 "
+            
 %
 (
+                
 manifest_file
+                
 str
 (
 e
 )
+            
 )
+        
 )
         
 return
@@ -4863,6 +4986,7 @@ log
 .
 error
 (
+            
 "
 failed
 to
@@ -4878,14 +5002,19 @@ s
 %
 s
 "
+            
 %
 (
+                
 manifest_file
+                
 str
 (
 e
 )
+            
 )
+        
 )
         
 return
@@ -5362,6 +5491,10 @@ append
 old_fr
 )
     
+if
+PY3
+:
+        
 with
 open
 (
@@ -5375,7 +5508,36 @@ w
 as
 output
 :
+            
+new_manifest
+.
+dump
+(
+output
+fmt
+=
+"
+json
+"
+)
+    
+else
+:
         
+with
+open
+(
+manifest_file
+mode
+=
+"
+wb
+"
+)
+as
+output
+:
+            
 new_manifest
 .
 dump
@@ -6592,6 +6754,7 @@ log
 .
 error
 (
+            
 "
 failed
 to
@@ -6607,14 +6770,19 @@ s
 %
 s
 "
+            
 %
 (
+                
 manifest_file
+                
 str
 (
 e
 )
+            
 )
+        
 )
         
 return
@@ -8466,6 +8634,10 @@ dumps
 batch
 )
     
+if
+PY3
+:
+        
 data
 =
 data
@@ -9155,17 +9327,20 @@ batch
 batch
 =
 {
+        
 "
 message
 "
 :
 message
+        
 "
 files
 "
 :
 {
 }
+    
 }
     
 for
@@ -9724,7 +9899,9 @@ auth_file
 data
 =
 [
+        
 {
+            
 "
 op
 "
@@ -9732,12 +9909,15 @@ op
 "
 set_visibility
 "
+            
 "
 visibility
 "
 :
 visibility
+        
 }
+    
 ]
     
 return
@@ -9761,7 +9941,9 @@ auth_file
 data
 =
 [
+        
 {
+            
 "
 op
 "
@@ -9769,7 +9951,9 @@ op
 "
 delete_instances
 "
+        
 }
+    
 ]
     
 return
@@ -10346,6 +10530,7 @@ get
 base_url
 "
 )
+            
 options
 .
 get
@@ -10354,6 +10539,7 @@ get
 digest
 "
 )
+            
 options
 .
 get
