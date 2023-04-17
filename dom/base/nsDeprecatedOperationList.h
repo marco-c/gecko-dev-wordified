8322f2f3ea399c4e10f1498b94c5166df5ea5fb1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 04:09:12 +0000

    Bug 973604 - Mark set/releaseCapture() as deprecated r=annevk,edgar
    Differential Revision: https://phabricator.services.mozilla.com/D113195
    
    UltraBlame original commit: df43273ec88cc9ebc17ed65553f62dbedb93a434

--- dom/base/nsDeprecatedOperationList.h
+++ dom/base/nsDeprecatedOperationList.h
@@ -354,3 +354,15 @@ DEPRECATED_OPERATION
 (
 FormSubmissionUntrustedEvent
 )
+DEPRECATED_OPERATION
+(
+ElementSetCapture
+)
+DEPRECATED_OPERATION
+(
+ElementReleaseCapture
+)
+DEPRECATED_OPERATION
+(
+DocumentReleaseCapture
+)
