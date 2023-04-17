8d2016fde020f20c85287a96f35ccd521b25f6f8
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-17 10:17:48 +0000

    Bug 1713300: Extend this timeout even more to give Windows aarch64 more time. r=jib
    Differential Revision: https://phabricator.services.mozilla.com/D117697
    
    UltraBlame original commit: 72de1a61b74961add0c99217078eed8d16e4e7a7

--- dom/media/webrtc/tests/mochitests/iceTestUtils.js
+++ dom/media/webrtc/tests/mochitests/iceTestUtils.js
@@ -455,7 +455,7 @@ await
 gatherWithTimeout
 (
 pc
-4000
+8000
 )
 ;
 info
@@ -615,7 +615,7 @@ await
 gatherWithTimeout
 (
 pc
-4000
+8000
 context
 )
 ;
@@ -761,7 +761,7 @@ await
 gatherWithTimeout
 (
 pc
-4000
+8000
 context
 )
 ;
@@ -880,7 +880,7 @@ await
 gatherWithTimeout
 (
 pc
-4000
+8000
 context
 )
 ;
@@ -1108,7 +1108,7 @@ await
 gatherWithTimeout
 (
 pc
-4000
+8000
 context
 )
 ;
@@ -1336,7 +1336,7 @@ await
 gatherWithTimeout
 (
 pc
-4000
+8000
 context
 )
 ;
@@ -1615,7 +1615,7 @@ await
 gatherWithTimeout
 (
 pc
-4000
+8000
 context
 )
 ;
