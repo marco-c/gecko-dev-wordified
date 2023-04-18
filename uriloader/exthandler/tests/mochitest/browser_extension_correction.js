760387a749e82a3aef792c39e85e9a65af89f85d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 05:51:29 +0000

    Bug 1721217 - Part 3: Fix tests that try to open URIs for download, r=nika,preferences-reviewers
    Previously, the tests were waiting for the load event, which was being emitted
    for the initial about:blank. With the pref enabled, this event is no longer
    propagated/fired to the content process. Instead of notifying the content
    process that it needs to emit a load event for about:blank, we can instead make
    the tests wait for a STATE_STOP event.
    
    Differential Revision: https://phabricator.services.mozilla.com/D126843
    
    UltraBlame original commit: 885ced5f4bb465ac60e1ff7a4fb07cefaec1dfc1

--- uriloader/exthandler/tests/mochitest/browser_extension_correction.js
+++ uriloader/exthandler/tests/mochitest/browser_extension_correction.js
@@ -1090,7 +1090,10 @@ BrowserTestUtils
 .
 openNewForegroundTab
 (
+{
 gBrowser
+opening
+:
 TEST_PATH
 +
 "
@@ -1102,6 +1105,13 @@ foo
 =
 bar
 "
+waitForLoad
+:
+false
+waitForStateStop
+:
+true
+}
 )
 .
 then
