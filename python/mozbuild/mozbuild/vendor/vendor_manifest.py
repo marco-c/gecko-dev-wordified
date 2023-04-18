657d6eb09d8eeb612a35f4505cd0292e56a15654
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 19:47:27 +0000

    Bug 1761111: Small typo and qualitity of life improvements r=jewilde
    This creates a way to override the 'tracking: tag' mechnaism
    of a moz.yaml file without having to edit it. Useful to
    test a library update when there is no new tag available.
    
    Differential Revision: https://phabricator.services.mozilla.com/D141901
    
    UltraBlame original commit: ce12879ae17f11e9a72dc009ddede55eecb01cb9

--- python/mozbuild/mozbuild/vendor/vendor_manifest.py
+++ python/mozbuild/mozbuild/vendor/vendor_manifest.py
@@ -304,6 +304,29 @@ tracking
 ]
             
 if
+revision
+=
+=
+"
+tip
+"
+:
+                
+ref
+timestamp
+=
+self
+.
+source_host
+.
+upstream_commit
+(
+"
+HEAD
+"
+)
+            
+elif
 ref_type
 =
 =
@@ -3697,7 +3720,7 @@ log
                 
 logging
 .
-WARNIGN
+WARNING
                 
 "
 header_files_warning
@@ -3766,7 +3789,7 @@ log
             
 logging
 .
-DEBUG
+INFO
             
 "
 vendor
