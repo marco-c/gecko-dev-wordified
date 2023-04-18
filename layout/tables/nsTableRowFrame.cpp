08d82fe692bf4557a8089e94c5b9e79b9d7530c6
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-18 10:41:52 +0000

    Bug 1697979 - Part 1: Reuse previously built stacking context display items without merging r=mstange
    Differential Revision: https://phabricator.services.mozilla.com/D128413
    
    UltraBlame original commit: 1513c7b1008569018b63d32b8447532123c89881

--- layout/tables/nsTableRowFrame.cpp
+++ layout/tables/nsTableRowFrame.cpp
@@ -9688,6 +9688,17 @@ IsBorderCollapse
 )
 )
 {
+const
+bool
+rebuild
+=
+StaticPrefs
+:
+:
+layout_display_list_retain_sc
+(
+)
+;
 GetParent
 (
 )
@@ -9703,7 +9714,7 @@ GetPosition
 (
 )
 aDisplayItemKey
-false
+rebuild
 )
 ;
 }
@@ -9776,7 +9787,7 @@ GetPosition
 (
 )
 aDisplayItemKey
-false
+aRebuildDisplayItems
 )
 ;
 }
