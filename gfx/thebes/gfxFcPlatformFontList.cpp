407461840487cf3f4d3c64d5c6f89686d90d71f5
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 09:49:33 +0000

    Backed out changeset 32689459fc62 (bug 1782731) for causing reftest failures. CLOSED TREE
    UltraBlame original commit: 3d354d73dc2a2c1f5c8251959560dcceba172f33

--- gfx/thebes/gfxFcPlatformFontList.cpp
+++ gfx/thebes/gfxFcPlatformFontList.cpp
@@ -16039,10 +16039,15 @@ happen
 uint32_t
 limit
 =
-StaticPrefs
+gfxPlatformGtk
 :
 :
-gfx_font_rendering_fontconfig_max_generic_substitutions
+GetPlatform
+(
+)
+-
+>
+MaxGenericSubstitions
 (
 )
 ;
