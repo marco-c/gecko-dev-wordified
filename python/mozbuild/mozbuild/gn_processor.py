cf72b99e86399e560d894b254b2508fe6daadd6c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 05:08:48 +0000

    Bug 1738845 - Add webrtc configs for arm and ppc64 linux. r=mjf
    Adding arm alone breaks ppc64el.
    
    Differential Revision: https://phabricator.services.mozilla.com/D134738
    
    UltraBlame original commit: 67ee3490fe22226bc860ce149dc5e80cc775166e

--- python/mozbuild/mozbuild/gn_processor.py
+++ python/mozbuild/mozbuild/gn_processor.py
@@ -5036,11 +5036,15 @@ arm64
             
 if
 target_os
-=
-=
+in
+(
 "
 android
 "
+"
+linux
+"
+)
 :
                 
 target_cpus
@@ -5074,6 +5078,24 @@ append
 (
 "
 x86
+"
+)
+            
+if
+target_os
+=
+=
+"
+linux
+"
+:
+                
+target_cpus
+.
+append
+(
+"
+ppc64
 "
 )
             
