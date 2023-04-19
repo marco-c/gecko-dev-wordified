0f72cdb43d00c0c08bc0727eaa2150bef1032e91
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-19 22:52:05 +0000

    Bug 1740530 - patch 4 - Use harfbuzz color-palette access functions, instead of rolling our own. r=gfx-reviewers,lsalzman
    Depends on D152038
    
    Differential Revision: https://phabricator.services.mozilla.com/D153870
    
    UltraBlame original commit: 02d6294b7a6d4e58e12ad1ca7259eb2cbdf53ce1

--- gfx/thebes/COLRFonts.h
+++ gfx/thebes/COLRFonts.h
@@ -110,6 +110,9 @@ h
 struct
 hb_blob_t
 ;
+struct
+hb_face_t
+;
 namespace
 mozilla
 {
@@ -175,9 +178,9 @@ PaintGlyphLayers
 hb_blob_t
 *
 aCOLR
-hb_blob_t
+hb_face_t
 *
-aCPAL
+aFace
 const
 COLRBaseGlyphRecord
 *
