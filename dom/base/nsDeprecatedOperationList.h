a3c9e77def9b2b7d32b22abae76e1eace929c944
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 12:33:12 +0000

    Bug 1294360 - Remove mozCurrentTransform. r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D160998
    
    UltraBlame original commit: 183400ded7d4b3db3d72c4a4551123b92593a67a

--- dom/base/nsDeprecatedOperationList.h
+++ dom/base/nsDeprecatedOperationList.h
@@ -364,13 +364,5 @@ IDBMutableFileOpen
 )
 DEPRECATED_OPERATION
 (
-MozCurrentTransform
-)
-DEPRECATED_OPERATION
-(
-MozCurrentTransformInverse
-)
-DEPRECATED_OPERATION
-(
 MozPreservesPitchDeprecatedPrefix
 )
