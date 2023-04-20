cd99ea4426225ee40d0a2cb1f1fcf6110d0150af
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:33:51 +0000

    Bug 1800920 - Vendor libwebrtc from 0cf140d720
    Upstream commit: https://webrtc.googlesource.com/src/+/0cf140d720e15f5b9064190511ef51746a6efe84
        Rewrite AudioState null poller to use TaskQueueBase interface
    
        Bug: webrtc:9702, webrtc:11318
        Change-Id: If39871b8b2b1ccbfb17827bc795874f9ecc317d0
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/271289
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Reviewed-by: Jakob Ivarsson‎ <jakobiwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#37797}
    
    UltraBlame original commit: 7a5b52f06e54619326b2525962afc9b08bed46e4

--- third_party/libwebrtc/audio/channel_receive.cc
+++ third_party/libwebrtc/audio/channel_receive.cc
@@ -407,15 +407,6 @@ include
 "
 rtc_base
 /
-location
-.
-h
-"
-#
-include
-"
-rtc_base
-/
 logging
 .
 h
--- third_party/libwebrtc/audio/channel_send.cc
+++ third_party/libwebrtc/audio/channel_send.cc
@@ -303,15 +303,6 @@ include
 "
 rtc_base
 /
-location
-.
-h
-"
-#
-include
-"
-rtc_base
-/
 logging
 .
 h
