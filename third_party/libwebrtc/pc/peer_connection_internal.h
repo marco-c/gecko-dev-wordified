c06c6fd8e6d699fa7590dc3dd8f071fd9ae9592b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 04:04:32 +0000

    Bug 1766646 - Vendor libwebrtc from bc32c56f83f
    Upstream commit: https://webrtc.googlesource.com/src/+/bc32c56f83f17314f8b4dc4e5c3938a74f80cd94
        Move pc.transport_controller_ to be network thread only
    
        A pointer to the transport controller is now maintained on
        both the network thread and the signaling thread. We use
        thread specific accessors to make it explicit which copy we
        are accessing at any given time.
    
        We also move the initial offerer value to the SDP offer/answer
        class; this is determined on the basis of SDP offer/answer, so
        there is no need to hop to the network thread for that.
    
        Work in progress.
    
        Bug: webrtc:9987
        Change-Id: Idbe5a7fbf44f667adcd119e486133cf6e43ab1f5
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/251382
        Reviewed-by: Tomas Gunnarsson <tommiwebrtc.org>
        Commit-Queue: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#35965}
    
    UltraBlame original commit: 2d48c9b8ccf9db0603b28f987da4744898103dee

--- third_party/libwebrtc/pc/peer_connection_internal.h
+++ third_party/libwebrtc/pc/peer_connection_internal.h
@@ -522,7 +522,16 @@ GetCryptoOptions
 virtual
 JsepTransportController
 *
-transport_controller
+transport_controller_s
+(
+)
+=
+0
+;
+virtual
+JsepTransportController
+*
+transport_controller_n
 (
 )
 =
