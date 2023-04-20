9bd15424e78ea29cf8ffb852730fdf1a34bb3204
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 02:09:02 +0000

    Bug 1766646 - Vendor libwebrtc from e2ab77ba57
    Upstream commit: https://webrtc.googlesource.com/src/+/e2ab77ba57bff5db8eaa7a8442fa6b2f43914b69
        Reland "Port: migrate to TaskQueue."
    
        This reverts commit a4aabb921353125f6d3a2caa2ceb9cda7e971f22.
    
        Reason for revert: downstream tests fixed.
    
        TBR=htawebrtc.org
        Original change's description:
        > Revert "Port: migrate to TaskQueue."
        >
        > This reverts commit 06540166ca97028454adea48cec9bf109b771ddc.
        >
        > Reason for revert: breaks downstream test.
        >
        > Original change's description:
        > > Port: migrate to TaskQueue.
        > >
        > > Port uses legacy rtc::Thread message handling. In order
        > > to cancel callbacks it uses rtc::Thread::Clear() which uses locks and
        > > necessitates looping through all currently queued (unbounded) messages
        > > in the thread. In particular, these Clear calls are common during
        > > negotiation and the probability of having a lot of queued messages is
        > > high due to a long-running network thread function invoked on the
        > > network thread.
        > >
        > > Fix this by migrating Port to task queues.
        > >
        > >
        > > Bug: webrtc:12840, webrtc:9702
        > > Change-Id: I6c6fb83323899b56091f0857a1c2d15d19199002
        > > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/221370
        > > Reviewed-by: Harald Alvestrand <htawebrtc.org>
        > > Commit-Queue: Markus Handell <handellmwebrtc.org>
        > > Cr-Commit-Position: refs/heads/master{#34338}
        >
        > TBR=htawebrtc.org,handellmwebrtc.org,webrtc-scopedluci-project-accounts.iam.gserviceaccount.com
        >
        > Change-Id: I014ef9267d224c10595cfa1c12899eabe0093306
        > No-Presubmit: true
        > No-Tree-Checks: true
        > No-Try: true
        > Bug: webrtc:12840, webrtc:9702
        > Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/223062
        > Reviewed-by: Markus Handell <handellmwebrtc.org>
        > Commit-Queue: Markus Handell <handellmwebrtc.org>
        > Cr-Commit-Position: refs/heads/master{#34339}
    
        # Not skipping CQ checks because this is a reland.
    
        Bug: webrtc:12840, webrtc:9702
        Change-Id: I4d2e086b686da8d5272d67293406300a07edef81
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/223260
        Reviewed-by: Markus Handell <handellmwebrtc.org>
        Commit-Queue: Markus Handell <handellmwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#34345}
    
    UltraBlame original commit: 130c89af0ebbf842d25f79abc1a433595db15a62

--- third_party/libwebrtc/p2p/base/turn_port.h
+++ third_party/libwebrtc/p2p/base/turn_port.h
@@ -181,6 +181,15 @@ include
 "
 rtc_base
 /
+message_handler
+.
+h
+"
+#
+include
+"
+rtc_base
+/
 ssl_certificate
 .
 h
@@ -243,6 +252,11 @@ TurnPort
 :
 public
 Port
+public
+rtc
+:
+:
+MessageHandler
 {
 public
 :
@@ -2106,8 +2120,6 @@ private
 enum
 {
 MSG_ALLOCATE_ERROR
-=
-MSG_FIRST_AVAILABLE
 MSG_ALLOCATE_MISMATCH
 MSG_TRY_ALTERNATE_SERVER
 MSG_REFRESH_ERROR
