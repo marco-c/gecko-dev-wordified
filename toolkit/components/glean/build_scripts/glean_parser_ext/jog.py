3496b1a6098b66eea6fd9b25e7c3144dce1a069b
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 09:15:42 +0000

    Bug 1767040 - Support runtime-registered pings in Glean r=TravisLong
    Differential Revision: https://phabricator.services.mozilla.com/D158023
    
    UltraBlame original commit: 1f4b4e4f5c07963aa0accd0241bf999de674b644

--- toolkit/components/glean/build_scripts/glean_parser_ext/jog.py
+++ toolkit/components/glean/build_scripts/glean_parser_ext/jog.py
@@ -85,6 +85,7 @@ from
 js
 import
 ID_BITS
+PING_INDEX_BITS
 from
 util
 import
@@ -472,6 +473,12 @@ metric_types
 runtime_metric_bit
 =
 ID_BITS
+-
+1
+            
+runtime_ping_bit
+=
+PING_INDEX_BITS
 -
 1
             
