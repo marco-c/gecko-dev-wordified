7faede96261cecfdfac778792fe38c76262fd3e1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 04:14:47 +0000

    Bug 1766646 - (fix-d6cdf80072) fix casting issue
    UltraBlame original commit: 77d0769c98e8c9edd1c9be0d6b09c0a13a81d887

--- third_party/libwebrtc/modules/video_coding/timing.cc
+++ third_party/libwebrtc/modules/video_coding/timing.cc
@@ -1435,7 +1435,17 @@ earliest_next_decode_start_time
 =
 last_decode_scheduled_
 +
+static_cast
+<
+const
+webrtc
+:
+:
+TimeDelta
+>
+(
 zero_playout_delay_min_pacing_
+)
 ;
 TimeDelta
 max_wait_time
