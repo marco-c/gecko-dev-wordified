0f019f2bf58547dca2d6a7f38afb5d4087beb69c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:46:27 +0000

    Bug 1750450 - Doc: use the new system for python deps r=firefox-source-docs-reviewers,ahal
    + update sphinx-js to 3.2.1
    
    Differential Revision: https://phabricator.services.mozilla.com/D153483
    
    UltraBlame original commit: 65695f7ddf1d080ab59ad76a6c419813f029eea7

--- tools/moztreedocs/mach_commands.py
+++ tools/moztreedocs/mach_commands.py
@@ -887,29 +887,6 @@ PATH
     
 )
     
-command_context
-.
-virtualenv_manager
-.
-install_pip_requirements
-(
-        
-os
-.
-path
-.
-join
-(
-here
-"
-requirements
-.
-txt
-"
-)
-    
-)
-    
 import
 webbrowser
     
