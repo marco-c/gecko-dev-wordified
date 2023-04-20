a82bf5c6a3f9875cbaa51e8bbc1ee8e3f0882787
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 04:58:39 +0000

    Bug 1766646 - Vendor libwebrtc from 0a16276290
    Upstream commit: https://webrtc.googlesource.com/src/+/0a16276290227c2611a685b99286e92927c531e8
        Restore FiredDirection and maybe fire OnTrack in Rollback.
    
        Prior to this CL, rollback did not restore FiredDirection and remote
        streams were only sometimes restored. This resulted in not firing
        ontrack if a track was rolled back and then added again on the same
        transceiver.
    
        Rollback also never performed OnTrack, which is incorrect because a
        transceiver that goes from sendrecv to inactive will cause OnRemoveTrack
        and if this is rolled back (so we become sendrecv again) then we need
        OnTrack to fire.
    
        This CL improves rollback's "memory", fires ontrack in Rollback() and
        adds test coverage.
    
        Needed to solve similar bugs in the Chromium layers as well:
        https://chromium-review.googlesource.com/c/chromium/src/+/3613313
    
        Bug: chromium:1320669
        Change-Id: I655dd7d8a6b86080fe0e7c32c9e8c6434062ae91
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/260330
        Commit-Queue: Henrik Boström <hboswebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#36734}
    
    UltraBlame original commit: 61ad88c8b295a086cac710fe8927645514462b70

--- third_party/libwebrtc/pc/rtp_transceiver.cc
+++ third_party/libwebrtc/pc/rtp_transceiver.cc
@@ -2671,7 +2671,13 @@ RtpTransceiver
 :
 set_fired_direction
 (
+absl
+:
+:
+optional
+<
 RtpTransceiverDirection
+>
 direction
 )
 {
--- third_party/libwebrtc/pc/transceiver_list.cc
+++ third_party/libwebrtc/pc/transceiver_list.cc
@@ -178,7 +178,7 @@ void
 TransceiverStableState
 :
 :
-SetRemoteStreamIdsIfUnset
+SetRemoteStreamIds
 (
 const
 std
