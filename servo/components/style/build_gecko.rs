4c29103292cf1669f7d034301b09792905bbf3fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 17:31:22 +0000

    Bug 1803702 - Remove explicit rust target for bindgen in stylo. r=emilio
    This doesn't seem necessary anymore. It was originally added to disable
    unions, which is now done with disable_untagged_union.
    
    Differential Revision: https://phabricator.services.mozilla.com/D163662
    
    UltraBlame original commit: 6b91a7882aea3269c6fcc658b65331c6d3cea969

--- servo/components/style/build_gecko.rs
+++ servo/components/style/build_gecko.rs
@@ -1106,12 +1106,6 @@ get_initial_builder
 >
 Builder
 {
-use
-bindgen
-:
-:
-RustTarget
-;
 /
 /
 Disable
@@ -1137,14 +1131,6 @@ Builder
 :
 default
 (
-)
-.
-rust_target
-(
-RustTarget
-:
-:
-Stable_1_25
 )
 .
 size_t_is_usize
