00095c836dc5f40e3929a4b5ca5c924ae7ec276c
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 19:43:37 +0000

    Backed out changeset 863fc99bca91 (bug 1806434) for causing unintended consequences CLOSED TREE
    UltraBlame original commit: 795cec1ac6175ecc12eaf5404462586d3bf5e4c6

--- taskcluster/gecko_taskgraph/util/scriptworker.py
+++ taskcluster/gecko_taskgraph/util/scriptworker.py
@@ -2212,13 +2212,32 @@ get_devedition_signing_cert_scope
 config
 )
     
-else
+elif
+is_shippable
 :
         
 return
 get_signing_cert_scope
 (
 config
+)
+    
+else
+:
+        
+return
+add_scope_prefix
+(
+config
+"
+signing
+:
+cert
+:
+dep
+-
+signing
+"
 )
 #
 generate_beetmover_upstream_artifacts
