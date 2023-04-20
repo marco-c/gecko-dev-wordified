92adc6f01f035162434a837a5f1bc4b76562e90d
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-20 00:15:27 +0000

    Bug 1766646 - Vendor libwebrtc from 19526a69c7
    Upstream commit: https://webrtc.googlesource.com/src/+/19526a69c763d26f4699f14dd33681db8976a1b1
        Add RTC_NO_UNIQUE_ADDRESS to webrtc::VideoStreamAdapter.
    
        Bug: webrtc:11495
        Change-Id: I32a1bc75d7c578753c158a61eb6cb5ea7565629a
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/194564
        Reviewed-by: Karl Wiberg <kwibergwebrtc.org>
        Commit-Queue: Mirko Bonadei <mbonadeiwebrtc.org>
        Cr-Commit-Position: refs/heads/master{#32676}
    
    UltraBlame original commit: c35404df2d4504452bd195a8e1206cbf36777147

--- third_party/libwebrtc/call/adaptation/video_stream_adapter.h
+++ third_party/libwebrtc/call/adaptation/video_stream_adapter.h
@@ -251,6 +251,17 @@ include
 "
 rtc_base
 /
+system
+/
+no_unique_address
+.
+h
+"
+#
+include
+"
+rtc_base
+/
 thread_annotations
 .
 h
@@ -1343,6 +1354,7 @@ RTC_RUN_ON
 sequence_checker_
 )
 ;
+RTC_NO_UNIQUE_ADDRESS
 SequenceChecker
 sequence_checker_
 RTC_GUARDED_BY
