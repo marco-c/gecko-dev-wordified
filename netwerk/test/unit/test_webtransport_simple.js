2ebc22086cc0a9c60ba0cae42f1755188783a3f7
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-21 10:28:19 +0000

    Backed out changeset 7796a0d39f43 (bug 1818828) for xpcshell failures at test_simple_conn.js CLOSED TREE
    UltraBlame original commit: 3f1b4a6704c5a2c934a9b9f856e48482e0f74b6a

--- netwerk/test/unit/test_webtransport_simple.js
+++ netwerk/test/unit/test_webtransport_simple.js
@@ -975,7 +975,6 @@ test_closed_0ms
 (
 )
 {
-await
 test_closed
 (
 "
@@ -995,7 +994,6 @@ test_closed_100ms
 (
 )
 {
-await
 test_closed
 (
 "
@@ -1435,7 +1433,7 @@ https
 host
 }
 /
-create_unidi_stream_and_hello
+create_unidi_stream
 )
 Services
 .
@@ -1533,7 +1531,7 @@ equal
 (
 data
 "
-qwerty
+0123456789
 "
 )
 ;
