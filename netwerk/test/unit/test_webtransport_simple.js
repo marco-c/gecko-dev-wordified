cb40a0734306f41c8637ae50ceb0630479c24a14
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-21 10:22:09 +0000

    Bug 1818828 - Added basic DOM-using xpcshell testing for simple webtransport coverage. r=necko-reviewers,kershaw,jesup
    Differential Revision: https://phabricator.services.mozilla.com/D170955
    
    UltraBlame original commit: 7796a0d39f43a9ff9b9e64acc17adc9933c0dddd

--- netwerk/test/unit/test_webtransport_simple.js
+++ netwerk/test/unit/test_webtransport_simple.js
@@ -975,6 +975,7 @@ test_closed_0ms
 (
 )
 {
+await
 test_closed
 (
 "
@@ -994,6 +995,7 @@ test_closed_100ms
 (
 )
 {
+await
 test_closed
 (
 "
@@ -1433,7 +1435,7 @@ https
 host
 }
 /
-create_unidi_stream
+create_unidi_stream_and_hello
 )
 Services
 .
@@ -1531,7 +1533,7 @@ equal
 (
 data
 "
-0123456789
+qwerty
 "
 )
 ;
