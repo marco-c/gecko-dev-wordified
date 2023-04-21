4fb8181224b686cb68debf533ebce35226c8a30a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-04-21 10:13:54 +0000

    Bug 1820776: Cleanup H3Session r=kershaw,valentin,necko-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D171900
    
    UltraBlame original commit: 43beacf35d14691fac3c138ef8d25ea878c91df9

--- netwerk/protocol/http/Http3Session.h
+++ netwerk/protocol/http/Http3Session.h
@@ -1750,7 +1750,7 @@ Http3StreamBase
 >
 mStreamTransactionHash
 ;
-nsDeque
+nsRefPtrDeque
 <
 Http3StreamBase
 >
@@ -1765,7 +1765,7 @@ Http3StreamBase
 >
 mSlowConsumersReadyForRead
 ;
-nsDeque
+nsRefPtrDeque
 <
 Http3StreamBase
 >
