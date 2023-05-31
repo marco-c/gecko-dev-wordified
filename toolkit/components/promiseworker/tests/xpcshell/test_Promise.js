3f1db63159d0c421a7614dd57e39d3446f370b24
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-05-31 12:07:02 +0000

    Bug 1824591 - Convert toolkit/components/promiseworker to ES modules. r=kpatenio
    Differential Revision: https://phabricator.services.mozilla.com/D179202
    
    UltraBlame original commit: 02dc223912e09b3232883a3fd157d83d45b4b8b3

--- toolkit/components/promiseworker/tests/xpcshell/test_Promise.js
+++ toolkit/components/promiseworker/tests/xpcshell/test_Promise.js
@@ -54,7 +54,7 @@ BasePromiseWorker
 =
 ChromeUtils
 .
-import
+importESModule
 (
 "
 resource
@@ -67,7 +67,9 @@ modules
 /
 PromiseWorker
 .
-jsm
+sys
+.
+mjs
 "
 )
 ;
--- toolkit/components/telemetry/tests/unit/test_UninstallPing.js
+++ toolkit/components/telemetry/tests/unit/test_UninstallPing.js
@@ -40,7 +40,7 @@ BasePromiseWorker
 =
 ChromeUtils
 .
-import
+importESModule
 (
 "
 resource
@@ -53,7 +53,9 @@ modules
 /
 PromiseWorker
 .
-jsm
+sys
+.
+mjs
 "
 )
 ;
