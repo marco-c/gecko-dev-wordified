fe933ecb6c07c91efb2e9e0240011e1dd151de46
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-01 00:42:54 +0000

    Bug 1838829 - Add Http/2 test server in mochitest, r=necko-reviewers,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D181249
    
    UltraBlame original commit: e125be49d2a61b1328cb4dedeefc8a97a44d05ff

--- testing/mochitest/runjunit.py
+++ testing/mochitest/runjunit.py
@@ -241,6 +241,12 @@ None
         
 self
 .
+http2Server
+=
+None
+        
+self
+.
 dohServer
 =
 None
@@ -845,6 +851,14 @@ self
 .
 options
 .
+http2ServerPath
+=
+None
+        
+self
+.
+options
+.
 keep_open
 =
 False
@@ -879,6 +893,14 @@ self
 options
 .
 useHttp3Server
+=
+False
+        
+self
+.
+options
+.
+useHttp2Server
 =
 False
         
