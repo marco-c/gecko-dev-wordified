3e134fb9b648e12c1845795cb1988fe436c2d8b1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-07-21 16:34:56 +0000

    Bug 1808732 - Move various dependencies out of the `mach` site to either the specific site they're needed in (like `build, or `lint`), and/or to `common` r=firefox-build-system-reviewers,glandium
    Now that we selectively load command modules, and activate the command
    virtualenv much earlier in the mach process, a lot of the module
    dependencies specified in `mach.txt` are no longer necessary there. With
    their removal from `mach.txt` they will no longer be automatically
    inherited by every site, which reduces potential dependency conflicts
    for specific sites.
    
    The `common` site still effectively has the same set of dependencies.
    This is the default site that all commands use unless otherwise
    specified. Most commands use this site, and going through every command
    and seeing if a dependency is or isn't needed, then deciding if or if
    not to create a new site for that command made sense was too time
    consuming to do here.
    
    Essentially the idea here going forward is that if you're trying to
    add/update a new dependency to a command that is currently defaulting to
    the `common` site and there is a conflict with one of the dependencies
    in `common` you can move your command to a new site specifically for
    your command, and you will have the minimal possible set of dependencies
    a mach command can have, improving the odds that you can add the
    module(s) you need for your command.
    
    Depends on D180500
    
    Differential Revision: https://phabricator.services.mozilla.com/D180501
    
    UltraBlame original commit: 49a85b3b07e07c1158f9cb0c1f58b67fc2d659f7

--- python/mozbuild/mozbuild/mach_commands.py
+++ python/mozbuild/mozbuild/mach_commands.py
@@ -119,13 +119,6 @@ SettingsProvider
     
 SubCommand
 )
-from
-voluptuous
-import
-All
-Boolean
-Required
-Schema
 import
 mozbuild
 .
@@ -730,6 +723,14 @@ _cargo_config_yaml_schema
 (
 )
 :
+    
+from
+voluptuous
+import
+All
+Boolean
+Required
+Schema
     
 def
 starts_with_cargo
