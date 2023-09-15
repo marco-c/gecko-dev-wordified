f343dde854a87bd2a4c1d43b84e49a98a9174aa1
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-15 20:34:30 +0000

    Bug 1401466 - make the client auth certificate selection dialog tab modal r=jschanck,necko-reviewers,bolsson,kershaw,valentin
    Previously, the client authentication certificate selection dialog could show
    up unexpectedly. Because it was modal, it would prevent user interaction with
    the browser. It could even get in a state where the dialog couldn't be
    interacted with, and neither could anything else, so the entire browser would
    be locked and the user would have to quit and restart.
    
    This patch associates a top-level outer content window ID (called "browserId"
    in networking code) with each NSSSocketControl. When a peer asks for a client
    authentication certificate, the NSSSocketControl can use the ID to find the
    relevant tab and open a tab-modal dialog, which allows other browser UI to be
    interacted with.
    
    Some loads cannot be associated with browser tabs, and so the implementation
    falls back to opening a window-modal dialog on the most recently active window.
    This is still better than the previous implementation, since the dialog is
    connected to a window rather than being its own separate dialog.
    
    Differential Revision: https://phabricator.services.mozilla.com/D183775
    
    UltraBlame original commit: 9d1733984f18d8faafeeb405e1519bd8f2f239d0

--- security/manager/pki/nsNSSDialogs.h
+++ security/manager/pki/nsNSSDialogs.h
@@ -114,13 +114,6 @@ h
 #
 include
 "
-nsIClientAuthDialogs
-.
-h
-"
-#
-include
-"
 nsIStringBundle
 .
 h
@@ -161,8 +154,6 @@ nsNSSDialogs
 public
 nsICertificateDialogs
 public
-nsIClientAuthDialogs
-public
 nsITokenPasswordDialogs
 {
 public
@@ -170,7 +161,6 @@ public
 NS_DECL_THREADSAFE_ISUPPORTS
 NS_DECL_NSITOKENPASSWORDDIALOGS
 NS_DECL_NSICERTIFICATEDIALOGS
-NS_DECL_NSICLIENTAUTHDIALOGS
 nsNSSDialogs
 (
 )
