0e9c6d0a4f6b8b47ac50e5779ca1a8733f56602a
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-15 20:38:28 +0000

    Bug 1850026 - Set dom.ipc.forkserver.enable to true in .deb repackage using a prefs file r=jcristau
    Differential Revision: https://phabricator.services.mozilla.com/D186831
    
    UltraBlame original commit: a06fd051fd6d828cbe838036a0fde4bdd8c0f94a

--- python/mozbuild/mozbuild/repackaging/deb.py
+++ python/mozbuild/mozbuild/repackaging/deb.py
@@ -479,9 +479,25 @@ source_dir
         
 _render_deb_templates
 (
+            
 template_dir
+            
 source_dir
+            
 build_variables
+            
+exclude_file_names
+=
+[
+"
+package
+-
+prefs
+.
+js
+"
+]
+        
 )
         
 app_name
@@ -563,6 +579,13 @@ fluent_localization
             
 fluent_resource_loader
         
+)
+        
+_inject_deb_prefs_file
+(
+source_dir
+app_name
+template_dir
 )
         
 _generate_deb_archive
@@ -1413,6 +1436,17 @@ endswith
 in
 "
 )
+and
+not
+filename
+.
+endswith
+(
+"
+.
+js
+"
+)
     
 ]
     
@@ -1710,6 +1744,57 @@ distribution
 "
 )
         
+)
+def
+_inject_deb_prefs_file
+(
+source_dir
+app_name
+template_dir
+)
+:
+    
+src
+=
+mozpath
+.
+join
+(
+template_dir
+"
+package
+-
+prefs
+.
+js
+"
+)
+    
+dst
+=
+mozpath
+.
+join
+(
+source_dir
+app_name
+.
+lower
+(
+)
+"
+defaults
+/
+pref
+"
+)
+    
+shutil
+.
+copy
+(
+src
+dst
 )
 def
 _inject_deb_desktop_entry_file
