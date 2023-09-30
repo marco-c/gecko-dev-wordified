ce5cf82326265dc1ae8b0ac84c6b9209f4eb4c41
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-09-30 00:26:35 +0000

    Bug 1854964 - adjust runtime chunking  to work with toml. r=ahal,taskgraph-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D189142
    
    UltraBlame original commit: 42974cdbd791eebba429876abb9117bd1073f3c8

--- taskcluster/gecko_taskgraph/util/chunking.py
+++ taskcluster/gecko_taskgraph/util/chunking.py
@@ -1336,11 +1336,29 @@ chunk
 "
 "
     
-manifests
+ini_manifests
 =
 set
 (
+[
+x
+.
+replace
+(
+"
+.
+toml
+"
+"
+.
+ini
+"
+)
+for
+x
+in
 manifests
+]
 )
     
 if
@@ -1379,18 +1397,15 @@ items
 if
 k
 in
-manifests
+ini_manifests
         
 }
         
-return
+retVal
+=
 [
-            
-c
-[
-1
 ]
-            
+        
 for
 c
 in
@@ -1403,12 +1418,47 @@ runtimes
 .
 get_chunked_manifests
 (
+            
+ini_manifests
+        
+)
+:
+            
+retVal
+.
+append
+(
                 
+[
+m
+if
+m
+in
 manifests
+else
+m
+.
+replace
+(
+"
+.
+ini
+"
+"
+.
+toml
+"
+)
+for
+m
+in
+c
+[
+1
+]
+]
             
 )
-        
-]
     
 #
 Keep
