29ef247a247ffcc3e66d46c117e01177640412d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-13 17:41:40 +0000

    Bug 1855321 part 5 / 17 (PBL): `AbstractFramePtr` tagging: fit within two bits. r=jandem
    This is needed to support a `JitStackAlignment` of 4, which can happen
    on 32-bit builds with PBL enabled (e.g. on wasm32-wasi).
    
    Differential Revision: https://phabricator.services.mozilla.com/D189298
    
    UltraBlame original commit: 598525a0055a8b8e6a4c9a725b7560020b63d11d

--- js/src/gdb/mozilla/Interpreter.py
+++ js/src/gdb/mozilla/Interpreter.py
@@ -542,29 +542,25 @@ object
 )
 :
     
-Tag_ScriptFrameIterData
+Tag_InterpreterFrame
 =
 0x0
     
-Tag_InterpreterFrame
+Tag_BaselineFrame
 =
 0x1
     
-Tag_BaselineFrame
+Tag_RematerializedFrame
 =
 0x2
     
-Tag_RematerializedFrame
+Tag_WasmDebugFrame
 =
 0x3
     
-Tag_WasmDebugFrame
-=
-0x4
-    
 TagMask
 =
-0x7
+0x3
     
 def
 __init__
