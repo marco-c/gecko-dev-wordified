89cb1b6d864cc708a6906c1f4d34a6a0649613f9
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 10:29:08 +0000

    Bug 1857862 - Vendor libwebrtc from c4e0254909
    Upstream commit: https://webrtc.googlesource.com/src/+/c4e0254909d951de48c633a1a65be3db407fa9c3
        Fix capture_clock_offset_updater_ data race.
    
        Change-Id: I1b84810ea19c8bf24ec49d7fb69e954d18759e37
        Bug: b/288066973
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/309680
        Reviewed-by: Jakob Ivarsson‎ <jakobiwebrtc.org>
        Commit-Queue: Jeremy Leconte <jlecontegoogle.com>
        Cr-Commit-Position: refs/heads/main{#40320}
    
    UltraBlame original commit: a66395161a9750f5a86a51c4d8b6ec1ce1eeeb48

--- third_party/libwebrtc/audio/channel_receive.cc
+++ third_party/libwebrtc/audio/channel_receive.cc
@@ -1804,6 +1804,10 @@ webrtc
 :
 CaptureClockOffsetUpdater
 capture_clock_offset_updater_
+RTC_GUARDED_BY
+(
+ts_stats_lock_
+)
 ;
 rtc
 :
@@ -3141,6 +3145,13 @@ has_value
 )
 )
 {
+MutexLock
+lock
+(
+&
+ts_stats_lock_
+)
+;
 local_capture_clock_offset_q32x32
 =
 capture_clock_offset_updater_
