e26790874b30db2d0b15416dad78a4748d983905
Marco Castelluccio <mcastelluccio@mozilla.com>
2023-10-30 11:18:15 +0000

    Bug 1858449 [wpt PR 42487] - [FedCM] Replace Account with IdentityCredential for extensibility, a=testonly
    Automatic update from web-platform-tests
    [FedCM] Replace Account with IdentityCredential for extensibility
    
    Today the FedCM API supports auto selecting an account during auto
    re-authentication. Hence we introduced a boolean with "account" in it
    to reflect the situation. There's some effort to support digital
    credentials using the `IdentityCredential` object in which case the term
    "account" would make less sense.
    
    Bug: 1473976
    Change-Id: I25dc47f420776a7b0c1997146ede21156c7bdce5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4930572
    Reviewed-by: Christian Biesinger <cbiesingerchromium.org>
    Commit-Queue: Yi Gu <yiguchromium.org>
    Reviewed-by: Nasko Oskov <naskochromium.org>
    Cr-Commit-Position: refs/heads/main{#1209096}
    
    --
    
    wpt-commits: f0f402ec611f5c62e4d3f6f43c8e85f0c2ef6d24
    wpt-pr: 42487
    
    UltraBlame original commit: 2b867ba256c7bc21078fcbfd562e46ae202104e6

--- testing/web-platform/tests/credential-management/support/fedcm/token_with_identity_credential_auto_selected_flag.py
+++ testing/web-platform/tests/credential-management/support/fedcm/token_with_identity_credential_auto_selected_flag.py
@@ -68,7 +68,7 @@ json
 "
 )
   
-is_account_auto_selected
+is_identity_credential_auto_selected
 =
 request
 .
@@ -78,7 +78,7 @@ get
 (
 b
 "
-is_account_auto_selected
+is_identity_credential_auto_selected
 "
 )
   
@@ -93,11 +93,11 @@ token
 :
 \
 "
-is_account_auto_selected
+is_identity_credential_auto_selected
 =
 "
 +
-is_account_auto_selected
+is_identity_credential_auto_selected
 .
 decode
 (
