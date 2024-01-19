b24558cb96ac87252028ca9ea31a6258a22032a3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-01-19 16:19:09 +0000

    Bug 1724089 - Remove browser.tabs.remote.autostart pref r=webdriver-reviewers,perftest-reviewers,geckoview-reviewers,jgraham,extension-reviewers,mossop,sparky,robwu,owlish,necko-reviewers,jmaher,valentin
    Differential Revision: https://phabricator.services.mozilla.com/D192136
    
    UltraBlame original commit: da092d881d44f60e7c5ae33f596dcdf706117ced

--- testing/mochitest/runtests.py
+++ testing/mochitest/runtests.py
@@ -15962,6 +15962,24 @@ MOZ_HEADLESS
 "
         
 if
+not
+options
+.
+e10s
+:
+            
+browserEnv
+[
+"
+MOZ_FORCE_DISABLE_E10S
+"
+]
+=
+"
+1
+"
+        
+if
 options
 .
 dmd
@@ -19481,20 +19499,6 @@ profile
 prefs
 =
 {
-            
-"
-browser
-.
-tabs
-.
-remote
-.
-autostart
-"
-:
-options
-.
-e10s
             
 #
 Enable
--- testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/firefox.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/browsers/firefox.py
@@ -2075,6 +2075,9 @@ headless
 chaos_mode_flags
 =
 None
+e10s
+=
+True
 )
 :
     
@@ -2287,6 +2290,22 @@ MOZ_HEADLESS
 1
 "
     
+if
+not
+e10s
+:
+        
+env
+[
+"
+MOZ_FORCE_DISABLE_E10S
+"
+]
+=
+"
+1
+"
+    
 return
 env
 def
@@ -2416,6 +2435,7 @@ leak_check
 stackfix_dir
 symbols_path
 asan
+e10s
 )
 :
         
@@ -2505,6 +2525,12 @@ self
 asan
 =
 asan
+        
+self
+.
+e10s
+=
+e10s
         
 self
 .
@@ -2730,6 +2756,9 @@ headless
 self
 .
 chaos_mode_flags
+self
+.
+e10s
 )
         
 args
@@ -4877,7 +4906,6 @@ prefs_root
 config
 test_type
 extra_prefs
-e10s
                  
 disable_fission
 debug_test
@@ -4918,12 +4946,6 @@ self
 extra_prefs
 =
 extra_prefs
-        
-self
-.
-e10s
-=
-e10s
         
 self
 .
@@ -5442,31 +5464,6 @@ False
 }
 )
         
-if
-self
-.
-e10s
-:
-            
-profile
-.
-set_preferences
-(
-{
-"
-browser
-.
-tabs
-.
-remote
-.
-autostart
-"
-:
-True
-}
-)
-        
 profile
 .
 set_preferences
@@ -5589,69 +5586,6 @@ remote
 prefs
 .
 recommended
-"
-:
-True
-}
-)
-        
-#
-Bug
-1262954
-:
-winxp
-+
-e10s
-disable
-hwaccel
-        
-if
-(
-self
-.
-e10s
-and
-platform
-.
-system
-(
-)
-in
-(
-"
-Windows
-"
-"
-Microsoft
-"
-)
-and
-            
-"
-5
-.
-1
-"
-in
-platform
-.
-version
-(
-)
-)
-:
-            
-profile
-.
-set_preferences
-(
-{
-"
-layers
-.
-acceleration
-.
-disabled
 "
 :
 True
@@ -6304,8 +6238,6 @@ test_type
                                          
 extra_prefs
                                          
-e10s
-                                         
 disable_fission
                                          
 debug_test
@@ -6361,6 +6293,8 @@ stackfix_dir
 symbols_path
                                                      
 asan
+                                                     
+e10s
 )
     
 def
@@ -6843,6 +6777,7 @@ binary
 debug_info
 headless
 chaos_mode_flags
+e10s
 )
         
 profile_creator
@@ -6860,8 +6795,6 @@ wdspec
 "
                                          
 extra_prefs
-                                         
-e10s
                                          
 disable_fission
                                          
@@ -6900,6 +6833,7 @@ binary
 debug_info
 headless
 chaos_mode_flags
+e10s
 )
 :
         
@@ -6918,6 +6852,7 @@ debug_info
 headless
                           
 chaos_mode_flags
+e10s
 )
         
 env
