ebad392075249b522e2588707b80976ca7474668
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 05:05:18 +0000

    Bug 1884775 [wpt PR 45001] - [WebDriverBiDi] Change test to match current spec, a=testonly
    Automatic update from web-platform-tests
    [WebDriverBiDi] Change test to match current spec (#45001)
    
    --
    
    wpt-commits: 5f87351e8bc0ec8f3fc843675b922ce4d949e2fa
    wpt-pr: 45001
    
    UltraBlame original commit: 208b66235120f4ee26b347cdc7024f5616bac090

--- testing/web-platform/tests/webdriver/tests/bidi/network/continue_with_auth/action.py
+++ testing/web-platform/tests/webdriver/tests/bidi/network/continue_with_auth/action.py
@@ -42,8 +42,6 @@ assert_response_event
     
 AUTH_REQUIRED_EVENT
     
-PAGE_EMPTY_TEXT
-    
 RESPONSE_COMPLETED_EVENT
 )
 pytestmark
@@ -62,7 +60,7 @@ setup_blocked_request
 subscribe_events
 wait_for_event
 bidi_session
-url
+wait_for_future_safe
 )
 :
     
@@ -116,7 +114,10 @@ on_response_completed
 response_event
 =
 await
+wait_for_future_safe
+(
 on_response_completed
+)
     
 assert_response_event
 (
@@ -151,9 +152,7 @@ test_default
     
 setup_blocked_request
 subscribe_events
-wait_for_event
 bidi_session
-url
 )
 :
     
@@ -322,7 +321,6 @@ test_provideCredentials
 setup_blocked_request
 subscribe_events
 bidi_session
-url
 )
 :
     
@@ -594,7 +592,7 @@ setup_blocked_request
 subscribe_events
 bidi_session
 wait_for_event
-url
+wait_for_future_safe
 )
 :
     
@@ -759,7 +757,10 @@ wrong
 credentials
     
 await
+wait_for_future_safe
+(
 on_auth_required
+)
     
 #
 Continue
