28f8b5e4671b652cebf86713df81efc0e3c0c4b0
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-05-01 04:59:18 +0000

    Bug 1861963 [wpt PR 42839] - Fix accessing update_properties for product, a=testonly
    Automatic update from web-platform-tests
    Fix accessing update_properties for product
    
    Now that we're passing around a Product class rather than a product
    name, we don't need a separate `load_properties_update`, but can
    straightforwardly just put `update_properties` directly on the Product
    object we already have.
    
    --
    
    wpt-commits: eadc585217cba960e6bf94dba7018e690b3e8ac4
    wpt-pr: 42839
    
    UltraBlame original commit: 273a2a4be71e70ad20e5908c3b864e1470ea221f

--- testing/web-platform/tests/tools/wpt/update.py
+++ testing/web-platform/tests/tools/wpt/update.py
@@ -271,15 +271,6 @@ extra_property
 "
 ]
                                                 
-config
-=
-kwargs
-[
-"
-config
-"
-]
-                                                
 product
 =
 kwargs
