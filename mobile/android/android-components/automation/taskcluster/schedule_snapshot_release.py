e343e531dc6b4c30b042c89f6c03c53a08abacd3
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-04 21:57:59 +0000

    [components] Bug 1533791 - part 2: Fix snapshot context
    UltraBlame original commit: 2f3605223077074d3943dcd7e44872486aaea41b

--- mobile/android/android-components/automation/taskcluster/schedule_snapshot_release.py
+++ mobile/android/android-components/automation/taskcluster/schedule_snapshot_release.py
@@ -312,6 +312,44 @@ slugids
 name
 ]
     
+repository_parts
+=
+params
+[
+'
+html_url
+'
+]
+.
+split
+(
+'
+/
+'
+)
+    
+repository_full_name
+=
+'
+/
+'
+.
+join
+(
+(
+repository_parts
+[
+-
+2
+]
+repository_parts
+[
+-
+1
+]
+)
+)
+    
 #
 provide
 a
@@ -410,6 +448,12 @@ params
 html_url
 '
 ]
+                
+'
+full_name
+'
+:
+repository_full_name
             
 }
             
