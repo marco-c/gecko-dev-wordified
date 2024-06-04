7d7c0ba02944158943721c26444d16d9c6a2746d
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-04 21:14:19 +0000

    [components] Bug 1512631 - part 8: Ensure uploadArchives and zipMavenArtifacts occur after the build finishes
    UltraBlame original commit: ecd3b440c7f4ab364a99090f0f5bbf60679045cb

--- mobile/android/android-components/automation/taskcluster/decision_task_publish.py
+++ mobile/android/android-components/automation/taskcluster/decision_task_publish.py
@@ -496,6 +496,10 @@ no
 -
 daemon
 clean
+'
++
+            
+'
 &
 &
 .
@@ -508,13 +512,22 @@ no
 daemon
 '
 +
-            
 snapshot_flag
 +
 gradle_tasks_for_this_module_only
 +
             
 '
+&
+&
+.
+/
+gradlew
+-
+-
+no
+-
+daemon
 uploadArchives
 zipMavenArtifacts
 '
