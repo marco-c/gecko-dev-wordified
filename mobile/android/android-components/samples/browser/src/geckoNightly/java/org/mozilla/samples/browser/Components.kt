712f703996fe6f2536618c12f4f6a5ea7d4d1092
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-05 02:06:29 +0000

    [components] Improve add-ons update flow
    UltraBlame original commit: 1c032d0775a396a525dd0ad8c3ddc0ae8f8d6763

--- mobile/android/android-components/samples/browser/src/geckoNightly/java/org/mozilla/samples/browser/Components.kt
+++ mobile/android/android-components/samples/browser/src/geckoNightly/java/org/mozilla/samples/browser/Components.kt
@@ -93,9 +93,9 @@ engine
 .
 gecko
 .
-fetch
+glean
 .
-GeckoViewFetchClient
+GeckoAdapter
 import
 mozilla
 .
@@ -107,9 +107,9 @@ engine
 .
 gecko
 .
-glean
+fetch
 .
-GeckoAdapter
+GeckoViewFetchClient
 import
 mozilla
 .
@@ -215,6 +215,11 @@ GeckoRuntimeSettings
 .
 Builder
 (
+)
+.
+aboutConfigEnabled
+(
+true
 )
 builder
 .
