6d5892f1a2f5a71f60b8213e04ffa8bf064b5858
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-05 09:57:31 +0000

    [focus] Update support library to 26.1.0 and architecture components to 1.0.0-beta2.
    Some of our custom lifecycle classes are not needed anymore as this functionality
    has been implemented in the support library.
    
    UltraBlame original commit: b9ae09775cb45d27e374db28170bfbd55874c7b8

--- mobile/android/focus-android/app/src/main/java/org/mozilla/focus/locale/LocaleAwareFragment.java
+++ mobile/android/focus-android/app/src/main/java/org/mozilla/focus/locale/LocaleAwareFragment.java
@@ -68,11 +68,13 @@ locale
 import
 android
 .
-arch
+support
 .
-lifecycle
+v4
 .
-LifecycleFragment
+app
+.
+Fragment
 ;
 import
 java
@@ -86,7 +88,7 @@ abstract
 class
 LocaleAwareFragment
 extends
-LifecycleFragment
+Fragment
 {
 private
 Locale
