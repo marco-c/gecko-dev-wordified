ff9048a8413d72d501c0f46418e1bf96cac55158
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-05 08:40:58 +0000

    Bug 1642977 - part 2: Let `complete` transform honor `soft-dependencies`
    UltraBlame original commit: 3749391132b1ff0aeb62dcb70b46bd3ec8089d71

--- mobile/android/taskcluster/ac_taskgraph/transforms/chunk.py
+++ mobile/android/taskcluster/ac_taskgraph/transforms/chunk.py
@@ -96,7 +96,7 @@ dep
 to
 be
 added
-MAX_REGULAR_DEPS
+MAX_NUMBER_OF_DEPS
 =
 MAX_DEPENDENCIES
 -
@@ -111,6 +111,7 @@ build_task_definition
 (
 orig_task
 deps
+soft_deps
 count
 )
 :
@@ -129,7 +130,29 @@ dependencies
 '
 ]
 =
+{
+label
+:
+label
+for
+label
+in
 deps
+}
+    
+task
+[
+'
+soft
+-
+dependencies
+'
+]
+=
+list
+(
+soft_deps
+)
     
 task
 [
@@ -266,27 +289,31 @@ count
 =
 1
         
-deps
+soft_deps
 =
-{
-}
+set
+(
+)
+        
+regular_deps
+=
+set
+(
+)
         
 chunked_labels
 =
-{
-}
+set
+(
+)
         
-#
-sort
-for
-deterministic
-chunking
-        
-dep_labels
+soft_dep_labels
 =
+list
+(
 task
 .
-pop
+get
 (
 '
 soft
@@ -296,10 +323,11 @@ dependencies
 [
 ]
 )
+)
         
-dep_labels
-.
-extend
+regular_dep_labels
+=
+list
 (
 task
 .
@@ -317,34 +345,66 @@ keys
 )
 )
         
-dep_labels
+#
+sort
+for
+deterministic
+chunking
+        
+all_dep_labels
 =
 sorted
 (
-dep_labels
+set
+(
+soft_dep_labels
++
+regular_dep_labels
+)
 )
         
 for
 dep_label
 in
-dep_labels
+all_dep_labels
 :
             
-deps
-[
+if
 dep_label
-]
-=
+in
+regular_dep_labels
+:
+                
+regular_deps
+.
+add
+(
 dep_label
+)
+            
+else
+:
+                
+soft_deps
+.
+add
+(
+dep_label
+)
             
 if
 len
 (
-deps
+regular_deps
+)
++
+len
+(
+soft_deps
 )
 =
 =
-MAX_REGULAR_DEPS
+MAX_NUMBER_OF_DEPS
 :
                 
 chunked_task
@@ -352,7 +412,8 @@ chunked_task
 build_task_definition
 (
 task
-deps
+regular_deps
+soft_deps
 count
 )
                 
@@ -365,19 +426,26 @@ chunked_task
 )
                 
 chunked_labels
-[
+.
+add
+(
 chunked_label
-]
-=
-chunked_label
+)
                 
 yield
 chunked_task
                 
-deps
-=
-{
-}
+soft_deps
+.
+clear
+(
+)
+                
+regular_deps
+.
+clear
+(
+)
                 
 count
 +
@@ -385,7 +453,9 @@ count
 1
         
 if
-deps
+regular_deps
+or
+soft_deps
 :
             
 chunked_task
@@ -393,7 +463,8 @@ chunked_task
 build_task_definition
 (
 task
-deps
+regular_deps
+soft_deps
 count
 )
             
@@ -406,19 +477,14 @@ chunked_task
 )
             
 chunked_labels
-[
+.
+add
+(
 chunked_label
-]
-=
-chunked_label
+)
             
 yield
 chunked_task
-            
-count
-+
-=
-1
         
 task
 [
@@ -427,7 +493,15 @@ dependencies
 '
 ]
 =
+{
+label
+:
+label
+for
+label
+in
 chunked_labels
+}
         
 #
 Chunk
