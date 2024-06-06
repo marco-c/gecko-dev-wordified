d4f39a1f5db25dc4f8fc5f0c5650f085f4c6a470
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-06 15:16:26 +0000

    Bug 1878034 - adjust run-on-projects for android build/signing tasks. r=bhearsum,releng-reviewers
    No need to run nightly and beta builds and tests everywhere on push;
    only sign when required by dependencies.
    
    Differential Revision: https://phabricator.services.mozilla.com/D201551
    
    UltraBlame original commit: b27cbae962e24fddde083536e6b520fa66d0821f

--- taskcluster/android_taskgraph/transforms/signing_android_app.py
+++ taskcluster/android_taskgraph/transforms/signing_android_app.py
@@ -193,14 +193,6 @@ notify
 "
             
 "
-run
--
-on
--
-projects
-"
-            
-"
 treeherder
 .
 platform
@@ -259,21 +251,6 @@ params
 [
 "
 level
-"
-]
-                    
-"
-tasks
--
-for
-"
-:
-config
-.
-params
-[
-"
-tasks_for
 "
 ]
                 
