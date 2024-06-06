0df353e5af9090fbc56f36d4997d820b5b372349
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-06 15:07:26 +0000

    Bug 1824856 - android_taskgraph: make build_config loader use gecko_taskgraph transform loader. r=bhearsum,releng-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D201413
    
    UltraBlame original commit: 2c600de231a10aedde92504da6b8a7166bded2be

--- taskcluster/android_taskgraph/loader/build_config.py
+++ taskcluster/android_taskgraph/loader/build_config.py
@@ -54,7 +54,7 @@ MPL
 /
 .
 from
-taskgraph
+gecko_taskgraph
 .
 loader
 .
@@ -125,7 +125,7 @@ yml
 config
 [
 "
-tasks
+jobs
 "
 ]
 =
@@ -209,7 +209,7 @@ yml
 under
 the
 key
-tasks
+jobs
 .
     
 "
@@ -238,7 +238,7 @@ config
 config
 [
 "
-tasks
+jobs
 "
 ]
 =
@@ -247,7 +247,7 @@ merge
 config
 [
 "
-tasks
+jobs
 "
 ]
 components_tasks
