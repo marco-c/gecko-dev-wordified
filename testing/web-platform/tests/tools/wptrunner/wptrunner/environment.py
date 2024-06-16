2251af9f34377e026b129d0292034e539c7812e5
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-16 02:27:01 +0000

    Bug 1889783 [wpt PR 45561] - [wptrunner] On exit, re-register prior `SIGINT` handler, not `SIG_DFL`, a=testonly
    Automatic update from web-platform-tests
    [wptrunner] Use `contextlib.ExitStack` to clean up `TestEnvironment`
    
    This is a no-op refactor that ensures every context manager entered in
    `TestEnvironment.__enter__()` is `__exit__`ed (i.e., this prevents
    accidental mismatches).
    
    --
    [wptrunner] On exit, re-register prior `SIGINT` handler, not `SIG_DFL`
    
    libc's default `SIGINT` behavior is to simply terminate the process (see
    section 24.2.2 "Termination Signals" of [0]). This means that
    interrupting wptrunner just after `TestEnvironment.process_interrupts()`
    will orphan the server subprocesses, as the code to stop them won't run.
    
    This PR fixes this by restoring the previous handler, which defaults to
    a different Python-provided handler that raises `KeyboardInterrupt`
    instead [1]:
    
    ```
    $ python
    Python 3.11.7 (main, Dec  8 2023, 14:22:46) [GCC 13.2.0] on linux
    Type "help", "copyright", "credits" or "license" for more information.
    >>> import signal
    >>> # Hit Ctrl+C here
    KeyboardInterrupt
    >>> signal.signal(signal.SIGINT, signal.SIG_DFL)
    <built-in function default_int_handler>
    >>> # Hit Ctrl+C again, this will terminate `python` instead
    ```
    
    Also, there's no need to switch handlers at all in a noninteractive
    session.
    
    This is a partial fix for https://crbug.com/330236796.
    
    [0]: https://www.gnu.org/software/libc/manual/pdf/libc.pdf
    [1]: https://docs.python.org/3/library/signal.html#signal.SIGINT
    
    --
    
    wpt-commits: 8ddcf20c3a27474e81040d9ee8ba4951e9d68bae, fb0918ddcfa6cb7d76f37b8801350a0e941d3017
    wpt-pr: 45561
    
    UltraBlame original commit: 4b290ea1239a45a71eb1e636c8bcfa631ae3af44

--- testing/web-platform/tests/tools/wptrunner/wptrunner/environment.py
+++ testing/web-platform/tests/tools/wptrunner/wptrunner/environment.py
@@ -7,6 +7,8 @@ untyped
 -
 defs
 import
+contextlib
+import
 errno
 import
 json
@@ -666,6 +668,16 @@ get_context
         
 self
 .
+_stack
+=
+contextlib
+.
+ExitStack
+(
+)
+        
+self
+.
 cache_manager
 =
 mp_context
@@ -742,10 +754,13 @@ server_log_handler
 =
 self
 .
-server_logging_ctx
+_stack
 .
-__enter__
+enter_context
 (
+self
+.
+server_logging_ctx
 )
         
 self
@@ -764,26 +779,35 @@ config
 =
 self
 .
-config_ctx
+_stack
 .
-__enter__
+enter_context
 (
+self
+.
+config_ctx
 )
         
+self
+.
+_stack
+.
+enter_context
+(
 self
 .
 stash
-.
-__enter__
-(
 )
         
 self
 .
-cache_manager
+_stack
 .
-__enter__
+enter_context
 (
+self
+.
+cache_manager
 )
         
 assert
@@ -831,10 +855,13 @@ self
 config
 )
             
-cm
+self
 .
-__enter__
+_stack
+.
+enter_context
 (
+cm
 )
             
 self
@@ -914,8 +941,15 @@ interactive
             
 self
 .
+_stack
+.
+enter_context
+(
+self
+.
 ignore_interrupts
 (
+)
 )
         
 return
@@ -930,12 +964,6 @@ exc_val
 exc_tb
 )
 :
-        
-self
-.
-process_interrupts
-(
-)
         
 for
 servers
@@ -987,15 +1015,9 @@ wait
 (
 )
         
-for
-cm
-in
 self
 .
-env_extras_cms
-:
-            
-cm
+_stack
 .
 __exit__
 (
@@ -1009,47 +1031,10 @@ self
 env_extras_cms
 =
 None
-        
-self
+    
+contextlib
 .
-cache_manager
-.
-__exit__
-(
-exc_type
-exc_val
-exc_tb
-)
-        
-self
-.
-stash
-.
-__exit__
-(
-)
-        
-self
-.
-config_ctx
-.
-__exit__
-(
-exc_type
-exc_val
-exc_tb
-)
-        
-self
-.
-server_logging_ctx
-.
-__exit__
-(
-exc_type
-exc_val
-exc_tb
-)
+contextmanager
     
 def
 ignore_interrupts
@@ -1058,6 +1043,8 @@ self
 )
 :
         
+prev_handler
+=
 signal
 .
 signal
@@ -1069,14 +1056,15 @@ signal
 .
 SIG_IGN
 )
-    
-def
-process_interrupts
-(
-self
-)
-:
         
+try
+:
+            
+yield
+        
+finally
+:
+            
 signal
 .
 signal
@@ -1084,9 +1072,7 @@ signal
 signal
 .
 SIGINT
-signal
-.
-SIG_DFL
+prev_handler
 )
     
 def
