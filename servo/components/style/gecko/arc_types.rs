5749507f913e8e47b46744a25e86b6ff5e557323
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-16 01:38:45 +0000

    Bug 1889496 - Part 2: Implement CSSStartingStyleRule interface. r=webidl,firefox-style-system-reviewers,layout-reviewers,emilio
    Introduce CSSStartingStyleRule and add one extra parsing test for
    starting-style rule.
    
    Differential Revision: https://phabricator.services.mozilla.com/D206564
    
    UltraBlame original commit: 7689f9c266fcdf019b8bea308bf5569885d72ed3

--- servo/components/style/gecko/arc_types.rs
+++ servo/components/style/gecko/arc_types.rs
@@ -188,6 +188,7 @@ NamespaceRule
 PageRule
 PropertyRule
 ScopeRule
+StartingStyleRule
 StyleRule
 StylesheetContents
 SupportsRule
@@ -550,5 +551,13 @@ impl_simple_arc_ffi
 ScopeRule
 Servo_ScopeRule_AddRef
 Servo_ScopeRule_Release
+)
+;
+impl_simple_arc_ffi
+!
+(
+StartingStyleRule
+Servo_StartingStyleRule_AddRef
+Servo_StartingStyleRule_Release
 )
 ;
