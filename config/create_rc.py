ccd8f030b98c2779920313572623766227f5247b
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-16 04:27:22 +0000

    Bug 1879975 - Newer versions of Windows disable pinning to the taskbar using older methods r=mpohle,bhearsum,nrishel,glandium,nalexander
    This is adding in the new Windows 11 only version of taskbar pinning. For the new pinning to work, we need to use limited access feature tokens. Those are going to be made private and aren't included with this change.
    This change will compile, and will work if built against the correct limited access feature tokens, as specified in developer local machine config files, but for every other build, the new taskbar pinning won't work and will fall back to the old methods.
    I will implement the try / release building machines using the secret limited access feature tokens in a follow-up diff.
    
    Differential Revision: https://phabricator.services.mozilla.com/D205004
    
    UltraBlame original commit: a002fbdcd4b28a71fb95b0ca364e70646c4145a2

--- config/create_rc.py
+++ config/create_rc.py
@@ -198,6 +198,19 @@ build
 {
 include
 }
+Identity
+LimitedAccessFeature
+{
+{
+L
+"
+{
+lafidentity
+}
+_pcsmm0jrprpb2
+"
+}
+}
 /
 /
 /
@@ -820,7 +833,7 @@ manifest_id
 :
     
 for
-line
+lineFromInput
 in
 module_rc
 .
@@ -831,7 +844,7 @@ splitlines
         
 line
 =
-line
+lineFromInput
 .
 split
 (
@@ -1222,6 +1235,48 @@ include
 "
 "
     
+#
+Set
+the
+identity
+field
+for
+the
+Limited
+Access
+Feature
+    
+#
+Must
+match
+the
+tokens
+used
+in
+Win11LimitedAccessFeatures
+.
+cpp
+    
+lafidentity
+=
+"
+MozillaFirefox
+"
+    
+#
+lafidentity
+=
+"
+FirefoxBeta
+"
+    
+#
+lafidentity
+=
+"
+FirefoxNightly
+"
+    
 data
 =
 TEMPLATE
@@ -1232,6 +1287,10 @@ format
 include
 =
 include
+        
+lafidentity
+=
+lafidentity
         
 fileversion
 =
