15c73ce64def63833f35b17ff0bf654e9e70e302
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-16 01:25:13 +0000

    Bug 1888772 - Apply ARM-CPU-runtime-detection patch r=media-playback-reviewers,padenot
    This patch is the result running the command below
    ```
    ./mach vendor media/libvpx/moz.yaml --patch-mode=only --ignore-modified
    ```
    with arm_cpu_runtime_detection_code_on_openbsd.patch in moz.yaml's
    patches list only (other patches are temporarily removed during the
    updating process).
    
    Depends on D206409
    
    Differential Revision: https://phabricator.services.mozilla.com/D206410
    
    UltraBlame original commit: 6d481aee635e3d467c417f973108609158ac229d

--- media/libvpx/libvpx/vpx_ports/aarch64_cpudetect.c
+++ media/libvpx/libvpx/vpx_ports/aarch64_cpudetect.c
@@ -122,6 +122,12 @@ endif
 if
 !
 CONFIG_RUNTIME_CPU_DETECT
+|
+|
+defined
+(
+__OpenBSD__
+)
 static
 int
 arm_get_cpu_caps
@@ -201,6 +207,12 @@ __APPLE__
 end
 !
 CONFIG_RUNTIME_CPU_DETECT
+|
+|
+defined
+(
+__OpenBSD__
+)
 /
 /
 sysctlbyname
