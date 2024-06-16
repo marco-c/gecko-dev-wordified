cd889b169cbb6e7beca6dfddaf168615389b7ee7
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-06-16 09:22:02 +0000

    Bug 1898502 - [devtools] Turn output-parser.js lookup Arrays into Sets. r=devtools-reviewers,bomsy.
    Depends on D211330
    
    Differential Revision: https://phabricator.services.mozilla.com/D211371
    
    UltraBlame original commit: 3f5f270fcb1f8680026dfeb399d89e4762fa5811

--- devtools/client/shared/output-parser.js
+++ devtools/client/shared/output-parser.js
@@ -219,6 +219,9 @@ argument
 const
 ANGLE_TAKING_FUNCTIONS
 =
+new
+Set
+(
 [
 "
 linear
@@ -292,6 +295,7 @@ hue
 rotate
 "
 ]
+)
 ;
 /
 /
@@ -308,6 +312,9 @@ names
 const
 BEZIER_KEYWORDS
 =
+new
+Set
+(
 [
 "
 linear
@@ -333,6 +340,7 @@ out
 ease
 "
 ]
+)
 ;
 /
 /
@@ -346,6 +354,9 @@ argument
 const
 COLOR_TAKING_FUNCTIONS
 =
+new
+Set
+(
 [
 "
 linear
@@ -430,6 +441,7 @@ color
 mix
 "
 ]
+)
 ;
 /
 /
@@ -443,6 +455,9 @@ argument
 const
 BASIC_SHAPE_FUNCTIONS
 =
+new
+Set
+(
 [
 "
 polygon
@@ -457,6 +472,7 @@ ellipse
 inset
 "
 ]
+)
 ;
 const
 BACKDROP_FILTER_ENABLED
@@ -2938,7 +2954,7 @@ isColorTakingFunction
 =
 COLOR_TAKING_FUNCTIONS
 .
-includes
+has
 (
 lowerCaseFunctionName
 )
@@ -2950,7 +2966,7 @@ isColorTakingFunction
 |
 ANGLE_TAKING_FUNCTIONS
 .
-includes
+has
 (
 lowerCaseFunctionName
 )
@@ -3729,7 +3745,7 @@ expectShape
 &
 BASIC_SHAPE_FUNCTIONS
 .
-includes
+has
 (
 lowerCaseFunctionName
 )
@@ -3775,7 +3791,7 @@ expectCubicBezier
 &
 BEZIER_KEYWORDS
 .
-includes
+has
 (
 lowerCaseTokenText
 )
