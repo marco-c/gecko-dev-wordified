3727ee77948542fd91582b02bcc9ee483f73c6a9
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-22 23:03:33 +0000

    Bug 1903098 - Vendor libwebrtc from b0fe794d7d
    Upstream commit: https://webrtc.googlesource.com/src/+/b0fe794d7d9867c67625b573b1f9afd24bb38a19
        Delete expired field trial WebRTC-SignalNetworkPreferenceChange
    
        Bug: webrtc:42221944
        Change-Id: I786d73f5ede27d4ab40a9b3b2fef49da45bd3444
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/350302
        Commit-Queue: Danil Chapovalov <danilchapwebrtc.org>
        Reviewed-by: Harald Alvestrand <htawebrtc.org>
        Cr-Commit-Position: refs/heads/main{#42274}
    
    UltraBlame original commit: 22d041ba7161c8b6ebb3c99dfc38c0c60b22c910

--- third_party/libwebrtc/experiments/field_trials.py
+++ third_party/libwebrtc/experiments/field_trials.py
@@ -4252,24 +4252,6 @@ FieldTrial
 '
 WebRTC
 -
-SignalNetworkPreferenceChange
-'
-               
-42221944
-               
-date
-(
-2024
-4
-1
-)
-)
-    
-FieldTrial
-(
-'
-WebRTC
--
 SimulcastEncoderAdapter
 -
 GetEncoderInfoOverride
@@ -5060,7 +5042,7 @@ str
 \
     
 '
-84f7bc41874cd36cc4d5306e86972f53fef83f29
+b0b0cce63bfdf327229b569576a07b716db186b7
 '
 REGISTERED_FIELD_TRIALS
 :
