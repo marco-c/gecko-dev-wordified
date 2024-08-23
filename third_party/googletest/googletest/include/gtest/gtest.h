f186ca2f86f1cdab9b2fb4735c83f34d2f46d3e0
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-08-23 02:04:22 +0000

    Bug 1906722 - Update googletest to 34ad51b3dc4f922d8ab622491dd44fc2c39afee9 r=gbrown
    Differential Revision: https://phabricator.services.mozilla.com/D217861
    
    UltraBlame original commit: 59aaf6047de67f184ff908a69a101a41754d8276

--- third_party/googletest/googletest/include/gtest/gtest.h
+++ third_party/googletest/googletest/include/gtest/gtest.h
@@ -728,15 +728,6 @@ string
 .
 h
 "
-#
-include
-"
-mozilla
-/
-Attributes
-.
-h
-"
 GTEST_DISABLE_MSC_WARNINGS_PUSH_
 (
 4251
@@ -2622,7 +2613,6 @@ or
 TEST_F
 macro
 .
-MOZ_CAN_RUN_SCRIPT
 virtual
 void
 TestBody
