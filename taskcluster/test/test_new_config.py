89fb42dc3a89021bf27089807ed5ef514cbbdf15
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-16 01:06:48 +0000

    Bug 1916582 - Fix firefox-ci new config test to avoid common variants. r=aryx
    Differential Revision: https://phabricator.services.mozilla.com/D221004
    
    UltraBlame original commit: 1bf029f0c1f6bef6d2cf60f828a4e3849a081aef

--- taskcluster/test/test_new_config.py
+++ taskcluster/test/test_new_config.py
@@ -588,10 +588,35 @@ qr
 /
 opt
 "
+                
+and
+(
+"
+spi
+-
+nw
+"
+not
+in
+t
+.
+label
+and
+"
+a11y
+-
+checks
+"
+not
+in
+t
+.
+label
+)
             
 )
             
-64
+32
             
 id
 =
