5ee91313e5d8ec589e375347888c01aae98e7930
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-09-16 01:30:04 +0000

    Bug 1913382 - reorder toolkit/components/glean/metrics_index.py to have parser/html/metrics.yaml at the correct position, r=freddyb.
    Differential Revision: https://phabricator.services.mozilla.com/D221758
    
    UltraBlame original commit: 6d0d5e032337473ecb73c3423174152fa19e7489

--- toolkit/components/glean/metrics_index.py
+++ toolkit/components/glean/metrics_index.py
@@ -345,16 +345,6 @@ yaml
 "
     
 "
-parser
-/
-html
-/
-metrics
-.
-yaml
-"
-    
-"
 image
 /
 decoders
@@ -424,6 +414,16 @@ netwerk
 protocol
 /
 http
+/
+metrics
+.
+yaml
+"
+    
+"
+parser
+/
+html
 /
 metrics
 .
