690da19c7ead03fe07a6b1afc60db2676325a41c
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-13 03:08:33 +0000

    Bug 1933136 - Sphinx: fix a sphinx get_html_theme_path warning r=firefox-source-docs-reviewers,ahal DONTBUILD
    Was showing:
    WARNING: Calling get_html_theme_path is deprecated. If you are calling it to define html_theme_path, you are safe to remove that code.
    
    Differential Revision: https://phabricator.services.mozilla.com/D230078
    
    UltraBlame original commit: bfec291b69fb9616d66c38c51d3ac6ce1f5ad4fb

--- docs/conf.py
+++ docs/conf.py
@@ -1134,24 +1134,11 @@ this
 otherwise
 .
     
-import
-sphinx_rtd_theme
-    
 html_theme
 =
 "
 sphinx_rtd_theme
 "
-    
-html_theme_path
-=
-[
-sphinx_rtd_theme
-.
-get_html_theme_path
-(
-)
-]
 html_static_path
 =
 [
