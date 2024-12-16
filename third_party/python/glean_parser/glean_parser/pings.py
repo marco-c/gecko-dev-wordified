da073817133f96fb4c25777e2b8980d9f46b7bbf
Marco Castelluccio <mcastelluccio@mozilla.com>
2024-12-16 00:54:50 +0000

    Bug 1933939 - Update to glean_parser v16.1.0 r=mach-reviewers,chutten
    Differential Revision: https://phabricator.services.mozilla.com/D230525
    
    UltraBlame original commit: 3b3c94c4b26ef859bd4f17069013a1ecc39619bc

--- third_party/python/glean_parser/glean_parser/pings.py
+++ third_party/python/glean_parser/glean_parser/pings.py
@@ -310,6 +310,22 @@ include_info_sections
 True
 )
         
+self
+.
+follows_collection_enabled
+=
+self
+.
+metadata
+.
+get
+(
+"
+follows_collection_enabled
+"
+True
+)
+        
 if
 enabled
 is
@@ -325,6 +341,19 @@ self
 enabled
 =
 enabled
+        
+if
+not
+self
+.
+follows_collection_enabled
+:
+            
+self
+.
+enabled
+=
+False
         
 self
 .
@@ -660,6 +689,20 @@ remove_output_params
 modified_dict
 "
 include_info_sections
+"
+        
+)
+        
+modified_dict
+=
+util
+.
+remove_output_params
+(
+            
+modified_dict
+"
+follows_collection_enabled
 "
         
 )
