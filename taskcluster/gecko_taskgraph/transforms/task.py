e15a0607d873d8269b0ad3daf45174912589433b
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 01:41:25 +0000

    Bug 1938593 - [ci] Add project and trust-domain tags to all tasks, r=taskgraph-reviewers,bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D232727
    
    UltraBlame original commit: 3b143ed7394f16a8bb6ed878b674dc626aa7a228

--- taskcluster/gecko_taskgraph/transforms/task.py
+++ taskcluster/gecko_taskgraph/transforms/task.py
@@ -18091,6 +18091,36 @@ else
 "
 false
 "
+                
+"
+project
+"
+:
+config
+.
+params
+[
+"
+project
+"
+]
+                
+"
+trust
+-
+domain
+"
+:
+config
+.
+graph_config
+[
+"
+trust
+-
+domain
+"
+]
             
 }
         
