20f66d5e57f23461ea6f58e86252067e8d6c68d4
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-01-01 02:04:21 +0000

    No Bug, mozilla-central repo-update HSTS HPKP ct-logs - r=release-managers,RyanVM
    Differential Revision: https://phabricator.services.mozilla.com/D232974
    
    UltraBlame original commit: 1542f650101aac965e1b6cb9dc6162fe2b5e35b3

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -162,7 +162,7 @@ kCTExpirationTime
 =
 INT64_C
 (
-1741259803000000
+1741605333000000
 )
 ;
 namespace
--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -7726,6 +7726,6 @@ kPreloadPKPinsExpirationTime
 =
 INT64_C
 (
-1743678990382000
+1744024518573000
 )
 ;
