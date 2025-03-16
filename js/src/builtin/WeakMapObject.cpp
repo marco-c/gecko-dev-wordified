e4ba817fb6a4b1432c13afb9149462a59977645e
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 02:32:43 +0000

    Bug 1935193 part 3 - Use fuses to optimize check for overridden 'add' or 'set' method. r=mgaudet
    This makes a `new Set(array)` micro-benchmark about 10-15% faster.
    
    We have existing jit-tests for monkey-patched `add` or `set` methods (eg `Map-constructor-set.js`)
    so this only adds a test for the fuse state.
    
    Differential Revision: https://phabricator.services.mozilla.com/D240453
    
    UltraBlame original commit: 8caf2c3c51c6d0cfb90e457556c38f33a5aea180

--- js/src/builtin/WeakMapObject.cpp
+++ js/src/builtin/WeakMapObject.cpp
@@ -2453,6 +2453,7 @@ WeakMapObject
 :
 :
 properties
+GenericFinishInitWithPrototypeFuseProperty
 }
 ;
 const
--- js/src/builtin/WeakSetObject.cpp
+++ js/src/builtin/WeakSetObject.cpp
@@ -1043,6 +1043,7 @@ WeakSetObject
 :
 :
 properties
+GenericFinishInitWithPrototypeFuseProperty
 }
 ;
 const
