10a9ed9f7b8ed6eb1e0db474cb21d7f749d6dd96
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-03-16 03:18:14 +0000

    Bug 1946905 - Fixup multi-device behaviour in mozrunner verify_android_device. r=jmaher
    Fix issues with mozrunner when multiple devices/emulators are on system.
        - Resolve inconsistent states between device_serial argument and
          DEVICE_SERIAL environment variable.
        - Ignore detached or unauthorized devices when deciding which to use or
          determining if selection is ambiguous.
        - Fail verify_android_device if device selection is ambiguous.
    
    Differential Revision: https://phabricator.services.mozilla.com/D239849
    
    UltraBlame original commit: e395081896864a66447cea150cd6e210301c768f

--- testing/webcompat/mach_commands.py
+++ testing/webcompat/mach_commands.py
@@ -1655,6 +1655,24 @@ verify_android_device
             
 )
             
+#
+verify_android_device
+sets
+up
+device
+/
+emulator
+and
+records
+selected
+            
+#
+one
+to
+DEVICE_SERIAL
+environment
+.
+            
 verify_android_device
 (
                 
