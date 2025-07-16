0e7f2dbb127f2472b00d5f84235fa8fcde0907ee
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-07-16 02:52:37 +0000

    Bug 1976132 - Update mach intermittents to handle wpt paths;r=jmaher
    Cut/paste some code from testing/skipfails.py so it can be used without side effects
    and tested more easily.
    
    Differential Revision: https://phabricator.services.mozilla.com/D256477
    
    UltraBlame original commit: e08db6217383be89a14f5535926dd7f15a0506c2

--- testing/intermittent_failures.py
+++ testing/intermittent_failures.py
@@ -84,6 +84,10 @@ Optional
 TypedDict
 import
 requests
+from
+wpt_path_utils
+import
+resolve_wpt_path
 USER_AGENT
 =
 "
@@ -595,6 +599,31 @@ if
 match
 :
                         
+test_path
+=
+match
+[
+0
+]
+                        
+if
+test_path
+.
+startswith
+(
+"
+/
+"
+)
+:
+                            
+test_path
+=
+resolve_wpt_path
+(
+test_path
+)
+                        
 result
 [
 "
@@ -602,10 +631,7 @@ test_path
 "
 ]
 =
-match
-[
-0
-]
+test_path
                 
 results
 .
