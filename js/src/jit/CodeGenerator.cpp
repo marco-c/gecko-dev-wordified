f831a5f5687e96bf88c34d209eb38f22ec088957
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-08-16 02:08:50 +0000

    Bug 1981188 - Part 6: Prefer BaseObjectElementIndex for BaseIndex into elements. r=iain
    Differential Revision: https://phabricator.services.mozilla.com/D260118
    
    UltraBlame original commit: 4518aedf5a98f1c0043ec5bde7e226a948575c06

--- js/src/jit/CodeGenerator.cpp
+++ js/src/jit/CodeGenerator.cpp
@@ -119655,7 +119655,7 @@ masm
 .
 computeEffectiveAddress
 (
-BaseValueIndex
+BaseObjectElementIndex
 (
 elements
 lengthReg
@@ -157714,7 +157714,7 @@ spectreTemp
 notDense
 )
 ;
-BaseValueIndex
+BaseObjectElementIndex
 element
 (
 temp
