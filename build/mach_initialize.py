01e2e8518531f0588972b6374d5f375765c412b6
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-09-01 03:07:49 +0000

    Bug 1982531 - Add `mach` telemetry for `Fleet` and `CrowdStrike` r=sylvestre,firefox-build-system-reviewers,glandium,afranchuk
    Differential Revision: https://phabricator.services.mozilla.com/D260789
    
    UltraBlame original commit: 162a9c539b1addd0244fcf3d8dc39004af6f88ff

--- build/mach_initialize.py
+++ build/mach_initialize.py
@@ -2146,6 +2146,10 @@ name
 bootstrap
 "
 success
+Path
+(
+topsrcdir
+)
         
 )
     
@@ -2920,6 +2924,7 @@ _finalize_telemetry_glean
 telemetry
 is_bootstrap
 success
+topsrcdir
 )
 :
     
@@ -2966,6 +2971,7 @@ mach
 telemetry
 import
 MACH_METRICS_PATH
+resolve_is_employee_by_credentials
     
 from
 mozbuild
@@ -2976,7 +2982,11 @@ import
         
 get_cpu_brand
         
+get_crowdstrike_running
+        
 get_distro_and_version
+        
+get_fleet_running
         
 get_psutil_stats
         
@@ -3133,6 +3143,46 @@ vscode_running
 set
 (
 get_vscode_running
+(
+)
+)
+    
+#
+Only
+collect
+Fleet
+and
+CrowdStrike
+metrics
+for
+Mozilla
+employees
+    
+if
+resolve_is_employee_by_credentials
+(
+topsrcdir
+)
+:
+        
+system_metrics
+.
+fleet_running
+.
+set
+(
+get_fleet_running
+(
+)
+)
+        
+system_metrics
+.
+crowdstrike_running
+.
+set
+(
+get_crowdstrike_running
 (
 )
 )
