28442971e74dbf4762dfe00722327c93a621d63a
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-10-16 01:29:01 +0000

    Bug 1993176 - Cleanup unused inclusion of mozilla/MemoryReporting.h r=emilio
    Differential Revision: https://phabricator.services.mozilla.com/D267921
    
    UltraBlame original commit: de9b8986e1ce29535bacc9938b279ecda143433a

--- dom/base/nsGlobalWindowOuter.cpp
+++ dom/base/nsGlobalWindowOuter.cpp
@@ -140,15 +140,6 @@ include
 "
 mozilla
 /
-MemoryReporting
-.
-h
-"
-#
-include
-"
-mozilla
-/
 ScopeExit
 .
 h
--- dom/base/nsWindowMemoryReporter.h
+++ dom/base/nsWindowMemoryReporter.h
@@ -143,15 +143,6 @@ include
 "
 mozilla
 /
-MemoryReporting
-.
-h
-"
-#
-include
-"
-mozilla
-/
 PodOperations
 .
 h
