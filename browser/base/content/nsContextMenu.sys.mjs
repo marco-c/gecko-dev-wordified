d85625c3ed7384d8536dd484aaaa03cb20764783
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 03:19:37 +0000

    Revert "Bug 2004502 - Text Fragments: Show "Copy Link to Highlight" context menu item if strip-on-share is disabled. r=Gijs" for causing bc failures URLQueryStringStripper.cpp.
    This reverts commit cad026ec7d15595fe06d164c6a3c9287341e3f34.
    
    UltraBlame original commit: f98f4d33e3405042da5163333598a3d781bfd239

--- browser/base/content/nsContextMenu.sys.mjs
+++ browser/base/content/nsContextMenu.sys.mjs
@@ -2757,6 +2757,11 @@ lazy
 TEXT_FRAGMENTS_ENABLED
 &
 &
+lazy
+.
+STRIP_ON_SHARE_ENABLED
+&
+&
 !
 (
 this
@@ -2838,11 +2843,6 @@ to
 highlight
 "
 shouldShow
-&
-&
-lazy
-.
-STRIP_ON_SHARE_ENABLED
 )
 ;
 /
