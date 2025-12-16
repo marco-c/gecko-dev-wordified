05d5d2bfac396b76d2889e759e4708cbf38d8ece
Marco Castelluccio <mcastelluccio@mozilla.com>
2025-12-16 02:59:02 +0000

    Revert "Bug 988938 - Replace toolkit/components/telemetry/Makefile.in by moz.build (s) r=glandium,toolkit-telemetry-reviewers" for causing xpc failures  test_TelemetryEnvironment.js
    This reverts commit 95115ae3f21054f91f7a20ffd6fe776a286499c3.
    
    UltraBlame original commit: 9bbf3b20c19c268955f6b400df92033a60a902da

--- python/mozbuild/mozbuild/frontend/emitter.py
+++ python/mozbuild/mozbuild/frontend/emitter.py
@@ -8866,10 +8866,6 @@ WASM_LIBS
 "
 XPI_PKGNAME
 "
-            
-"
-XPI_TESTDIR
-"
         
 ]
         
@@ -8901,34 +8897,6 @@ context
 [
 v
 ]
-        
-if
-"
-XPI_TESTDIR
-"
-in
-context
-and
-"
-XPI_PKGNAME
-"
-not
-in
-context
-:
-            
-raise
-SandboxValidationError
-(
-"
-XPI_TESTDIR
-set
-but
-XPI_PKGNAME
-not
-set
-"
-)
         
 if
 (
