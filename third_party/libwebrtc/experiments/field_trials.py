e6219536f7205c970c4991d067f765c57dab3a83
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 02:34:19 +0000

    Bug 2006933 - Vendor libwebrtc from 3a2bd6e66a
    Upstream commit: https://webrtc.googlesource.com/src/+/3a2bd6e66aeb273715792efd0bbfd2430323c4f8
        Add EncoderSpeedExperiment field trial helper class.
    
        This CL adds a helper class to simplify parsing of the
        WebRTC-EncoderSpeed field trial string, which specifies the
        encoder complexity to use per codec and camera/screenshare permutation.
    
        Bug: webrtc:443906251
        Change-Id: I2a89e9149b8583b117e448e2dd3e9cf309729bb5
        Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/414401
        Commit-Queue: Erik Språng <sprangwebrtc.org>
        Reviewed-by: Sergey Silkin <ssilkinwebrtc.org>
        Auto-Submit: Erik Språng <sprangwebrtc.org>
        Cr-Commit-Position: refs/heads/main{#45854}
    
    UltraBlame original commit: 48cb4c8c700e5237189875f369a32ce97a030e7e

--- third_party/libwebrtc/experiments/field_trials.py
+++ third_party/libwebrtc/experiments/field_trials.py
@@ -621,6 +621,24 @@ FieldTrial
 '
 WebRTC
 -
+EncoderSpeed
+'
+               
+443906251
+               
+date
+(
+2026
+4
+6
+)
+)
+    
+FieldTrial
+(
+'
+WebRTC
+-
 EnforceTransceiverDirection
 '
                
