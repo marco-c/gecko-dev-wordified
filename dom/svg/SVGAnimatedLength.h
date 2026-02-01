bfe3011dd6cd6554cb2e5bab3229b2dfc285a66f
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-01 03:06:18 +0000

    Revert "Bug 2009781 - Wrap `SVGElementMetrics::mCtx` in a RefPtr r=firefox-svg-reviewers,longsonr" for causing build bustage on RefPtr.h
    This reverts commit b76ea5ad0c17da1801350c004d5cb3ad70ba5a46.
    
    UltraBlame original commit: c2d47848e16d8e7b98c2ccc56ef16d88096bf43e

--- dom/svg/SVGAnimatedLength.h
+++ dom/svg/SVGAnimatedLength.h
@@ -457,13 +457,6 @@ aCtx
 nullptr
 )
 ;
-~
-SVGElementMetrics
-(
-)
-=
-default
-;
 float
 GetEmLength
 (
@@ -567,11 +560,9 @@ SVGElement
 mSVGElement
 ;
 mutable
-RefPtr
-<
 const
 SVGViewportElement
->
+*
 mCtx
 ;
 }
