5f33df28e45272a3d457e1f7adaaf0257e96804a
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 02:06:31 +0000

    Bug 2014249: Add try.noartifact mach option to skip autodetection of artifact builds by try-chooser. r=ahal
    For people who regularly switch between branches, working directories and build
    modes, the automatic behaviour of try chooser to select artifact try builds
    based on current build environment can be error prone. This patch adds a mach
    setting to disable the autodetection while still allowing manual control with
    the `./mach try --artifact` command.
    
    Differential Revision: https://phabricator.services.mozilla.com/D281661
    
    UltraBlame original commit: 4e5c098a5b3fd0de59496e82c0e71202662c4878

--- tools/tryselect/mach_commands.py
+++ tools/tryselect/mach_commands.py
@@ -240,6 +240,7 @@ tryselect
 import
 lando
 push
+task_config
     
 mach_context
 =
@@ -281,6 +282,24 @@ try
 [
 "
 maxhistory
+"
+]
+    
+task_config
+.
+SKIP_ARTIFACT_BUILD_CHECK
+=
+mach_context
+.
+settings
+[
+"
+try
+"
+]
+[
+"
+noartifact
 "
 ]
 memoize
