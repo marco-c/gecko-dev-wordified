fd312f73ee56253a22100c70730a517373670615
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 03:17:26 +0000

    No Bug, mozilla-central repo-update HSTS HPKP remote-settings tld-suffixes ct-logs - r=release-managers,pascalc
    Differential Revision: https://phabricator.services.mozilla.com/D282417
    
    UltraBlame original commit: 223533e87cfafe385ef9c0d09a5e39994bed9dc6

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -162,7 +162,7 @@ kCTExpirationTime
 =
 INT64_C
 (
-1776473243000000
+1776700747000000
 )
 ;
 namespace
--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -7424,6 +7424,6 @@ kPreloadPKPinsExpirationTime
 =
 INT64_C
 (
-1778892414006000
+1779119918258000
 )
 ;
