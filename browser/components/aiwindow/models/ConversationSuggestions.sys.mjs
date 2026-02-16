069b4d62818021e6ce3e58850fd85f73933914cf
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-02-16 04:18:10 +0000

    Revert "Bug 2012830 - updating ChatConversation starters to pull systemPrompt and realTimeInfo prompt from remote settings r=omarg,ai-models-reviewers,ai-frontend-reviewers,cdipersio" for causing xpcshell failures in test_ChatConversation.js.
    This reverts commit acc5e916a90f5a0f87c81798eedad40d8ca5b41d.
    
    UltraBlame original commit: 1a50801c42aaf3d49f99cd28cec815f1f72d6e56

--- browser/components/aiwindow/models/ConversationSuggestions.sys.mjs
+++ browser/components/aiwindow/models/ConversationSuggestions.sys.mjs
@@ -451,6 +451,7 @@ n
 const
 memoryPrompt
 =
+await
 renderPrompt
 (
 conversationMemoriesPrompt
@@ -1436,6 +1437,7 @@ template
 const
 base
 =
+await
 renderPrompt
 (
 conversationStarterPrompt
@@ -1869,6 +1871,7 @@ CONVERSATION_SUGGESTIONS_ASSISTANT_LIMITATIONS
 const
 base
 =
+await
 renderPrompt
 (
 conversationFollowupPrompt
