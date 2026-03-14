609f1b63f2da935dadea2fd8220d68134f1ec656
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 05:33:47 +0000

    Bug 2018816 - Move prim_origin from interning keys to prim instances r=gfx-reviewers,lsalzman
    This is a step towards no longer caring about the local position of the
    primitives in the display list, so long as they end up positioned at
    the same transformed location.
    
    Differential Revision: https://phabricator.services.mozilla.com/D284574
    
    UltraBlame original commit: d64b31ee87aef1d1c980a693ff615c01be019cbf

--- gfx/wr/webrender/src/scene_building.rs
+++ gfx/wr/webrender/src/scene_building.rs
@@ -11396,6 +11396,11 @@ new
 (
 instance_kind
 clip_leaf_id
+info
+.
+rect
+.
+min
 )
 }
 fn
@@ -17226,6 +17231,12 @@ clip_tree_builder
 build_for_picture
 (
 clip_node_id
+)
+LayoutPoint
+:
+:
+zero
+(
 )
 )
 ;
@@ -26150,6 +26161,12 @@ clip_tree_builder
 build_for_picture
 (
 clip_node_id
+)
+LayoutPoint
+:
+:
+zero
+(
 )
 )
 }
