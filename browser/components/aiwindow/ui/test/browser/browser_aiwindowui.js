68060fec0c6b8b883dfa9ba136c48b91016ee754
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 05:49:41 +0000

    Bug 2015576 - enable switching tabs while LLM responses are streaming r=ai-frontend-reviewers,ai-models-reviewers,ngrato
    Differential Revision: https://phabricator.services.mozilla.com/D286405
    
    UltraBlame original commit: 52f52ad2f983c3ccf32127a4c56facbd610af81a

--- browser/components/aiwindow/ui/test/browser/browser_aiwindowui.js
+++ browser/components/aiwindow/ui/test/browser/browser_aiwindowui.js
@@ -318,7 +318,7 @@ is
 (
 box
 .
-hidden
+collapsed
 true
 "
 Box
@@ -334,7 +334,7 @@ is
 (
 splitter
 .
-hidden
+collapsed
 true
 "
 Splitter
@@ -401,7 +401,7 @@ is
 (
 box
 .
-hidden
+collapsed
 false
 "
 Box
@@ -418,7 +418,7 @@ is
 (
 splitter
 .
-hidden
+collapsed
 false
 "
 Splitter
@@ -487,7 +487,7 @@ is
 (
 box
 .
-hidden
+collapsed
 true
 "
 Box
@@ -504,7 +504,7 @@ is
 (
 splitter
 .
-hidden
+collapsed
 true
 "
 Splitter
@@ -547,13 +547,13 @@ initial
 state
 box
 .
-hidden
+collapsed
 =
 initialBoxHidden
 ;
 splitter
 .
-hidden
+collapsed
 =
 initialSplitterHidden
 ;
