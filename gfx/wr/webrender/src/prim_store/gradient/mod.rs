f16ab2fa4fb25ec7c1a06bfd28110d4a67b382bc
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 04:28:41 +0000

    Bug 2020923 - Disable the remaining use of non-quad linear gradients. r=gw
    Differential Revision: https://phabricator.services.mozilla.com/D286035
    
    UltraBlame original commit: 10735a085e48920c89499e88f2f25527bab3d96e

--- gfx/wr/webrender/src/prim_store/gradient/mod.rs
+++ gfx/wr/webrender/src/prim_store/gradient/mod.rs
@@ -112,15 +112,6 @@ use
 linear
 :
 :
-MAX_CACHED_SIZE
-as
-LINEAR_MAX_CACHED_SIZE
-;
-pub
-use
-linear
-:
-:
 *
 ;
 pub
@@ -2900,7 +2891,7 @@ LinearGradientTemplate
 >
 (
 )
-144
+152
 "
 LinearGradientTemplate
 size
@@ -2966,7 +2957,7 @@ RadialGradientTemplate
 >
 (
 )
-144
+152
 "
 RadialGradientTemplate
 size
@@ -3032,7 +3023,7 @@ ConicGradientTemplate
 >
 (
 )
-144
+152
 "
 ConicGradientTemplate
 size
