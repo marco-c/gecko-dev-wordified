63b88b4aa143c09f63424e23d3e88c3d5b63c2fe
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 05:47:16 +0000

    Revert "Bug 2018816 - Move prim_origin from interning keys to prim instances r=gfx-reviewers,lsalzman" for causing failures fixed-pos-scrolled-clip-2.html.
    This reverts commit f725a211382a836f974ac2b376df45108e088eef.
    
    UltraBlame original commit: 0a18fd244c7f8ac3ad7c952b6257e2692339d857

--- gfx/wr/webrender/src/scene_building.rs
+++ gfx/wr/webrender/src/scene_building.rs
@@ -11323,11 +11323,6 @@ new
 (
 instance_kind
 clip_leaf_id
-info
-.
-rect
-.
-min
 )
 }
 fn
@@ -17158,12 +17153,6 @@ clip_tree_builder
 build_for_picture
 (
 clip_node_id
-)
-LayoutPoint
-:
-:
-zero
-(
 )
 )
 ;
@@ -26088,12 +26077,6 @@ clip_tree_builder
 build_for_picture
 (
 clip_node_id
-)
-LayoutPoint
-:
-:
-zero
-(
 )
 )
 }
