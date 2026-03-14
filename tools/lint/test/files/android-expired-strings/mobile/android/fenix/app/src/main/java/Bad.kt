72f9ee29b56c6c5d5ac4679c8885b8faa4585da9
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 05:47:30 +0000

    Bug 1988790 - Add linter to detect references to Android strings marked for removal (moz:removedIn) r=tthibaud,linter-reviewers,android-reviewers,android-l10n-reviewers,flod,sylvestre,sergesanspaille
    Differential Revision: https://phabricator.services.mozilla.com/D286380
    
    UltraBlame original commit: 913dacd00b5ca1483bb1b2b44da282fe2f697fc6

---
+++ tools/lint/test/files/android-expired-strings/mobile/android/fenix/app/src/main/java/Bad.kt
@@ -0,0 +1,15 @@
+package
+test
+val
+old
+=
+context
+.
+getString
+(
+R
+.
+string
+.
+fenix_deprecated
+)
---
+++ tools/lint/test/files/android-expired-strings/mobile/android/fenix/app/src/test/java/TestFile.kt
@@ -0,0 +1,15 @@
+package
+test
+val
+testOld
+=
+context
+.
+getString
+(
+R
+.
+string
+.
+fenix_deprecated
+)
