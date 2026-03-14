649c017a84c0aaa41f63647877ece34a7e5127c5
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-14 03:17:37 +0000

    Revert "Bug 2020127 - Simplify animation range storage / handling. r=firefox-style-system-reviewers,boris" for causing column-rule-shorthand.html failures.
    This reverts commit 952c3c47f6bfaa6bddb6d7815aec4f38634f0b50.
    
    Revert "Bug 2020127 - Autogenerate initial specified value handling. r=firefox-style-system-reviewers,boris"
    
    This reverts commit cde39a6fb8055a1128dd0177a967cb6207a5c473.
    
    UltraBlame original commit: d196b0e15fb423d4e8dea0a6a924664ca6a7248f

--- servo/components/style/properties/data.py
+++ servo/components/style/properties/data.py
@@ -2400,6 +2400,10 @@ initial_value
 =
 None
         
+initial_specified_value
+=
+None
+        
 parse_method
 =
 '
@@ -2578,6 +2582,12 @@ self
 initial_value
 =
 initial_value
+        
+self
+.
+initial_specified_value
+=
+initial_specified_value
         
 self
 .
