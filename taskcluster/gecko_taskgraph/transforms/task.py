ed18deda424e47ca3a2e74329c4e94f690a8fb47
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-03-27 01:06:42 +0000

    Bug 2021009 - [ci] Support regex match when evaluating 'treeherder.branch-map' config, r=taskgraph-reviewers,bhearsum
    Differential Revision: https://phabricator.services.mozilla.com/D288746
    
    UltraBlame original commit: cb1c6d983e36ad98adedefc25c821448eb4a7c4b

--- taskcluster/gecko_taskgraph/transforms/task.py
+++ taskcluster/gecko_taskgraph/transforms/task.py
@@ -199,6 +199,7 @@ util
 keyed_by
 import
 evaluate_keyed_by
+keymatch
 from
 taskgraph
 .
@@ -5225,15 +5226,22 @@ config
 if
 head_ref
 and
-head_ref
-in
+(
+matches
+:
+=
+keymatch
+(
 th_branch_map
+head_ref
+)
+)
 :
         
 return
-th_branch_map
+matches
 [
-head_ref
+0
 ]
     
 return
