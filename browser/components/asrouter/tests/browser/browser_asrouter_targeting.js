c503981b18a7e8a5c24451e5f1a4dcb2985406c4
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-04-16 03:49:15 +0000

    Bug 2029253 - Add RESTORE_SESSION message action and related targeting parameters. r=omc-reviewers,emcminn
    Differential Revision: https://phabricator.services.mozilla.com/D292042
    
    UltraBlame original commit: eb7ebb01ba2f92fdd0183a9ff535a6d10c3f164e

--- browser/components/asrouter/tests/browser/browser_asrouter_targeting.js
+++ browser/components/asrouter/tests/browser/browser_asrouter_targeting.js
@@ -3696,7 +3696,7 @@ expected
 =
 PrivateBrowsingUtils
 .
-isContentWindowPrivate
+isWindowPrivate
 (
 win
 )
@@ -3740,7 +3740,7 @@ equal
 to
 PrivateBrowsingUtils
 .
-isContentWindowPrivate
+isWindowPrivate
 (
 )
 "
@@ -3841,7 +3841,7 @@ expected
 =
 PrivateBrowsingUtils
 .
-isContentWindowPrivate
+isWindowPrivate
 (
 privateWin
 )
@@ -3885,7 +3885,7 @@ equal
 to
 PrivateBrowsingUtils
 .
-isContentWindowPrivate
+isWindowPrivate
 (
 )
 "
