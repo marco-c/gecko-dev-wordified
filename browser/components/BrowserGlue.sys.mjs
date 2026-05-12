382b94f0f8c69d283fa564d480e7d5cba21d0162
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-05-12 13:00:39 +0000

    Revert "Bug 2034260 - Enable semantic history search feature gate when smart window is enabled r=Mardak,mak,rrando,ai-frontend-reviewers,places-reviewers,ai-models-reviewers,firefox-desktop-core-reviewers ,urlbar-reviewers" for causing xpcshell failures  test_profileDataUpgrade_smartwindow_semanticHistory.js
    This reverts commit 6ba267f5a1eb21156c59f9f8173d032d74f82307.
    
    UltraBlame original commit: 3cdfe145ff1ee28af2da0c444687dada0fb7f2e3

--- browser/components/BrowserGlue.sys.mjs
+++ browser/components/BrowserGlue.sys.mjs
@@ -10181,7 +10181,7 @@ number
 const
 APP_DATA_VERSION
 =
-171
+170
 ;
 const
 PREF
--- browser/components/aiwindow/models/SearchBrowsingHistory.sys.mjs
+++ browser/components/aiwindow/models/SearchBrowsingHistory.sys.mjs
@@ -3647,7 +3647,7 @@ canUseSemantic
 =
 semanticManager
 .
-isEnabledForSmartWindow
+canUseSemanticSearch
 &
 &
 (
