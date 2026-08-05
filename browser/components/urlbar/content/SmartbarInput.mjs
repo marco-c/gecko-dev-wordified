c608a23eb6452499ed49a35a086de052eaf2d3ae
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-08-05 21:41:02 +0000

    Bug 2053759 - Make it possible import UrlbarPrefs into non-chrome content. r=dao,urlbar-reviewers
    Differential Revision: https://phabricator.services.mozilla.com/D312735
    
    UltraBlame original commit: c5f7b1cac09eae93b7c8090cec5a416570c225a4

--- browser/components/urlbar/content/SmartbarInput.mjs
+++ browser/components/urlbar/content/SmartbarInput.mjs
@@ -33141,7 +33141,7 @@ value
 =
 lazy
 .
-UrlbarPrefs
+UrlbarUtils
 .
 isPersistedSearchTermsEnabled
 (
@@ -37751,7 +37751,7 @@ if
 !
 lazy
 .
-UrlbarPrefs
+UrlbarUtils
 .
 isPersistedSearchTermsEnabled
 (
