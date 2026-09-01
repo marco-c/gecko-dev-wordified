338d1f5852e4e7228deb3c7873395b403fbfa510
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-09-01 16:07:33 +0000

    Bug 2053447: Part 4 - Update rest of LaunchOnLogin code to use new API.r=omc-reviewers,firefox-desktop-core-reviewers ,aminomancer
    Port ASRouter and telemetry code to new LaunchOnLogin API
    
    Differential Revision: https://phabricator.services.mozilla.com/D313485
    
    UltraBlame original commit: 1ef229f106094efc1111207b611aab9a87a4c97d

--- toolkit/components/messaging-system/lib/SpecialMessageActions.sys.mjs
+++ toolkit/components/messaging-system/lib/SpecialMessageActions.sys.mjs
@@ -867,7 +867,7 @@ sys
 .
 mjs
 "
-WindowsLaunchOnLogin
+LaunchOnLogin
 :
 "
 resource
@@ -878,7 +878,7 @@ gre
 /
 modules
 /
-WindowsLaunchOnLogin
+LaunchOnLogin
 .
 sys
 .
@@ -6722,9 +6722,9 @@ CONFIRM_LAUNCH_ON_LOGIN
 await
 lazy
 .
-WindowsLaunchOnLogin
+LaunchOnLogin
 .
-createLaunchOnLogin
+enable
 (
 )
 ;
@@ -6738,9 +6738,9 @@ REMOVE_LAUNCH_ON_LOGIN
 await
 lazy
 .
-WindowsLaunchOnLogin
+LaunchOnLogin
 .
-removeLaunchOnLogin
+disable
 (
 )
 ;
