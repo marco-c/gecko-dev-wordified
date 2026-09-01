bbc2aafa6e0bee59b5b5f196f15069d29ef4c0db
Marco Castelluccio <mcastelluccio@mozilla.com>
2026-09-01 18:43:59 +0000

    Revert "Bug 2063064, Bug 2056051: Restructure ImageDecoderReadRequest teardown to remove its CAN_RUN_SCRIPT_BOUNDARY r=media-playback-reviewers,aosmond" for causing build bustages on ImageDecoderBinding.cpp
    This reverts commit 324adfbd3024ea6c35c60e444a6c67b10a8c29d1.
    
    Revert "Bug 2056051: Don't cancel the ImageDecoder read request from cycle-collection unlink r=dom-core-reviewers,media-playback-reviewers,smaug,aosmond"
    
    This reverts commit e8ff0efe95edd43df51f9e0cb8b5d9f54f879cf7.
    
    UltraBlame original commit: 871510f51054eb93d3ed826874ac397bf60b708f

--- dom/media/webcodecs/ImageDecoder.h
+++ dom/media/webcodecs/ImageDecoder.h
@@ -268,7 +268,6 @@ aGivenProto
 )
 override
 ;
-MOZ_CAN_RUN_SCRIPT
 static
 already_AddRefed
 <
@@ -350,7 +349,6 @@ return
 mTracks
 ;
 }
-MOZ_CAN_RUN_SCRIPT
 already_AddRefed
 <
 Promise
@@ -371,7 +369,6 @@ Reset
 (
 )
 ;
-MOZ_CAN_RUN_SCRIPT
 void
 Close
 (
@@ -399,7 +396,6 @@ uint32_t
 aSelectedIndex
 )
 ;
-MOZ_CAN_RUN_SCRIPT
 void
 ProcessControlMessageQueue
 (
@@ -504,7 +500,6 @@ ImageDecoder
 )
 ;
 }
-MOZ_CAN_RUN_SCRIPT
 void
 Initialize
 (
@@ -521,7 +516,6 @@ ErrorResult
 aRv
 )
 ;
-MOZ_CAN_RUN_SCRIPT
 void
 Close
 (
@@ -533,28 +527,6 @@ aResult
 ;
 void
 CloseWithoutRef
-(
-const
-MediaResult
-&
-aResult
-)
-;
-MOZ_CAN_RUN_SCRIPT
-void
-CloseAndCancelWithoutRef
-(
-const
-MediaResult
-&
-aResult
-)
-;
-already_AddRefed
-<
-ImageDecoderReadRequest
->
-CloseCommon
 (
 const
 MediaResult
@@ -589,13 +561,11 @@ QueueDecodeFrameMessage
 (
 )
 ;
-MOZ_CAN_RUN_SCRIPT
 void
 ResumeControlMessageQueue
 (
 )
 ;
-MOZ_CAN_RUN_SCRIPT
 MessageProcessedResult
 ProcessConfigureMessage
 (
@@ -659,7 +629,6 @@ DecodeMetadataResult
 aMetadata
 )
 ;
-MOZ_CAN_RUN_SCRIPT
 void
 OnMetadataFailed
 (
@@ -688,7 +657,6 @@ DecodeFrameCountResult
 aResult
 )
 ;
-MOZ_CAN_RUN_SCRIPT
 void
 OnFrameCountFailed
 (
